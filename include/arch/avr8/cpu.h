b11160430431cd3b5d1895deb3fc008bc48cad90
lucas <lucas@lisha.ufsc.br>
2005-04-15 19:56:56 +0000

    Completing AVR CPU Mediator

--- include/arch/avr8/cpu.h
+++ include/arch/avr8/cpu.h
@@ -1,10 +1,5 @@
 // EPOS-- AVR8 Declarations 
 
-// This work is licensed under the Creative Commons 
-// Attribution-NonCommercial-NoDerivs License. To view a copy of this license, 
-// visit http://creativecommons.org/licenses/by-nc-nd/2.0/ or send a letter to 
-// Creative Commons, 559 Nathan Abbott Way, Stanford, California 94305, USA.
-
 #ifndef __avr8_h
 #define __avr8_h
 
@@ -20,14 +15,7 @@ class AVR8: public CPU_Common
   typedef Traits<AVR8> Traits;
   static const Type_Id TYPE = Type<AVR8>::TYPE;
 
-  // AVR8 private imports, types and constants
-
-
  public:
-  // Memory page
-  typedef unsigned char Page;
-    
-    
   // CPU Flags
   typedef Reg8 Flags;
   enum {  
@@ -43,7 +31,7 @@ class AVR8: public CPU_Common
     FLAG_CLEAR        = ~FLAG_DEFAULTS
   };
 
-
+  // CPU Context
   class Context {
 		
   public:
@@ -64,7 +52,7 @@ class AVR8: public CPU_Common
   public:
       Reg8 _sreg;
       Reg8 _r0;
-      //Reg8 _r1;		// r1 is always 0
+//    Reg8 _r1;		// r1 is always 0, thus not part of ctx
       Reg8 _r2;
       Reg8 _r3;
       Reg8 _r4;
@@ -97,88 +85,91 @@ class AVR8: public CPU_Common
       Reg8 _r31;
       Reg16 _pc;		
   };
-    
-  class IO_Registers {
-  public:
-    typedef unsigned char volatile IOReg8;
-    
-    IOReg8 twbr;    	// [0x20]
-    IOReg8 twsr;    	// [0x21]
-    IOReg8 twar;    	// [0x22]
-    IOReg8 twdr;    	// [0x23]
-    IOReg8 adcl;    	// [0x24]
-    IOReg8 adch;    	// [0x25]
-    IOReg8 adcsra;      // [0x26]
-    IOReg8 admux;    	// [0x27]
-    IOReg8 acsr;    	// [0x28]   
-    IOReg8 ubrrl;    	// [0x29]
-    IOReg8 ucsrb;    	// [0x2A]
-    IOReg8 ucsra;    	// [0x2B]
-    IOReg8 udr;    	// [0x2C]
-    IOReg8 spcr;    	// [0x2D]
-    IOReg8 spsr;    	// [0x2E]
-    IOReg8 spdr;    	// [0x2F]
-    IOReg8 pind;    	// [0x30]
-    IOReg8 ddrd;    	// [0x31]
-    IOReg8 portd;    	// [0x32]
-    IOReg8 pinc;    	// [0x33]
-    IOReg8 ddrc;    	// [0x34]
-    IOReg8 portc;    	// [0x35]
-    IOReg8 pinb;    	// [0x36]
-    IOReg8 ddrb;    	// [0x37]
-    IOReg8 portb;    	// [0x38]
-    IOReg8 pina;    	// [0x39] 
-    IOReg8 ddra;    	// [0x3A]
-    IOReg8 porta;    	// [0x3B] 
-    IOReg8 eecr;    	// [0x3C] 
-    IOReg8 eedr;    	// [0x3D]
-    IOReg8 eearl;    	// [0x3E]
-    IOReg8 eearh;    	// [0x3F]
+
+  // I/O ports
+  typedef volatile Reg8 IO_Port;
+  typedef volatile Reg8 IO_Irq;    
+  struct IO_Ports {
+    IO_Port twbr;    	// [0x20]
+    IO_Port twsr;    	// [0x21]
+    IO_Port twar;    	// [0x22]
+    IO_Port twdr;    	// [0x23]
+    IO_Port adcl;    	// [0x24]
+    IO_Port adch;    	// [0x25]
+    IO_Port adcsra;     // [0x26]
+    IO_Port admux;    	// [0x27]
+    IO_Port acsr;    	// [0x28]   
+    IO_Port ubrrl;    	// [0x29]
+    IO_Port ucsrb;    	// [0x2A]
+    IO_Port ucsra;    	// [0x2B]
+    IO_Port udr;    	// [0x2C]
+    IO_Port spcr;    	// [0x2D]
+    IO_Port spsr;    	// [0x2E]
+    IO_Port spdr;    	// [0x2F]
+    IO_Port pind;    	// [0x30]
+    IO_Port ddrd;    	// [0x31]
+    IO_Port portd;    	// [0x32]
+    IO_Port pinc;    	// [0x33]
+    IO_Port ddrc;    	// [0x34]
+    IO_Port portc;    	// [0x35]
+    IO_Port pinb;    	// [0x36]
+    IO_Port ddrb;    	// [0x37]
+    IO_Port portb;    	// [0x38]
+    IO_Port pina;    	// [0x39] 
+    IO_Port ddra;    	// [0x3A]
+    IO_Port porta;    	// [0x3B] 
+    IO_Port eecr;    	// [0x3C] 
+    IO_Port eedr;    	// [0x3D]
+    IO_Port eearl;    	// [0x3E]
+    IO_Port eearh;    	// [0x3F]
     union
     {
-      IOReg8 ubrrh;	// [0x40]
-      IOReg8 ucsrc;
+      IO_Port ubrrh;	// [0x40]
+      IO_Port ucsrc;
     };
-    IOReg8 wdtcr;    	// [0x41]
-    IOReg8 assr;    	// [0x42]
-    IOReg8 ocr2;    	// [0x43]
-    IOReg8 tcnt2;    	// [0x44]
-    IOReg8 tccr2;    	// [0x45]
-    IOReg8 icr1l;    	// [0x46]
-    IOReg8 icr1h;    	// [0x47]
-    IOReg8 ocr1bl;      // [0x48]
-    IOReg8 ocr1bh;      // [0x49]
-    IOReg8 ocr1al;      // [0x4A]
-    IOReg8 ocr1ah;      // [0x4B]
-    IOReg8 tcnt1l;      // [0x4C]
-    IOReg8 tcnt1h;      // [0x4D]
-    IOReg8 tccr1b;      // [0x4E]
-    IOReg8 tccr1a;      // [0x4F]
-    IOReg8 sfior;    	// [0x50]
+    IO_Port wdtcr;    	// [0x41]
+    IO_Port assr;    	// [0x42]
+    IO_Port ocr2;    	// [0x43]
+    IO_Port tcnt2;    	// [0x44]
+    IO_Port tccr2;    	// [0x45]
+    IO_Port icr1l;    	// [0x46]
+    IO_Port icr1h;    	// [0x47]
+    IO_Port ocr1bl;     // [0x48]
+    IO_Port ocr1bh;     // [0x49]
+    IO_Port ocr1al;     // [0x4A]
+    IO_Port ocr1ah;     // [0x4B]
+    IO_Port tcnt1l;     // [0x4C]
+    IO_Port tcnt1h;     // [0x4D]
+    IO_Port tccr1b;     // [0x4E]
+    IO_Port tccr1a;     // [0x4F]
+    IO_Port sfior;    	// [0x50]
     union
     {
-      IOReg8 osccal;	// [0x51]
-      IOReg8 ocdr;
+      IO_Port osccal;	// [0x51]
+      IO_Port ocdr;
     };
-    IOReg8 tcnt0;    	// [0x52]
-    IOReg8 tccr0;    	// [0x53]
-    IOReg8 mcucsr;      // [0x54]
-    IOReg8 mcucr;    	// [0x55]
-    IOReg8 twcr;    	// [0x56]
-    IOReg8 spmcr;    	// [0x57]
-    IOReg8 tifr;    	// [0x58]
-    IOReg8 timsk;    	// [0x59]
-    IOReg8 gifr;    	// [0x5A]
-    IOReg8 gicr;     	// [0x5B]
-    IOReg8 ocr0;    	// [0x5C] 
-    IOReg8 spl;    	// [0x5D]
-    IOReg8 sph;    	// [0x5E]
-    IOReg8 sreg;    	// [0x5F]
+    IO_Port tcnt0;    	// [0x52]
+    IO_Port tccr0;    	// [0x53]
+    IO_Port mcucsr;     // [0x54]
+    IO_Port mcucr;    	// [0x55]
+    IO_Port twcr;    	// [0x56]
+    IO_Port spmcr;    	// [0x57]
+    IO_Port tifr;    	// [0x58]
+    IO_Port timsk;    	// [0x59]
+    IO_Port gifr;    	// [0x5A]
+    IO_Port gicr;     	// [0x5B]
+    IO_Port ocr0;    	// [0x5C] 
+    IO_Port spl;    	// [0x5D]
+    IO_Port sph;    	// [0x5E]
+    IO_Port sreg;    	// [0x5F]
   };
 	
 
  public:
-  Hertz clock(){ return 0; };
+  AVR8() {} 
+  ~AVR8() {}
+ 
+  Hertz clock(){  Traits::CLOCK; };
 
   static void int_enable(){ASMV("sei");};
   static void int_disable(){ASMV("cli");};
@@ -187,15 +178,110 @@ class AVR8: public CPU_Common
   static void switch_context(Context * volatile * current, 
 			     Context * volatile next);
 
-  static bool tsl(volatile bool & lock){ return false;};
-  static int finc(volatile int & number){ return 0;};
-  static int fdec(volatile int & number){ return 0;};
+  static Flags flags() { return sreg(); }
+  static void flags(Flags flags) { sreg(flags); }
 
-  static int init(System_Info * si);
+  static Reg16 sp() { return sph_l(); }
+  static void sp(Reg16 sp) { sph_l(sp); }
 
-  static Reg16 fr() { return 0; }
+  static Reg8 fr() { return r25_24(); }
+  static void fr(Reg16 fr) { r25_24(fr); }
 
-  // AVR8 implementation methods
+  static Reg16 pdp() { return 0; }
+  static void pdp(Reg16 pdp) { }
+  
+  static Log_Addr ip() { return pc(); }  
+
+  static bool tsl(volatile bool & lock) {
+    int_disable();
+    register bool old = CPU_Common::tsl(lock);
+    int_enable();
+    return old;
+  }
+    
+  static int finc(volatile int & value) {
+    int_disable();
+    register bool old = CPU_Common::finc(value);
+    int_enable();
+    return old;
+  }
+  
+  static int fdec(volatile int & value) {
+    int_disable();
+    register bool old = CPU_Common::finc(value);
+    int_enable();
+    return old;
+  }
+
+  static Reg32 htonl(Reg32 v)	{
+    return htonl_lsb(v);
+  }
+  
+  static Reg16 htons(Reg16 v)	{
+    return htons_lsb(v);
+  }
+  
+  static Reg32 ntohl(Reg32 v)	{
+    return htonl(v);
+  }
+  
+  static Reg16 ntohs(Reg16 v)	{
+    return htons(v);
+  }
+
+  // AVR8 specific methods
+  
+  static Reg16 sph_l() {
+    return (in8(0x3e)<<8) | in8(0x3d);
+  }
+  
+  static void sph_l(Reg16 value) {
+    out8(0x3e,value>>8);
+    out8(0x3d,(Reg8)value);
+  }  
+  
+  static Reg8 sreg(){
+    return in8(0x3f);
+  }
+  
+  static void sreg(Reg8 value){
+    out8(0x3f,value);
+  }
+  
+  static Reg16 r25_24(){
+    Reg16 value;
+    ASMV("mov 	%A0,r24"	"\n"
+    	 "mov 	%B0,r25"	"\n"
+	 : "=r" (value)
+	 : 
+    );
+    return value;
+  }  
+  
+  static void r25_24(Reg16 value){
+    ASMV("mov 	r24,%A0"	"\n"
+    	 "mov 	r25,%B0"	"\n"
+	 : 
+	 : "r" (value)
+    );  
+  }
+  
+  static Log_Addr pc(){
+    Log_Addr value;
+    ASMV("cli"			"\n"
+    	 "call 1f"		"\n"
+    	 "1:"			"\n"
+	 "in 	%A0,0x3e"	"\n"
+    	 "in 	%B0,0x3d"	"\n"
+	 "pop 	r1"		"\n"
+	 "pop	r1"		"\n"
+	 "clr	r1"		"\n"
+	 "sei"			"\n"
+	 : "=r" (value)
+	 : 
+	);
+    return value;  
+  }
     
   static unsigned char in8(const unsigned char port) {
     return (*(volatile unsigned char *)(port + 0x20));
@@ -204,11 +290,15 @@ class AVR8: public CPU_Common
   static void out8(unsigned char port, unsigned char value) {
     (*(volatile unsigned char *)(port + 0x20)) = value;
   }
+  
+  static int init(System_Info * si);
 
-  static IO_Registers * io;
+  // This will be eliminated
+  static IO_Ports * io;
     
  private:
 
+  // This will be replaced
   static unsigned int swap_bytes(unsigned int i)
   {
     return ((i << 8) | (i >> 8));
