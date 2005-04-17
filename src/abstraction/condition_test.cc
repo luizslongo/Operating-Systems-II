af535e11cbb62502c3cb35499ba23c6b66d010a0
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2005-04-17 21:54:12 +0000

    This is a very bad example for condition variable usege, but those on pthread tutorials on the web are even worse. If some one comes by one of the old good books on concurrent/parallel programming, please, send us a better use case.

--- src/abstraction/condition_test.cc
+++ src/abstraction/condition_test.cc
@@ -1,17 +1,100 @@
-// EPOS-- Condition Variable Abstraction Test Program
+// EPOS-- Semaphore Test Program
 
 #include <utility/ostream.h>
+#include <thread.h>
 #include <condition.h>
+#include <alarm.h>
+#include <display.h>
 
 __USING_SYS
 
+const int iterations = 10;
+
+Thread * m;
+Thread * phil[5];
+Condition * chopstick[5];
+Condition * barrier;
+
+OStream cout;
+
+int philosopher(int n, int l, int c)
+{
+    Display display;
+    
+//     cout << "I'm the philosopher " << n << "\n";
+//     cout << "I'll print at " << l << " x " << c << "\n";
+ 
+    int first = (n < 4)? n : 0;
+    int second = (n < 4)? n + 1 : 4;
+
+    barrier->wait();
+
+    for(int i = iterations; i > 0; i--) {
+	display.position(l, c);
+ 	cout << "thinking";
+	Alarm::delay(1000000);
+
+	chopstick[first]->wait();   // get first chopstick
+	chopstick[second]->wait();   // get second chopstick
+	display.position(l, c);
+	cout << " eating ";
+	Alarm::delay(500000);
+	chopstick[first]->signal();   // release first chopstick
+	chopstick[second]->signal();   // release second chopstick
+    }
+
+    return(iterations);
+}
+
 int main()
 {
-    OStream cout;
+    Display display;
 
-    cout << "Condition test\n";
+    display.clear();
+    cout << "The Philosopher's Dinner:\n";
 
-    Condition synchronizer;
+    for(int i = 0; i < 5; i++)
+	chopstick[i] = new Condition;
+    barrier = new Condition;
+    
+    phil[0] = new Thread(&philosopher, 0, 5, 32);
+    phil[1] = new Thread(&philosopher, 1, 10, 44);
+    phil[2] = new Thread(&philosopher, 2, 16, 39);
+    phil[3] = new Thread(&philosopher, 3, 16, 24);
+    phil[4] = new Thread(&philosopher, 4, 10, 20);
+
+    cout << "Philosophers are alife and hungry!\n";
+
+    cout << "The dinner is served!\n";
+    display.position(7, 44);
+    cout << '/';
+    display.position(13, 44);
+    cout << '\\';
+    display.position(16, 35);
+    cout << '|';
+    display.position(13, 27);
+    cout << '/';
+    display.position(7, 27);
+    cout << '\\';
+
+    Alarm::delay(2000000);
+    barrier->broadcast();
+
+    for(int i = 0; i < 5; i++)
+	chopstick[i]->signal();
+
+    for(int i = 0; i < 5; i++) {
+	int ret = phil[i]->join();
+	display.position(20 + i, 0);
+	cout << "Philosopher " << i << " ate " << ret << " times \n";
+    }
+
+    for(int i = 0; i < 5; i++)
+	delete chopstick[i];
+    for(int i = 0; i < 5; i++)
+	delete phil[i];
+    
+    cout << "The end!\n";
 
     return 0;
 }
