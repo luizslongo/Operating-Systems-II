72b744b44652e4ad18b2f9006ee398d0b4f0a00e
lucas <lucas@lisha.ufsc.br>
2005-04-25 21:11:53 +0000

    Default UART initialization is now performed by init (instead of default constructor)

--- src/mach/avrmcu/uart_init.cc
+++ src/mach/avrmcu/uart_init.cc
@@ -1,6 +1,5 @@
 // EPOS-- AVRMCU_UART Initialization
 
-
 #include <uart.h>
 
 __BEGIN_SYS
@@ -9,7 +8,20 @@ __BEGIN_SYS
 int AVRMCU_UART::init(System_Info * si)
 {
     db<AVRMCU_UART>(TRC) << "AVRMCU_UART::init()\n";
+    
+    __ucsrc = 0;
+    
+    unsigned long br = (Traits<CPU>::CLOCK / 8 / B9600) - 1;
+    ubrrh(!URSEL & (br>>8));
+    ubrrl(br);       
+    
+    __ucsrc |= (UCSZ1 | UCSZ0); // 8 Data Bits
+    __ucsrc &= (!UPM1 & !UPM0); // No Parity
+    __ucsrc |= USBS;		// 2 Stop Bits	
+    ucsrc(URSEL|__ucsrc);    
 
+    start();
+    
     return 0;
 }
 
