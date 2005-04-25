7651ca5aa69de77e4c7fae1e311dd825434c4961
lucas <lucas@lisha.ufsc.br>
2005-04-25 21:08:02 +0000

    Using new register access mechanism.

--- include/mach/avrmcu/adc.h
+++ include/mach/avrmcu/adc.h
@@ -1,11 +1,5 @@
 // EPOS-- AVRMCU ADC Mediator
 
-// This work is licensed under the Creative Commons 
-// Attribution-NonCommercial-NoDerivs License. To view a copy of this license, 
-// visit http://creativecommons.org/licenses/by-nc-nd/2.0/ or send a letter to 
-// Creative Commons, 559 Nathan Abbott Way, Stanford, California 94305, USA.
-
-
 #ifndef __avrmcu_adc_h
 #define __avrmcu_adc_h
 
@@ -17,152 +11,181 @@ class AVRMCU_ADC: public ADC_Common
 {
  private:
 
-  // This should be informed by the MCU
-  static const long CLOCK = 8000000;
+    // This should be informed by the MCU
+    static const long long CLOCK = Traits<AVR8>::CLOCK;
 
-  typedef Traits<AVRMCU_ADC> _Traits;
-  static const Type_Id TYPE = Type<AVRMCU_ADC>::TYPE;
+    typedef Traits<AVRMCU_ADC> _Traits;
+    static const Type_Id TYPE = Type<AVRMCU_ADC>::TYPE;
 
-  // ADC IO Registers
-  enum {
-    ADMUX = 0x07,
-    ADCSRA = 0x06,
-    ADCH = 0x05,
-    ADCL = 0x04,
-    SFIOR = 0x30
-  };
+    // ADC IO Registers
+    enum {
+        ADMUX 	= 0x07,
+        ADCSRA 	= 0x06,
+        ADCH 	= 0x05,
+        ADCL 	= 0x04,
+        SFIOR 	= 0x30
+    };
 
-  //UART IO Register Bits
-  enum {
-    // ADMUX
-    REFS1 = 0x80,
-    REFS0 = 0x40,
-    ADLAR = 0x20,
-    MUX4 = 0x10,
-    MUX3 = 0x08,
-    MUX2 = 0x04,
-    MUX1 = 0x02,
-    MUX0 = 0x01,
-    // ADCSRA
-    ADEN = 0x80,
-    ADSC = 0x40,
-    ADATE = 0x20,
-    ADIF = 0x10,
-    ADIE = 0x08,
-    ADPS2 = 0x04,
-    ADPS1 = 0x02,
-    ADPS0 = 0x01,
-    // SFIOR
-    ADTS2 = 0x80,
-    ADTS1 = 0x40,
-    ADTS0 = 0x20,
-  };
+    //UART IO Register Bits
+    enum {
+        // ADMUX
+        REFS1 = 0x80,
+        REFS0 = 0x40,
+        ADLAR = 0x20,
+        MUX4 = 0x10,
+        MUX3 = 0x08,
+        MUX2 = 0x04,
+        MUX1 = 0x02,
+        MUX0 = 0x01,
+        // ADCSRA
+        ADEN = 0x80,
+        ADSC = 0x40,
+        ADATE = 0x20,
+        ADIF = 0x10,
+        ADIE = 0x08,
+        ADPS2 = 0x04,
+        ADPS1 = 0x02,
+        ADPS0 = 0x01,
+        // SFIOR
+        ADTS2 = 0x80,
+        ADTS1 = 0x40,
+        ADTS0 = 0x20,
+    };
 
 
  public:
 
+    AVRMCU_ADC()    { /* Default Initialization is up to init */ }
 
-
-  AVRMCU_ADC()  {
-	// Defaults: Channel 0, System Reference, Free Running Mode, CLOCK/128
-	AVR8::io->admux  = 0x00;	// Channel 0, AREF
-	AVR8::io->sfior  = ADATE;	// Free Running Mode
-	AVR8::io->adcsra = ADPS2 | ADPS1 |  ADPS0;	// CLOCK/128
-  }
-
-  AVRMCU_ADC(Channel c,Reference r, Trigger t, Hertz f) {
+    AVRMCU_ADC(Channel c,Reference r, Trigger t, Hertz f) {
 	channel(c);
 	reference(r);
 	trigger(t);
 	frequency(f);
-  }
-
-  int read(){
-    if (data_ready() == 1) {
-      int result = AVR8::io->adcl;
-      result |= ((int)(AVR8::io->adch) << 8);
-	  return result;
     }
-    return -1;
-  }
+    
+    ~AVRMCU_ADC();
 
-
-  int data_ready(){
-    if (AVR8::io->adcsra & ADIF)
-      return 1;
-    return -1;
-  }
-
-  int start(){
-    AVR8::io->adcsra |= ADSC | ADEN;
-    return 1;
-  }
-
-  int stop(){
-    AVR8::io->adcsra &= ~ADSC & ~ADEN;
-    return 1;
-  }
-
-  int trigger(Trigger t){
-    if (t == FREE_RUNNING_MODE) {
-	  AVR8::io->sfior  &= (~ADTS2 & ~ADTS1 & ~ADTS0);
-      AVR8::io->adcsra |= ADATE;
-    } else if (t == SINGLE_CONVERSION_MODE) {
-	  AVR8::io->adcsra &= ~ADATE;
+    int read(){
+        if (data_ready() == 1)
+	    return adchl();
+        return -1;
     }
-    return 1;
-  }
 
-  int channel(Channel c){
-	AVR8::io->admux = c | (AVR8::io->admux & ~MUX4 & ~MUX3 & ~MUX2 & ~MUX1 & ~MUX0);
-    return 1;
-  }
 
-  int reference(Reference r)
-  {
-    unsigned char mask = AVR8::io->admux;
-    switch (r){
-      case SYSTEM_REFERENCE:   mask &= ~REFS1 & ~REFS0; break; 
-      case EXTERNAL_REFERENCE: mask = (mask & ~REFS1) | REFS0;  break;
-      case INTERNAL_REFERENCE: mask |= REFS1 | REFS0;  break;
+    int data_ready(){
+        if (adcsra() & ADIF)
+            return 1;
+        return -1;
     }
-    AVR8::io->admux = mask;
-    return 1;
-  }
 
-  Hertz frequency(Hertz f)
-  {
-    unsigned int div = CLOCK/f;
-    unsigned int ps;
-    // there MUST be a better way to do this!
-    if (div >= 128) {
-      ps = ADPS2|ADPS1|ADPS0;
-      div = 128;
-    } else if (div >= 64) {
-      ps = ADPS2|ADPS1;
-      div = 64;
-    } else if (div >= 32) {
-      ps = ADPS2|ADPS0;
-      div = 32;
-    } else if (div >= 16) {
-      ps = ADPS2;
-      div = 16;
-    } else if (div >= 8) {
-      ps = ADPS1 | ADPS0;
-      div = 8;
-    } else if (div >= 4) {
-      ps = ADPS1;
-      div = 4;
-    } else {
-      ps = 0;
-      div = 2;
+    int start(){
+    	adcsra(adcsra() | ADSC | ADEN);
+        return 1;
     }
-    ps |= (AVR8::io->adcsra & ~ADPS2 & ~ADPS1 & ~ADPS0);
-    AVR8::io->adcsra = ps;
-    return (Hertz)(CLOCK/div);
-  }
-  
-  static int init(System_Info *si);
+
+    int stop(){
+        adcsra(adcsra() & ~ADSC & ~ADEN);
+        return 1;
+    }
+
+    int trigger(Trigger t){
+        if (t == FREE_RUNNING_MODE) {
+	    sfior(sfior() & ~ADTS2 & ~ADTS1 & ~ADTS0);
+	    adcsra(adcsra() | ADATE);
+        } else if (t == SINGLE_CONVERSION_MODE) {
+	    adcsra(adcsra() & ~ADATE);
+        }
+        return 1;
+    }
+
+    int channel(Channel c){
+    	admux(c | (admux() & ~MUX4 & ~MUX3 & ~MUX2 & ~MUX1 & ~MUX0));
+        return 1;
+    }
+
+    int reference(Reference r)
+    {
+        unsigned char mask = admux();
+        switch (r){
+            case SYSTEM_REFERENCE:   mask &= ~REFS1 & ~REFS0; 		break; 
+            case EXTERNAL_REFERENCE: mask = (mask & ~REFS1) | REFS0;    break;
+            case INTERNAL_REFERENCE: mask |= REFS1 | REFS0;    		break;
+        }
+        admux(mask);
+        return 1;
+    }
+
+    Hertz frequency(Hertz f)
+    {
+        unsigned int div = CLOCK/f;
+        unsigned int ps;
+        // there MUST be a better way to do this!
+        if (div >= 128) {
+            ps = ADPS2|ADPS1|ADPS0;
+            div = 128;
+        } else if (div >= 64) {
+            ps = ADPS2|ADPS1;
+            div = 64;
+        } else if (div >= 32) {
+            ps = ADPS2|ADPS0;
+            div = 32;
+        } else if (div >= 16) {
+            ps = ADPS2;
+            div = 16;
+        } else if (div >= 8) {
+            ps = ADPS1 | ADPS0;
+            div = 8;
+        } else if (div >= 4) {
+            ps = ADPS1;
+            div = 4;
+        } else {
+            ps = 0;
+            div = 2;
+        }
+        ps |= (adcsra() & ~ADPS2 & ~ADPS1 & ~ADPS0);
+        adcsra(ps);
+        return (Hertz)(CLOCK/div);
+    }
+    
+    static int init(System_Info *si);
+ 
+private:
+
+    typedef AVR8::Reg8 Reg8;
+    typedef AVR8::Reg16 Reg16;
+
+    static Reg8 admux(){
+	return AVR8::in8(ADMUX);
+    }
+    
+    static void admux(Reg8 value){
+	AVR8::out8(ADMUX,value);
+    }   
+    
+    static Reg8 adcsra(){
+	return AVR8::in8(ADCSRA);
+    }
+    
+    static void adcsra(Reg8 value){
+	AVR8::out8(ADCSRA,value);
+    }       
+    
+    static Reg8 sfior(){
+	return AVR8::in8(SFIOR);
+    }
+    
+    static void sfior(Reg8 value){
+	AVR8::out8(SFIOR,value);
+    }     
+    
+    static Reg16 adchl(){
+	return AVR8::in16(ADCL);
+    }
+    
+    static void adchl(Reg16 value){
+	AVR8::out16(ADCL,value);
+    }           
   
 
 };
--- include/mach/avrmcu/uart.h
+++ include/mach/avrmcu/uart.h
@@ -15,138 +15,195 @@ __BEGIN_SYS
 class AVRMCU_UART: public UART_Common
 {
 private:
-  typedef Traits<AVRMCU_UART> _Traits;
-  static const Type_Id TYPE = Type<AVRMCU_UART>::TYPE;
+    typedef Traits<AVRMCU_UART> _Traits;
+    static const Type_Id TYPE = Type<AVRMCU_UART>::TYPE;
 
-  //UART IO Registers
-  enum {
-    UDR     = 0x0c,
-    UCSRA   = 0x0b,
-    UCSRB   = 0x0a,
-    UCSRC   = 0x20,
-    UBRRL   = 0x09,
-    UBRRH   = 0x20
-  };
+    //UART IO Registers
+    enum {
+        UDR	= 0x0c,
+        UCSRA	= 0x0b,
+        UCSRB	= 0x0a,
+        UCSRC	= 0x20,
+        UBRRL	= 0x09,
+        UBRRH	= 0x20
+    };
 
-  //UART IO Register Bits
-  enum {
-    //UCSRA
-    RCX     = 0x80,
-    TXC     = 0x40,
-    UDRE    = 0x20,
-    FE      = 0x10,
-    DOR     = 0x08,
-    PE      = 0x04,
-    U2X     = 0x02,
-    MPCM    = 0x01,
-    //UCSRB
-    RXCIE   = 0x80,
-    TXCIE   = 0x40,
-    UDRIE   = 0x20,
-    RXEN    = 0x10,
-    TXEN    = 0x08,
-    UCS2Z   = 0x04,
-    RXB8    = 0x02,
-    TXB8    = 0x01,
-    //UCSRC
-    URSEL   = 0x80,
-    UMSEL   = 0x40,
-    UPM1    = 0x20,
-    UPM0    = 0x10,
-    USBS    = 0x08,
-    UCSZ1   = 0x04,
-    UCSZ0   = 0x02,
-    UCPOL   = 0x01
+    //UART IO Register Bits
+    enum {
+        //UCSRA
+        RCX	= 0x80,
+        TXC 	= 0x40,
+        UDRE	= 0x20,
+        FE	= 0x10,
+        DOR	= 0x08,
+        PE	= 0x04,
+        U2X	= 0x02,
+        MPCM	= 0x01,
+        //UCSRB
+        RXCIE	= 0x80,
+        TXCIE	= 0x40,
+        UDRIE	= 0x20,
+        RXEN	= 0x10,
+        TXEN	= 0x08,
+        UCS2Z	= 0x04,
+        RXB8	= 0x02,
+        TXB8	= 0x01,
+        //UCSRC
+        URSEL	= 0x80,
+        UMSEL	= 0x40,
+        UPM1	= 0x20,
+        UPM0	= 0x10,
+        USBS	= 0x08,
+        UCSZ1	= 0x04,
+        UCSZ0	= 0x02,
+        UCPOL	= 0x01
   };
   
-  /* The UCSRC register cannot be read in an atomic operation,
-     therefore we must keep its current value in a variable */
-  unsigned char __ucsrc ; 
+    /* The UCSRC register cannot be read in an atomic operation,
+       therefore we must keep its current value in a variable */
+    static unsigned char __ucsrc ; 
   
 public:  
 
-  AVRMCU_UART() : __ucsrc(0) { 
-    set_baud_rate(B9600);
-    set_data_bits(D8);
-    set_parity(NO_PARITY);
-    set_stop_bits(TWO_STPO_BITS);
-    AVR8::out8(UCSRB,(TXEN|RXEN));
-  }
+    AVRMCU_UART() { /* Default Initialization is up to init */ }
 
-  AVRMCU_UART(Baud_Rate br, Data_Bits db, Parity p, Stop_Bits sb) : __ucsrc(0) { 
-    set_baud_rate(br);
-    set_data_bits(db);
-    set_parity(p);
-    set_stop_bits(sb);
-    AVR8::out8(UCSRB,(TXEN|RXEN));
-  }
+    AVRMCU_UART(Baud_Rate br, Data_Bits db, Parity p, Stop_Bits sb)  { 
+        __ucsrc = 0;
+        set_baud_rate(br);
+        set_data_bits(db);
+        set_parity(p);
+        set_stop_bits(sb);
+	start();
+    }
 
-  ~AVRMCU_UART(){ 
-    AVR8::out8(UCSRB,(!TXEN & !RXEN));
-  }
-  
-  int set_baud_rate(Baud_Rate baudrate){
-    //unsigned long br = (Traits<CPU>::CLOCK / 8 / baudrate) - 1;
-    unsigned long br = (4000000 / 8 / baudrate) - 1;
-    AVR8::out8(UBRRH,!URSEL & (br>>8));
-    AVR8::out8(UBRRL,br);    
-    return 1;
-  }
+    ~AVRMCU_UART(){ 
+	stop();
+    }
+    
+    int set_baud_rate(Baud_Rate baudrate){
+        unsigned long br = (Traits<CPU>::CLOCK / 8 / baudrate) - 1;
+	ubrrh(!URSEL & (br>>8));
+	ubrrl(br);     
+        return 1;
+    }
 
-  int set_data_bits(Data_Bits databits){
-    if (databits & D5)	__ucsrc &= (!UCSZ1 & !UCSZ0);
-    if (databits & D6)	__ucsrc =  ((__ucsrc & !UCSZ1) | UCSZ0);
-    if (databits & D7)	__ucsrc =  ((__ucsrc & !UCSZ0) | UCSZ1);
-    if (databits & D8)	__ucsrc |= (UCSZ1 | UCSZ0);
-    AVR8::out8(UCSRC,URSEL|__ucsrc);
-    return 1;  
-  }
+    int set_data_bits(Data_Bits databits){
+        if (databits & D5)	__ucsrc &= (!UCSZ1 & !UCSZ0);
+        if (databits & D6)	__ucsrc =  ((__ucsrc & !UCSZ1) | UCSZ0);
+        if (databits & D7)	__ucsrc =  ((__ucsrc & !UCSZ0) | UCSZ1);
+        if (databits & D8)	__ucsrc |= (UCSZ1 | UCSZ0);
+	ucsrc(URSEL|__ucsrc);
+        return 1;    
+    }
 
-  int set_stop_bits(Stop_Bits stopbits){
-    if (stopbits & ONE_HALF_STOP_BITS)	return -1; 	// unsupported
-    if (stopbits & ONE_STOP_BIT)	__ucsrc &= !USBS;
-    if (stopbits & TWO_STPO_BITS)	__ucsrc |= USBS;
-    AVR8::out8(UCSRC,URSEL|__ucsrc);
-    return 1;
-  }
+    int set_stop_bits(Stop_Bits stopbits){
+        if (stopbits & ONE_HALF_STOP_BITS)	return -1; 	// unsupported
+        if (stopbits & ONE_STOP_BIT)		__ucsrc &= !USBS;
+        if (stopbits & TWO_STPO_BITS)		__ucsrc |= USBS;
+        ucsrc(URSEL|__ucsrc);	
+        return 1;
+    }
 
-  int set_parity(Parity parity){
-    if (parity & NO_PARITY)   __ucsrc &= (!UPM1 & !UPM0);
-    if (parity & EVEN_PARITY) __ucsrc =  ((__ucsrc & !UPM0) | UPM1);
-    if (parity & ODD_PARITY)  __ucsrc |= (UPM1 | UPM0);
-    AVR8::out8(UCSRC,URSEL|__ucsrc);
-    return 1;
-  }
+    int set_parity(Parity parity){
+        if (parity & NO_PARITY)		__ucsrc &= (!UPM1 & !UPM0);
+        if (parity & EVEN_PARITY) 	__ucsrc =  ((__ucsrc & !UPM0) | UPM1);
+        if (parity & ODD_PARITY)    	__ucsrc |= (UPM1 | UPM0);
+        ucsrc(URSEL|__ucsrc);
+        return 1;
+    }
 
-  int receive_byte(unsigned char *rec_char){
-    if (data_ready() != 1)
-    	return -1;
-    *rec_char = AVR8::in8(UDR);
-    return 1;
-  }
+    int receive_byte(unsigned char *rec_char){
+        if (data_ready() != 1)
+        	return -1;
+        *rec_char = udr();
+        return 1;
+    }
 
-  int send_byte(unsigned char send_char){
-    if (line_empty() != 1) 
+    int send_byte(unsigned char send_char){
+        if (line_empty() != 1) 
 	return -1;
-    AVR8::out8(UDR,send_char);
-    return 1;
-  }
+	udr(send_char);
+        return 1;
+    }
 
-  int line_empty(){
-    if (AVR8::in8(UCSRA) & UDRE)
-    	return 1;
-    return -1;
-  }
+    int line_empty(){
+        if (ucsra() & UDRE)
+            return 1;
+        return -1;
+    }
 
-  int data_ready(){
-    if (AVR8::in8(UCSRA) & RCX)
-    	return 1;
-    return -1;
-  }
-  
-  static int init(System_Info *si); 
+    int data_ready(){
+        if (ucsra() & RCX)
+            return 1;
+        return -1;
+    }
+    
+    static int init(System_Info *si); 
 
+private:
 
+    typedef AVR8::Reg8 Reg8;
+    typedef AVR8::Reg16 Reg16;
+    
+    static void start() {
+	ucsrb(TXEN|RXEN);  
+    }
+    
+    static void stop() {
+	ucsrb(!TXEN & !RXEN);      
+    }
+
+    static Reg8 udr(){
+	return AVR8::in8(UDR);
+    }
+    
+    static void udr(Reg8 value){
+	AVR8::out8(UDR,value);
+    }   
+    
+    static Reg8 ucsra(){
+	return AVR8::in8(UCSRA);
+    }
+    
+    static void ucsra(Reg8 value){
+	AVR8::out8(UCSRA,value);
+    }    
+    
+    static Reg8 ucsrb(){
+	return AVR8::in8(UCSRB);
+    }
+    
+    static void ucsrb(Reg8 value){
+	AVR8::out8(UCSRB,value);
+    }         
+
+    static Reg8 ucsrc(){
+	return AVR8::in8(UCSRC);
+    }
+    
+    static void ucsrc(Reg8 value){
+	AVR8::out8(UCSRC,value);
+    }  
+    
+    // UBRRH and UBRRL are not contiguous
+    static Reg8 ubrrl(){
+	return AVR8::in8(UBRRL);
+    }
+    
+    static void ubrrl(Reg8 value){
+	AVR8::out8(UBRRL,value);
+    }     
+    
+    static Reg8 ubrrh(){
+	return AVR8::in8(UBRRH);
+    }
+    
+    static void ubrrh(Reg8 value){
+	AVR8::out8(UBRRH,value);
+    }     
+    
+    
+   
 
 };
 
