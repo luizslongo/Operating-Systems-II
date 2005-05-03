43f79d5cd6eb36911b7e08e57cba6d95aa1dd60a
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2005-05-03 20:07:09 +0000

    This is just an encapsulation for the different types of event handlers. In summary, to be an event handler, an object just have to implement the operator() (i.e., it must be a function object).

---
+++ include/utility/handler.h
@@ -0,0 +1,35 @@
+// EPOS-- Handler Utility Declarations
+
+#ifndef __handler_h
+#define __handler_h
+
+#include <system/config.h>
+
+__BEGIN_SYS
+
+class Handler
+{
+public:
+    // A handler function
+    typedef void (Function)();
+
+public:
+    Handler() {}
+
+    virtual void operator()() = 0;
+};
+
+class Handler_Function: public Handler
+{
+public:
+    Handler_Function(Function * h) : _handler(h) {}
+
+    void operator()() { _handler(); }
+	
+private:
+    Function * _handler;
+};
+
+__END_SYS
+
+#endif
