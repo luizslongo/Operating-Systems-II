1fe2ed27bbf781a7b406bfe03c6c97ca99b9ac1c
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2005-05-06 18:35:12 +0000

    Queue was specialized for use with smp systems, in which operations are implicitly guarded by spin locks.

--- include/utility/queue.h
+++ include/utility/queue.h
@@ -1,101 +1,53 @@
-// EPOSi-- Queue Utility Declarations
+// EPOS-- Queue Utility Declarations
 
-#ifndef __queue_h
-#define	__queue_h
+// Queue<T, false, false> is a traditional queue, with insertions at the tail
+// and removals either from the head or from specific objects 
 
-#include "list.h"
-
-__BEGIN_SYS
-
-// This is a traditional queue, with insertions at the tail and removals either
-// from the head or from specific objects 
-template <class T>
-class Queue: private List<T>
-{
-private:
-    typedef List<T> List;
-
-public:
-    typedef typename List::Element Element;
-
-public:
-    Queue() {}
-
-    bool empty() const { return List::empty(); }
-    unsigned int size() const { return List::size(); }
-
-    Element * head() { return List::head(); }
-    Element * tail() { return List::tail(); }
-
-    void insert(Element * e) { 	List::insert_tail(e);  }
-
-    Element * remove() { return List::remove_head(); }
-    Element * remove(const T * obj) { return List::remove(obj); }
-
-    Element * search(const T * obj) { return List::search(obj); }
-};
-
-// This is an ordered queue, i.e. objects are inserted in-order based on the 
-// integral value of "order". Note that "order" implies an order, but does not
-// necessarily need to be "the absolute order" in the queue; it could, for
-// instance, be a priority information or a time-out specification.
-// Insertions must be tagged with "order". Removals are just like in the 
-// traditional queue. Elements of Queue and Ordered_Queue may be exchanged.
+// Queue<T, true, false> is an ordered queue, i.e. objects are inserted
+// in-order based on the integral value of "element.rank". Note that "rank"
+// implies an order, but does not necessarily need to be "the absolute order"
+// in the queue; it could, for instance, be a priority information or a 
+// time-out specification. Insertions must first tag "element" with "rank".
+// Removals are just like in the traditional queue. Elements of both Queues
+// may be exchanged.
 // Example: insert(B,7);insert(C,9);insert(A,4)
 // +---+		+---+	+---+	+---+
 // |obj|		| A |-->| B |-->| C |
 // + - +       head -->	+ - +	+ - +	+ - + <-- tail
 // |ord|		| 4 |<--| 7 |<--| 9 |
 // +---+ 		+---+	+---+	+---+
-template <class T>
-class Ordered_Queue: private Ordered_List<T>
-{
-private:
-    typedef Ordered_List<T> List;
 
-public:
-    typedef typename List::Element Element;
-
-public:
-    Ordered_Queue() {}
-
-    bool empty() const { return List::empty(); }
-    unsigned int size() const { return List::size(); }
-
-    Element * head() { return List::head(); }
-    Element * tail() { return List::tail(); }
-
-    void insert(Element * e) { List::insert(e);  }
-    void insert(Element * e, int order) { List::insert(e, order);  }
-
-    Element * remove() { return List::remove_head(); }
-    Element * remove(const T * obj) { return List::remove(obj); }
-
-    Element * search(const T * obj) { return List::search(obj); }
-};
-
-// This is an ordered queue, i.e. objects are inserted in-order based on the 
-// integral value of "order" just like an Ordered_Queue. But differently from
-// that, a Relative_Queue handles "order" as relative offsets. This is very
-// useful for alarm queues. Elements of Relative_Queue cannot be exchanged
-// with elements of Queue or Ordered_Queue.
+// Queue<T, true, true> is an ordered queue, i.e. objects are inserted
+// in-order based on the integral value of "element.rank" just like above.
+// But differently from that, a Relative Queue handles "rank" as relative 
+// offsets. This is very useful for alarm queues. Elements of Relative Queue
+// cannot be exchanged with elements of the other queues described earlier.
 // Example: insert(B,7);insert(C,9);insert(A,4)
 // +---+		+---+	+---+	+---+
 // |obj|		| A |-->| B |-->| C |
 // + - +       head -->	+ - +	+ - +	+ - + <-- tail
 // |ord|		| 4 |<--| 3 |<--| 2 |
 // +---+ 		+---+	+---+	+---+
-template <class T>
-class Relative_Queue: private Relative_List<T>
+
+#ifndef __queue_h
+#define	__queue_h
+
+#include "list.h"
+#include "spin.h"
+
+__BEGIN_SYS
+
+template <typename T, bool ordered, bool relative, bool atomic>
+class Queue_Common: private List<T, ordered, relative>
 {
 private:
-    typedef Relative_List<T> List;
+    typedef List<T, ordered, relative> List;
 
 public:
     typedef typename List::Element Element;
 
 public:
-    Relative_Queue() {}
+    Queue_Common() {}
 
     bool empty() const { return List::empty(); }
     unsigned int size() const { return List::size(); }
@@ -103,13 +55,94 @@ public:
     Element * head() { return List::head(); }
     Element * tail() { return List::tail(); }
 
-    void insert(Element * e, int order) { List::insert(e, order);  }
+    void insert(Element * e) { List::insert(e); }
 
-    Element * remove() { return List::remove_head(); }
+    Element * remove() { return List::remove(); }
     Element * remove(const T * obj) { return List::remove(obj); }
 
     Element * search(const T * obj) { return List::search(obj); }
 };
+
+template <typename T, bool ordered, bool relative>
+class Queue_Common<T, ordered, relative, true>
+    : private List<T, ordered, relative>
+{
+private:
+    typedef List<T, ordered, relative> List;
+
+public:
+    typedef typename List::Element Element;
+
+public:
+    Queue_Common() {}
+
+    bool empty() {
+	_lock.acquire(); 
+	bool tmp = List::empty();
+	_lock.release();
+	return tmp;
+    }
+    unsigned int size() {
+	_lock.acquire(); 
+	unsigned int tmp = List::size();
+	_lock.release();
+	return tmp;
+    }
+
+    Element * head() { 
+	_lock.acquire(); 
+	Element * tmp = List::head();
+	_lock.release();
+	return tmp;
+    }
+    Element * tail() { 
+	_lock.acquire(); 
+	Element * tmp = List::tail();
+	_lock.release();
+	return tmp;
+    }
+
+    void insert(Element * e) { 
+	_lock.acquire(); 
+	List::insert(e);
+	_lock.release();
+    }
+
+    Element * remove() { 
+	_lock.acquire(); 
+	Element * tmp = List::remove();
+	_lock.release();
+	return tmp;
+    }
+    Element * remove(const T * obj) {
+	_lock.acquire(); 
+	Element * tmp = List::remove(obj); 
+	_lock.release();
+	return tmp;
+    }
+
+    Element * search(const T * obj) {
+	_lock.acquire(); 
+	Element * tmp = List::search(obj);
+	_lock.release();
+	return tmp;
+    }
+
+private:
+    Spin _lock;
+};
+
+template <typename T, bool atomic = true>
+class Queue: public Queue_Common<T, false, false, atomic>
+{};
+
+template <typename T, bool atomic = true>
+class Ordered_Queue: public Queue_Common<T, true, false, atomic>
+{};
+
+template <typename T, bool atomic = true>
+class Relative_Queue: public Queue_Common<T, true, true, atomic>
+{};
 
 __END_SYS
  
