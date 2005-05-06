b2e355bc2ca847a8450993206341010ed473ea14
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2005-05-06 18:42:35 +0000

    insert() and remove() added to basic list so queue can easilly vary from ordered to unordered. template List can now be used to build any kind of list. rank for ordered lists now comes exclusivelly from the corresponding Element attribute (insert(Element, rank) was removed).

--- src/utility/list_test.cc
+++ src/utility/list_test.cc
@@ -13,14 +13,14 @@ struct Integer1 {
 };
 
 struct Integer2 {
-    Integer2(int _i) : i(_i), e(this) {}
+    Integer2(int _i, int _r) : i(_i), e(this, _r) {}
     
     int i;
     Ordered_List<Integer2>::Element e;
 };
 
 struct Integer3 {
-    Integer3(int _i) : i(_i), e(this) {}
+    Integer3(int _i, int _r) : i(_i), e(this, _r) {}
     
     int i;
     Relative_List<Integer3>::Element e;
@@ -38,84 +38,87 @@ int main()
 
     cout << "List Utility Test\n";
 
-    List<Integer1> l1;
+    cout << "\nThis is a list of integers:\n";
     Integer1 i1(1), i2(2), i3(3), i4(4);
-    cout << "This is a list of integers:\n";
-    cout << "Inserting the integer " << i1.i << " into the list.\n";
+    List<Integer1> l1;
+    cout << "Inserting the integer " << i1.i << "\n";
     l1.insert_tail(&i1.e);
-    cout << "Inserting the integer " << i2.i << " into the list.\n";
+    cout << "Inserting the integer " << i2.i << "\n";
     l1.insert_tail(&i2.e);
-    cout << "Inserting the integer " << i3.i << " into the list.\n";
+    cout << "Inserting the integer " << i3.i << "\n";
     l1.insert_tail(&i3.e);
-    cout << "Inserting the integer " << i4.i << " into the list.\n";
+    cout << "Inserting the integer " << i4.i << "\n";
     l1.insert_tail(&i4.e);
     cout << "The list has now " << l1.size() << " elements.\n";
-    cout << "Removing the element whose value is " << i2.i << "(" 
-	 << l1.remove(&i2)->object()->i << ")" << " from the list.\n";
-    cout << "Removing the list's head (" << l1.remove_head()->object()->i 
-	 << ")" << ".\n";
-    cout << "Removing the element whose value is " << i4.i << "(" 
-	 << l1.remove(&i4)->object()->i << ")" << " from the list.\n";
-    cout << "Removing the list's tail (" << l1.remove_tail()->object()->i
-	 << ")" << ".\n";
+    cout << "Removing the element whose value is " << i2.i << " => " 
+	 << l1.remove(&i2)->object()->i << "\n";
+    cout << "Removing the list's head => " << l1.remove_head()->object()->i 
+	 << "\n";
+    cout << "Removing the element whose value is " << i4.i << " => " 
+	 << l1.remove(&i4)->object()->i << "\n";
+    cout << "Removing the list's head =>" << l1.remove_head()->object()->i
+	 << "\n";
     cout << "The list has now " << l1.size() << " elements.\n";
 
-    Ordered_List<Integer2> l2;
-    Integer2 j1(1), j2(2), j3(3), j4(4);
+
     cout << "This is an ordered list of integers:\n";
+    Integer2 j1(1, 2), j2(2, 3), j3(3, 4), j4(4, 1);
+    Ordered_List<Integer2> l2;
     cout << "Inserting the integer " << j1.i 
-	 << " into the list with order 2.\n";
-    l2.insert(&j1.e, 2);
+	 << " with rank " << j1.e.rank() << ".\n";
+    l2.insert(&j1.e);
     cout << "Inserting the integer " << j2.i
-	 << " into the list with order 3.\n";
-    l2.insert(&j2.e, 3);
+	 << " with rank " << j2.e.rank() << ".\n";
+    l2.insert(&j2.e);
     cout << "Inserting the integer " << j3.i
-	 << " into the list with order 4.\n";
-    l2.insert(&j3.e, 4);
+	 << " with rank " << j3.e.rank() << ".\n";
+    l2.insert(&j3.e);
     cout << "Inserting the integer " << j4.i
-	 << " into the list with order 1.\n";
-    l2.insert(&j4.e, 1);
+	 << " with rank " << j4.e.rank() << ".\n";
+    l2.insert(&j4.e);
     cout << "The list has now " << l2.size() << " elements.\n";
-    cout << "Removing the element whose value is " << j2.i << "(" 
-	 << l2.remove(&j2)->object()->i << ")" << " from the list.\n";
-    cout << "Removing the list's head (" << l2.remove_head()->object()->i 
-	 << ")" << ".\n";
-    cout << "Removing the list's head (" << l2.remove_head()->object()->i
-	 << ")" << ".\n";
-    cout << "Removing the list's tail (" << l2.remove_tail()->object()->i 
-	 << ")" << " from the list.\n";
+    cout << "Removing the element whose value is " << j2.i << " => " 
+	 << l2.remove(&j2)->object()->i << "\n";
+    cout << "Removing the list's head => " << l2.remove()->object()->i 
+	 << "\n";
+    cout << "Removing the list's head => " << l2.remove()->object()->i
+	 << "\n";
+    cout << "Removing the list's head => " << l2.remove()->object()->i 
+	 << "\n";
     cout << "The list has now " << l2.size() << " elements.\n";
 
-    Relative_List<Integer3> l3;
-    Integer3 k1(1), k2(2), k3(3), k4(4);
+
     cout << "This is an list of integers with relative ordering:\n";
+    Integer3 k1(1, 2), k2(2, 3), k3(3, 4), k4(4, 1);
+    Relative_List<Integer3> l3;
     cout << "Inserting the integer " << k1.i 
-	 << " into the list with order 2.\n";
-    l3.insert(&k1.e, 2);
+	 << " with relative order " << k1.e.rank() << ".\n";
+    l3.insert(&k1.e);
     cout << "Inserting the integer " << k2.i 
-	 << " into the list with order 3.\n";
-    l3.insert(&k2.e, 3);
+	 << " with relative order " << k2.e.rank() << ".\n";
+    l3.insert(&k2.e);
     cout << "Inserting the integer " << k3.i
-	 << " into the list with order 4.\n";
-    l3.insert(&k3.e, 4);
+	 << " with relative order " << k3.e.rank() << ".\n";
+    l3.insert(&k3.e);
     cout << "Inserting the integer " << k4.i
-	 << " into the list with order 1.\n";
-    l3.insert(&k4.e, 1);
+	 << " with relative order " << k4.e.rank() << ".\n";
+    l3.insert(&k4.e);
     cout << "The list has now " << l3.size() << " elements.\n";
-    cout << "Removing the element whose value is " << k2.i << "(" 
-	 << l3.remove(&k2)->object()->i << ")" << " from the list.\n";
-    cout << "Removing the list's head (" << l3.remove_head()->object()->i
-	 << ")" << ".\n";
-    cout << "Removing the list's head (" << l3.remove_head()->object()->i
-	 << ")" << ".\n";
-    cout << "Removing the list's tail (" << l3.remove_tail()->object()->i
-	 << ")" << " from the list.\n";
+    cout << "Removing the element whose value is " << j2.i << " => " 
+	 << l3.remove(&k2)->object()->i << "\n";
+    cout << "Removing the list's head => " << l3.remove()->object()->i
+	 << "\n";
+    cout << "Removing the list's head => " << l3.remove()->object()->i
+	 << "\n";
+    cout << "Removing the list's head => " << l3.remove()->object()->i
+	 << "\n";
     cout << "The list has now " << l3.size() << " elements.\n";
 
+
+    cout << "This is a grouping list of integers:\n";
     Grouping_List<int> l4;
     Grouping_List<int>::Element m1((int *)0, 2), m2((int *)16, 2), m3((int *)8, 2), m4((int *)24, 2);
     Grouping_List<int>::Element * d1, * d2;
-    cout << "This is a grouping list of integers:\n";
     cout << "Inserting the integer group beginning with " << m1.object()
 	 << " and spanning " << m1.size() << " elements into the list.\n";
     l4.insert_merging(&m1, &d1, &d2);
--- src/utility/queue_test.cc
+++ src/utility/queue_test.cc
@@ -13,14 +13,14 @@ struct Integer1 {
 };
 
 struct Integer2 {
-    Integer2(int _i) : i(_i), e(this) {}
+    Integer2(int _i, int _r) : i(_i), e(this, _r) {}
     
     int i;
     Ordered_Queue<Integer2>::Element e;
 };
 
 struct Integer3 {
-    Integer3(int _i) : i(_i), e(this) {}
+    Integer3(int _i, int _r) : i(_i), e(this, _r) {}
     
     int i;
     Relative_Queue<Integer3>::Element e;
@@ -32,79 +32,80 @@ int main()
 
     cout << "Queue Utility Test\n";
 
+    cout << "\nThis is an integer queue:\n";
     Integer1 i1(1), i2(2), i3(3), i4(4);
-    Integer2 j1(1), j2(2), j3(3), j4(4);
-    Integer3 k1(1), k2(2), k3(3), k4(4);
-    
-    cout << "This is an integer queue:\n";
     Queue<Integer1> q1;
-    cout << "Inserting the integer " << i1.i << " into the queue.\n";
+    cout << "Inserting the integer " << i1.i << "\n";
     q1.insert(&i1.e);
-    cout << "Inserting the integer " << i2.i << " into the queue.\n";
+    cout << "Inserting the integer " << i2.i << "\n";
     q1.insert(&i2.e);
-    cout << "Inserting the integer " << i3.i << " into the queue.\n";
+    cout << "Inserting the integer " << i3.i << "\n";
     q1.insert(&i3.e);
-    cout << "Inserting the integer " << i4.i << " into the queue.\n";
+    cout << "Inserting the integer " << i4.i << "\n";
     q1.insert(&i4.e);
     cout << "The queue has now " << q1.size() << " elements.\n";
-    cout << "Removing the element whose value is " << i2.i << "(" 
-	 << q1.remove(&i2)->object()->i << ")" << " from the queue.\n";
-    cout << "Removing the queue's head (" << q1.remove()->object()->i 
-	 << ")" << " from the queue.\n";
-    cout << "Removing the element whose value is " << i4.i << "(" 
-	 << q1.remove(&i4)->object()->i << ")" << " from the queue.\n";
-    cout << "Removing the queue's head (" << q1.remove()->object()->i
-	 << ")" << " from the queue.\n";
+    cout << "Removing the element whose value is " << i2.i << " => " 
+	 << q1.remove(&i2)->object()->i << "\n";
+    cout << "Removing the queue's head => " << q1.remove()->object()->i 
+	 << "\n";
+    cout << "Removing the element whose value is " << i4.i << " => " 
+	 << q1.remove(&i4)->object()->i << "\n";
+    cout << "Removing the queue's head =>" << q1.remove()->object()->i
+	 << "\n";
     cout << "The queue has now " << q1.size() << " elements.\n";
 
-    cout << "This is an ordered integer queue:\n";
+
+    cout << "\nThis is an ordered integer queue:\n";
+    Integer2 j1(1, 2), j2(2, 3), j3(3, 4), j4(4, 1);
     Ordered_Queue<Integer2> q2;
     cout << "Inserting the integer " << j1.i 
-	 << " into the queue with order 2.\n";
-    q2.insert(&j1.e, 2);
+	 << " with rank " << j1.e.rank() << ".\n";
+    q2.insert(&j1.e);
     cout << "Inserting the integer " << j2.i
-	 << " into the queue with order 3.\n";
-    q2.insert(&j2.e, 3);
+	 << " with rank " << j2.e.rank() << ".\n";
+    q2.insert(&j2.e);
     cout << "Inserting the integer " << j3.i
-	 << " into the queue with order 4.\n";
-    q2.insert(&j3.e, 4);
+	 << " with rank " << j3.e.rank() << ".\n";
+    q2.insert(&j3.e);
     cout << "Inserting the integer " << j4.i
-	 << " into the queue with order 1.\n";
-    q2.insert(&j4.e, 1);
+	 << " with rank " << j4.e.rank() << ".\n";
+    q2.insert(&j4.e);
     cout << "The queue has now " << q2.size() << " elements.\n";
-    cout << "Removing the element whose value is " << j2.i << "(" 
-	 << q2.remove(&j2)->object()->i << ")" << " from the queue.\n";
-    cout << "Removing the queue's head (" << q2.remove()->object()->i 
-	 << ")" << " from the queue.\n";
-    cout << "Removing the queue's head (" << q2.remove()->object()->i
-	 << ")" << " from the queue.\n";
-    cout << "Removing the queue's head (" << q2.remove()->object()->i 
-	 << ")" << " from the queue.\n";
+    cout << "Removing the element whose value is " << j2.i << " => " 
+	 << q2.remove(&j2)->object()->i << "\n";
+    cout << "Removing the queue's head => " << q2.remove()->object()->i 
+	 << "\n";
+    cout << "Removing the queue's head => " << q2.remove()->object()->i
+	 << "\n";
+    cout << "Removing the queue's head => " << q2.remove()->object()->i 
+	 << "\n";
     cout << "The queue has now " << q2.size() << " elements.\n";
 
-    cout << "This is an integer queue with relative ordering:\n";
+
+    cout << "\nThis is an integer queue with relative ordering:\n";
+    Integer3 k1(1, 2), k2(2, 3), k3(3, 4), k4(4, 1);
     Relative_Queue<Integer3> q3;
-    cout << "Inserting the integer " << j1.i 
-	 << " into the queue with relative order 2.\n";
-    q3.insert(&k1.e, 2);
-    cout << "Inserting the integer " << j2.i 
-	 << " into the queue with relative order 3.\n";
-    q3.insert(&k2.e, 3);
-    cout << "Inserting the integer " << j3.i
-	 << " into the queue with relative order 4.\n";
-    q3.insert(&k3.e, 4);
-    cout << "Inserting the integer " << j4.i
-	 << " into the queue with relative order 1.\n";
-    q3.insert(&k4.e, 1);
+    cout << "Inserting the integer " << k1.i 
+	 << " with relative order " << k1.e.rank() << ".\n";
+    q3.insert(&k1.e);
+    cout << "Inserting the integer " << k2.i 
+	 << " with relative order " << k2.e.rank() << ".\n";
+    q3.insert(&k2.e);
+    cout << "Inserting the integer " << k3.i
+	 << " with relative order " << k3.e.rank() << ".\n";
+    q3.insert(&k3.e);
+    cout << "Inserting the integer " << k4.i
+	 << " with relative order " << k4.e.rank() << ".\n";
+    q3.insert(&k4.e);
     cout << "The queue has now " << q3.size() << " elements.\n";
-    cout << "Removing the element whose value is " << j2.i << "(" 
-	 << q3.remove(&k2)->object()->i << ")" << " from the queue.\n";
-    cout << "Removing the queue's head (" << q3.remove()->object()->i
-	 << ")" << " from the queue.\n";
-    cout << "Removing the queue's head (" << q3.remove()->object()->i
-	 << ")" << " from the queue.\n";
-    cout << "Removing the queue's head (" << q3.remove()->object()->i
-	 << ")" << " from the queue.\n";
+    cout << "Removing the element whose value is " << j2.i << " => " 
+	 << q3.remove(&k2)->object()->i << "\n";
+    cout << "Removing the queue's head => " << q3.remove()->object()->i
+	 << "\n";
+    cout << "Removing the queue's head => " << q3.remove()->object()->i
+	 << "\n";
+    cout << "Removing the queue's head => " << q3.remove()->object()->i
+	 << "\n";
     cout << "The queue has now " << q3.size() << " elements.\n";
 
     return 0;
