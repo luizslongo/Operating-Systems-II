5b938d4198067c8e395eb80cd1ab3228f8b56140
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2005-05-06 04:01:37 +0000

    Traits cleaned up. busy_waiting in Thread was replaced by idle_waiting in Synchronizer, thus enabling different policies for differente modules.

--- include/traits.h
+++ include/traits.h
@@ -11,23 +11,20 @@ struct Traits
     static const bool debugged = false;
 };
 
-class Null_Debug;
-template <> struct Traits<Null_Debug>
-{
-    static const bool error   = false;
-    static const bool warning = false;
-    static const bool info    = false;
-    static const bool trace   = false;
-};
-
-class Debug;
-template <> struct Traits<Debug>: public Traits<Null_Debug>
+// Utilities
+template <> struct Traits<Debug>
 {
     static const bool error   = true;
     static const bool warning = true;
     static const bool info    = true;
     static const bool trace   = true;
 };
+
+template <> struct Traits<Heap>: public Traits<void>
+{
+    static const bool debugged = false;
+};
+
 
 // System parts
 template <> struct Traits<Boot>: public Traits<void>
@@ -46,24 +43,12 @@ template <> struct Traits<System>: public Traits<void>
 {
 };
 
-template <> struct Traits<Framework>: public Traits<void>
-{
-};
 
 // Mediators
 template <> struct Traits<IA32>: public Traits<void>
 {
-  static const int CLOCK = 1000000000;
-};
-
-template <> struct Traits<H8>: public Traits<void>
-{
-  static const long CLOCK = 16000000;
-};
-
-template <> struct Traits<PPC32>: public Traits<void>
-{
-  static const int CLOCK = 200000000;
+//    static const int CLOCK = 2003402000;
+    static const int CLOCK = 797886000;
 };
 
 template <> struct Traits<IA32_MMU>: public Traits<void>
@@ -71,17 +56,7 @@ template <> struct Traits<IA32_MMU>: public Traits<void>
     static const bool fast_log_to_phy = false;
 };
 
-template <> struct Traits<H8_MMU>: public Traits<void>
-{
-    static const bool fast_log_to_phy = false;
-};
-
 template <> struct Traits<AVR8_MMU>: public Traits<void>
-{
-    static const bool fast_log_to_phy = false;
-};
-
-template <> struct Traits<PPC32_MMU>: public Traits<void>
 {
     static const bool fast_log_to_phy = false;
 };
@@ -114,49 +89,10 @@ template <> struct Traits<PC>: public Traits<void>
     static const unsigned int APPLICATION_HEAP_SIZE = 100 * 4096;
 };
 
-template <> struct Traits<RCX>: public Traits<void>
-{
-    static const unsigned int BOOT_IMAGE_ADDR = 0x8080;	
-    static const int INT_BASE = 0x0A;
-
-    static const unsigned int SYSTEM_STACK_SIZE = 512;
-    static const unsigned int SYSTEM_HEAP_SIZE = 512;
-    static const unsigned int APPLICATION_STACK_SIZE = 512;
-    static const unsigned int APPLICATION_HEAP_SIZE = 512;
-};
-
-template <> struct Traits<Khomp>: public Traits<void>
-{
-    static const unsigned int BOOT_IMAGE_ADDR = 0x0180;	
-    static const int INT_BASE = 0x00;
-
-    static const unsigned int SYSTEM_HEAP_SIZE = 4096;
-    static const unsigned int SYSTEM_STACK_SIZE = 4096;
-    static const unsigned int APPLICATION_STACK_SIZE = 4096;
-    static const unsigned int APPLICATION_HEAP_SIZE = 4096;
-};
-
-template <> struct Traits<AVRMCU>: public Traits<void>
-{
-    static const unsigned int BOOT_IMAGE_ADDR = 0x0000;
-    static const int INT_BASE = 0x00; 
-    static const int INT_VEC_SIZE = 24; 
-
-    static const unsigned int SYSTEM_STACK_SIZE = 64;
-    static const unsigned int SYSTEM_HEAP_SIZE = 64;
-    static const unsigned int APPLICATION_STACK_SIZE = 64;
-    static const unsigned int APPLICATION_HEAP_SIZE = 128;
-};
-
 template <> struct Traits<PC_PCI>: public Traits<void>
 {
     static const int MAX_BUS = 0;
     static const int MAX_DEV_FN = 0xff;
-};
-
-template <> struct Traits<AVRMCU_Timer>: public Traits<void>
-{
-    static const int FREQUENCY = 40; // Hz
 };
 
 template <> struct Traits<PC_Timer>: public Traits<void>
@@ -172,15 +108,23 @@ template <> struct Traits<PC_Display>: public Traits<void>
     static const unsigned int FRAME_BUFFER_ADDRESS = 0xb8000;
 };
 
-template <> struct Traits<RCX_Display>: public Traits<void>
+template <> struct Traits<AVRMCU>: public Traits<void>
 {
-    static const int COLUMNS = 5;
-    static const int LINES = 1;
-    static const int TAB_SIZE = 8;
-    static const unsigned int FRAME_BUFFER_ADDRESS = 0xef43;
-    static const int DIGIT_SEGMENTS = 7;
-    static const int SCROLL_DELAY = 2; /* (1)=FAST (2)=MEDIUM (3)=SLOW */
+    static const unsigned int BOOT_IMAGE_ADDR = 0x0000;
+    static const int INT_BASE = 0x00; 
+    static const int INT_VEC_SIZE = 24; 
+
+    static const unsigned int SYSTEM_STACK_SIZE = 64;
+    static const unsigned int SYSTEM_HEAP_SIZE = 64;
+    static const unsigned int APPLICATION_STACK_SIZE = 64;
+    static const unsigned int APPLICATION_HEAP_SIZE = 128;
 };
+
+template <> struct Traits<AVRMCU_Timer>: public Traits<void>
+{
+    static const int FREQUENCY = 40; // Hz
+};
+
 
 // Abstractions
 template <> struct Traits<Alarm>: public Traits<void>
@@ -190,11 +134,17 @@ template <> struct Traits<Alarm>: public Traits<void>
 
 template <> struct Traits<Thread>: public Traits<void>
 {
-    static const bool busy_waiting = false;
     static const bool active_scheduler = true;
-    static const unsigned int quantum = 500000; // us
+    static const unsigned int quantum = 10000; // us
     static const bool preemptive = true;
 };
+
+template <> struct Traits<Synchronizer>: public Traits<void>
+{
+    static const bool idle_waiting = true;
+    static const bool strict_ordering = true;
+};
+
 
 __END_SYS
 
