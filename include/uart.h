3babd1a59416e13c20e66b9309a770aefbbe4a50
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2005-06-27 14:07:22 +0000

    Config parameters are no longer ENUM, but correspondiong ints.

--- include/uart.h
+++ include/uart.h
@@ -11,35 +11,6 @@ class UART_Common
 {
 protected:
     UART_Common() {}
-
-public:
-    enum Baud_Rate {
-      B1200 = 1200,
-      B2400 = 2400,
-      B4800 = 4800,
-      B9600 = 9600,
-      B19200 = 19200,
-      B28800 = 28800,
-      B33600 = 33600,
-      B57600 = 57600,
-      B115200 = 115200
-    };
-    enum Data_Bits {
-      D5 = 5,
-      D6 = 6,
-      D7 = 7,
-      D8 = 8
-    };
-    enum Parity {
-      NO_PARITY = 0,
-      EVEN_PARITY = 1,
-      ODD_PARITY = 2
-    };
-    enum Stop_Bits {
-      ONE_STOP_BIT = 1,
-      ONE_HALF_STOP_BITS = 2,
-      TWO_STPO_BITS = 3
-    };
 };
 
 __END_SYS
