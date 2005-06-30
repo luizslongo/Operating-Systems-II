f746b347661d0c8b54d71cc3910439f3b049e93d
lucas <lucas@lisha.ufsc.br>
2005-06-30 14:01:25 +0000

    Display for the Mica2 extends the common serial display now.

--- include/mach/mica2/display.h
+++ include/mach/mica2/display.h
@@ -4,35 +4,18 @@
 #define __mica2_display_h
 
 #include <display.h>
+#include "../common/serial_display.h"
 
 __BEGIN_SYS
 
-class Mica2_Display: public Display_Common
+class Mica2_Display: public Serial_Display
 {
 private:
     typedef Traits<Mica2_Display> Traits;
- //   static const Type_Id TYPE = Type<Mica2_Display>::TYPE;
-
-    // Mica2_Display private imports, types and constants
+    static const Type_Id TYPE = Type<Mica2_Display>::TYPE;
 
 public:
-    Mica2_Display(){};
-    ~Mica2_Display(){};
-    void clear(){};
-    void putc(char c){};
-    void puts(const char * s){};
-    int lines(){ return 0; };
-    int columns(){ return 0; };
-    void position(int * line, int * column){};
-    void position(int line, int column){};
-
     static int init(System_Info *si);
-
-private:
-    // Mica2_Display implementation methods
-
-private:
-    // Mica2_Display attributes
     
 };
 
