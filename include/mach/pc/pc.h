56f52cc8cf05b8d233c01ee706dfbcf8a951e9bc
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2005-07-06 03:47:03 +0000

    The old handler_wrapper was split in two: isr for hardware interrupts and fsr for exceptions (faults).

--- include/mach/pc/pc.h
+++ include/mach/pc/pc.h
@@ -3,7 +3,6 @@
 #ifndef __pc_h
 #define __pc_h
 
-#include <utility/handler.h>
 #include <machine.h>
 #include <arch/ia32/cpu.h>
 #include <arch/ia32/mmu.h>
@@ -26,6 +25,10 @@ private:
 
     static const int SYSCALL_INT = Traits::SYSCALL_INT;
     
+    typedef IA32::Reg32 Reg32;
+    typedef IA32::ISR ISR;
+    typedef IA32::FSR FSR;
+
 public:
     // Interrupts
     enum {
@@ -37,49 +40,32 @@ public:
 public:
     PC() {}
   
-    static Handler::Function * int_handler(int i) {
-	IA32::IDT_Entry * idt = (IA32::IDT_Entry *)Memory_Map<PC>::INT_VEC;
+    static ISR * int_handler(int i) {
+	IA32::IDT_Entry * idt = 
+	    reinterpret_cast<IA32::IDT_Entry *>(Memory_Map<PC>::INT_VEC);
 	if(i < IA32::IDT_ENTRIES)
-	    return (Handler::Function *)idt[i].offset();
+	    return reinterpret_cast<ISR *>(idt[i].offset());
     }
-    static void int_handler(int i, Handler::Function * h) {
-	IA32::IDT_Entry * idt = (IA32::IDT_Entry *)Memory_Map<PC>::INT_VEC;
+    static void int_handler(int i, ISR * r) {
+	IA32::IDT_Entry * idt = 
+	    reinterpret_cast<IA32::IDT_Entry *>(Memory_Map<PC>::INT_VEC);
 	if(i < IA32::IDT_ENTRIES)
 	    idt[i] = IA32::IDT_Entry(IA32::GDT_SYS_CODE,
-				     (IA32::Reg32)h,
+				     reinterpret_cast<Reg32>(r),
 				     IA32::SEG_IDT_ENTRY);
     }
-    template <Handler::Function * h> static void handler_wrapper() {
-	ASM("	cli							\n"
-	    "	pushl	%eax						\n"
-	    "	pushl	%ecx						\n"
-	    "	pushl	%edx						\n"
-	    "	pushl	%ebx						\n"
-	    "	pushl	%esi						\n"
-	    "	pushl	%edi						\n");
-	ASM("	call  *%0						\n"
-	    "	movb  $0x20, %%al					\n"
-	    "	outb  %%al, $0x20					\n"
-	    "	popl  %%edi						\n"
-	    "	popl  %%esi						\n"
-	    "	popl  %%ebx						\n"
-	    "	popl  %%edx						\n"
-	    "	popl  %%ecx						\n"
-	    "	popl  %%eax						\n"
-	    "	leave			# this may change with GCC	\n"
-	    "	sti							\n"
-	    "	iret							\n"
-	    :	    : "r"(h));
-    }
 
-    static void panic() {
-	CPU::int_disable(); 
-	Display display; 
-	display.position(0, 75); 
-	display.puts("PANIC"); 
-	CPU::halt(); 
-    }
-    
+    static void panic();
+    static void int_not();
+    static void exc_not(Reg32 error, Reg32 eip, Reg32 cs, Reg32 eflags);
+    static void exc_pf(Reg32 error, Reg32 eip, Reg32 cs, Reg32 eflags);
+    static void exc_gpf(Reg32 error, Reg32 eip, Reg32 cs, Reg32 eflags);
+    static void exc_fpu(Reg32 error, Reg32 eip, Reg32 cs, Reg32 eflags);
+
+    template <ISR * r> static void isr_wrapper();
+    template <FSR * r> static void fsr_wrapper();
+
+
     static int init(System_Info * si);
 
 public:
@@ -90,6 +76,28 @@ public:
     PC_PCI pci;
     PC_Display display;
 };
+
+// These wrappers must be defined out of class to prevent inlining
+// Units using (instantiating) them must be compiled with -fomit-frame-pointer
+template <PC::ISR * r>
+void PC::isr_wrapper() 
+{
+    // Flags are implicitly pushed by the CPU's interrupt dispatcher
+    // Interrupts are kept enabled (driver can disable it if necessary)
+    ASM("pushal");
+    ASM("call *%0" : : "r"(r));
+    // Send EOI to the PIC (what about the cascaded PIC?
+    ASM("movb	$0x20, %al\n" 
+  	"outb	%al, $0x20\n"); 
+    ASM("popal");
+    ASM("iret");
+}
+
+template <PC::FSR * r>
+void PC::fsr_wrapper() {
+    ASM("call *%0" : : "r"(r));
+    ASM("iret");
+}
 
 typedef PC Machine;
 
