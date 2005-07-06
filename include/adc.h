15554265c6e33f3604be722fa35b8525f82a0440
lucas <lucas@lisha.ufsc.br>
2005-07-06 13:08:02 +0000

    makeup

--- include/adc.h
+++ include/adc.h
@@ -1,11 +1,5 @@
 // EPOS-- ADC Mediator Common Package
 
-// This work is licensed under the Creative Commons 
-// Attribution-NonCommercial-NoDerivs License. To view a copy of this license, 
-// visit http://creativecommons.org/licenses/by-nc-nd/2.0/ or send a letter to 
-// Creative Commons, 559 Nathan Abbott Way, Stanford, California 94305, USA.
-
-
 #ifndef __adc_h
 #define __adc_h
 
@@ -16,31 +10,31 @@ __BEGIN_SYS
 class ADC_Common
 {
 
- public:
+public:
 
-  typedef unsigned long Hertz;
+    typedef unsigned long Hertz;
 
-  enum Channel {
-    SINGLE_ENDED_ADC0 = 0x00,
-    SINGLE_ENDED_ADC1 = 0x01,
-    SINGLE_ENDED_ADC2 = 0x02,
-    SINGLE_ENDED_ADC3 = 0x03,
-    SINGLE_ENDED_ADC4 = 0x04,
-    SINGLE_ENDED_ADC5 = 0x05,
-    SINGLE_ENDED_ADC6 = 0x06,
-    SINGLE_ENDED_ADC7 = 0x07
-  };
+    enum Channel {
+	SINGLE_ENDED_ADC0 = 0,
+	SINGLE_ENDED_ADC1 = 1,
+	SINGLE_ENDED_ADC2 = 2,
+	SINGLE_ENDED_ADC3 = 3,
+	SINGLE_ENDED_ADC4 = 4,
+	SINGLE_ENDED_ADC5 = 5,
+	SINGLE_ENDED_ADC6 = 6,
+	SINGLE_ENDED_ADC7 = 7
+    };
 
-  enum Reference {
-    SYSTEM_REFERENCE,
-    EXTERNAL_REFERENCE,
-    INTERNAL_REFERENCE
-  };
+    enum Reference {
+	SYSTEM_REF   = 0,
+	EXTERNAL_REF = 1,
+	INTERNAL_REF = 3
+    };
 
-  enum Trigger {
-    FREE_RUNNING_MODE,
-    SINGLE_CONVERSION_MODE
-  };
+    enum Trigger {
+	SINGLE_CONVERSION_MODE = 0,
+	FREE_RUNNING_MODE      = 1,
+    };
 
 };
 
