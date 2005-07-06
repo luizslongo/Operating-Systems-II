ed6a6dc42accc62da218a93db7945146b3e8da90
lucas <lucas@lisha.ufsc.br>
2005-07-06 13:08:50 +0000

    Adjusting to new implementation

--- src/mach/avrmcu/adc_init.cc
+++ src/mach/avrmcu/adc_init.cc
@@ -9,11 +9,6 @@ int AVRMCU_ADC::init(System_Info * si)
 {
     db<AVRMCU_ADC>(TRC) << "AT90S_ADC::init()\n";
     
-    // Defaults: Channel 0, System Reference, Free Running Mode, CLOCK/128
-    admux(0x00);			// Channel 0, AREF
-    sfior(ADATE);			// Free Running Mode
-    adcsra(ADPS2 | ADPS1 | ADPS0);	// CLOCK/128	
-
     return 0;
 }
 
--- src/mach/mica2/adc_init.cc
+++ src/mach/mica2/adc_init.cc
@@ -9,11 +9,6 @@ int Mica2_ADC::init(System_Info * si)
 {
     db<Mica2_ADC>(TRC) << "Mica2_ADC::init()\n";
     
-    // Defaults: Channel 0, System Reference, Free Running Mode, CLOCK/128
-    admux(0x00);			// Channel 0, AREF
-    sfior(ADATE);			// Free Running Mode
-    adcsra(ADPS2 | ADPS1 | ADPS0);	// CLOCK/128	
-
     return 0;
 }
 
