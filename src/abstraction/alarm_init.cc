47a541a726ef8328aa52125ca4bd766e65f76cd9
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2005-07-06 03:43:13 +0000

    In order to control where the isr_wrapper template is instantiated, and also to compile the ISR with -fomit-frame-pointer without disturbing other functions, the timer_isr has been moved to a privite file where an explicit template instantiation is done. Preventive instantiation declaretais were added to alarm_init.

--- src/abstraction/alarm_init.cc
+++ src/abstraction/alarm_init.cc
@@ -5,14 +5,17 @@
 
 __BEGIN_SYS
 
+// Prevent implicit template instantiations
+extern template void Machine::isr_wrapper<Alarm::timer_isr>();
+
 int Alarm::init(System_Info * si)
 {
     db<Alarm>(TRC) << "Alarm::init()\n";
 
     CPU::int_disable();
 
-    Machine::int_handler(Machine::INT_TIMER,
-			 &Machine::handler_wrapper<timer_handler>);
+    Machine::int_handler(Machine::INT_TIMER, 
+			 &Machine::isr_wrapper<timer_isr>);
 
     _timer.frequency(FREQUENCY);
     _timer.enable();
