dae21b23278c5c7df548669ed22b270450c461d7
lucas <lucas@lisha.ufsc.br>
2005-07-11 13:02:13 +0000

    Including sensors.

---
+++ include/sensor.h
@@ -0,0 +1,22 @@
+// EPOS-- Sensor Mediator Common Package
+
+#ifndef __sensor_h
+#define __sensor_h
+
+#include <system/config.h>
+
+__BEGIN_SYS
+
+class Sensor_Common
+{
+
+public:
+
+
+};
+
+__END_SYS
+
+#include __HEADER_MACH(sensor)
+
+#endif
