00ef42778f497650f9a8686e834925f692057b28
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2005-11-01 23:17:38 +0000

    Just style!

--- app/pc_app.cc
+++ app/pc_app.cc
@@ -18,19 +18,21 @@ int main()
     OStream cout;
 
     cout << "I'm just a dummy test application.\n";
-    cout << "Since I have nothing better to do, I'll start an ubound memory test (i.e. I'll even test the memory your computer doesn't have :-)!!\n";
+    cout << "Since I have nothing better to do, I'll start an ubound memory test (that is, I'll even test the memory your computer doesn't have :-)!!\n";
      cout << "Testing memory: ";
 
-    Display disp;
-    for(char * ptr = (char *)(1024*1024); ptr < (char *)0xffffffff; ptr+= 64) {
- 	disp.position(-1, 16);
-	cout << (void *)ptr;
- 	*ptr = 'G';
- 	if(*ptr != 'G') {
- 	    cout << "\nLast memory position at " << ptr << "!\n";
- 	    break;
-	}
-    }
+     Display disp;
+     for(char * ptr = (char *)(1024*1024);
+	 ptr < (char *)0xffffffff;
+	 ptr+= 64) {
+	 disp.position(-1, 16);
+	 cout << (void *)ptr;
+	 *ptr = 'G';
+	 if(*ptr != 'G') {
+	     cout << "\nLast memory position at " << ptr << "!\n";
+	     break;
+	 }
+     }
 
     return 0;
 }
