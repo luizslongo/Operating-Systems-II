49af79394788fd506d85dad493a2739dcc71d0c5
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2005-12-03 18:00:13 +0000

    Serial_Display added. Selecion between CGA and Serial based on Traits::on_serial.

--- src/mach/pc/display_init.cc
+++ src/mach/pc/display_init.cc
@@ -11,4 +11,11 @@ int PC_Display::init(System_Info * si)
     return 0;
 }
 
+int PC_Serial_Display::init(System_Info * si)
+{
+    db<PC_Display>(TRC) << "PC_Display::init()\n";
+
+    return 0;
+}
+
 __END_SYS
