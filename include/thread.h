20a9215116ff2891aca26c4f369b1e339b98b864
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2005-12-03 17:45:30 +0000

    The critical section in the constructors has been enlarged to produce better debug output.
    Reschedule has been moved (along with some other implementation methods) to the .cpp, firstly because INIT was geting a local copy.

--- include/thread.h
+++ include/thread.h
@@ -50,87 +50,83 @@ public:
     Thread(int (* entry)(), 
 	   const State & state = READY,
 	   const Priority & priority = NORMAL,
-	   unsigned int stack_size = STACK_SIZE)
-	: _stack(malloc(stack_size)), 
-	  _context(new (_stack + stack_size
-			- sizeof(int) - sizeof(Context))
-		   Context(entry)),
-	  _state(state),
-	  _waiting(0),
-	  _joining(0),
-	  _link(this, priority)
+	   unsigned int stack_size = STACK_SIZE) : _link(this, priority)
+
     {
+	prevent_scheduling();
+
+	_stack = malloc(stack_size);
+	_context = new (_stack + stack_size 
+			- sizeof(int) - sizeof(Context)) Context(entry);
+
 	header(entry, stack_size);
+
 	Log_Addr sp = _stack + stack_size;
-	sp -= sizeof(int); *static_cast<unsigned int *>(sp) = 
-			       reinterpret_cast<unsigned int>(&implicit_exit);
-	body();
+
+	body(state, priority, sp);
     }
     template<class T1>
     Thread(int (* entry)(T1 a1), T1 a1,
 	   const State & state = READY,
 	   const Priority & priority = NORMAL,
-	   unsigned int stack_size = STACK_SIZE)
-	: _stack(malloc(stack_size)), 
-	  _context(new (_stack + stack_size - sizeof(T1)
-			- sizeof(int) - sizeof(Context))
-		   Context(entry)),
-	  _state(state),
-	  _waiting(0),
-	  _joining(0),
-	  _link(this, priority)
+	   unsigned int stack_size = STACK_SIZE) : _link(this, priority)
     {
+	prevent_scheduling();
+
+	_stack = malloc(stack_size);
+	_context = new (_stack + stack_size
+			- sizeof(T1)
+			- sizeof(int) - sizeof(Context)) Context(entry);
+
 	header(entry, stack_size);
+
 	Log_Addr sp = _stack + stack_size;
 	sp -= sizeof(T1); *static_cast<T1 *>(sp) = a1;
-	sp -= sizeof(int); *static_cast<unsigned int *>(sp) = 
-			       reinterpret_cast<unsigned int>(&implicit_exit);
-	body();
+
+	body(state, priority, sp);
     }
     template<class T1, class T2>
     Thread(int (* entry)(T1 a1, T2 a2), T1 a1, T2 a2,
 	   const State & state = READY,
 	   const Priority & priority = NORMAL,
-	   unsigned int stack_size = STACK_SIZE)
-	: _stack(malloc(stack_size)), 
-	  _context(new (_stack + stack_size - sizeof(T2) - sizeof(T1)
-			- sizeof(int) - sizeof(Context))
-		   Context(entry)),
-	  _state(state),
-	  _waiting(0),
-	  _joining(0),
-	  _link(this, priority)
+	   unsigned int stack_size = STACK_SIZE) : _link(this, priority)
     {
+	prevent_scheduling();
+
+	_stack = malloc(stack_size);
+	_context = new (_stack + stack_size 
+			- sizeof(T2) - sizeof(T1)
+			- sizeof(int) - sizeof(Context)) Context(entry);
+
 	header(entry, stack_size);
+
 	Log_Addr sp = _stack + stack_size;
 	sp -= sizeof(T2); *static_cast<T2 *>(sp) = a2;
 	sp -= sizeof(T1); *static_cast<T1 *>(sp) = a1;
-	sp -= sizeof(int); *static_cast<unsigned int *>(sp) = 
-			       reinterpret_cast<unsigned int>(&implicit_exit);
-	body();
+
+	body(state, priority, sp);
     }
     template<class T1, class T2, class T3>
     Thread(int (* entry)(T1 a1, T2 a2, T3 a3), T1 a1, T2 a2, T3 a3,
 	   const State & state = READY,
 	   const Priority & priority = NORMAL,
-	   unsigned int stack_size = STACK_SIZE)
-	: _stack(malloc(stack_size)), 
-	  _context(new (_stack + stack_size - sizeof(T3) - sizeof(T2) 
-			- sizeof(T1) - sizeof(int) - sizeof(Context))
-		   Context(entry)),
-	  _state(state),
-	  _waiting(0),
-	  _joining(0),
-	  _link(this, priority)
+	   unsigned int stack_size = STACK_SIZE) : _link(this, priority)
     {
+	prevent_scheduling();
+
+	_stack = malloc(stack_size);
+	_context = new (_stack + stack_size 
+			- sizeof(T3) - sizeof(T2) - sizeof(T1)
+			- sizeof(int) - sizeof(Context)) Context(entry);
+
 	header(entry, stack_size);
+
 	Log_Addr sp = _stack + stack_size;
 	sp -= sizeof(T3); *static_cast<T3 *>(sp) = a3;
 	sp -= sizeof(T2); *static_cast<T2 *>(sp) = a2;
 	sp -= sizeof(T1); *static_cast<T1 *>(sp) = a1;
-	sp -= sizeof(int); *static_cast<unsigned int *>(sp) = 
-			       reinterpret_cast<unsigned int>(&implicit_exit);
-	body();
+
+	body(state, priority, sp);
     }
     ~Thread() {
 	db<Thread>(TRC) << "~Thread(this=" << this 
@@ -169,19 +165,24 @@ public:
 
 private:
     void header(Log_Addr entry, unsigned int stack_size) {
-	db<Thread>(TRC) << "Thread(this=" << this 
-			<< ",entry=" << (void *)entry 
+	db<Thread>(TRC) << "Thread(entry=" << (void *)entry 
 			<< ",state=" << _state
 			<< ",priority=" << _link.rank()
 			<< ",stack={b=" << _stack
 			<< ",s=" << stack_size
-			<< ",context={b=" << _context
-			<< "," << *_context << "})\n";
+			<< "},context={b=" << _context
+			<< "," << *_context << "}) => " << this << "\n";
     }
-    void body() {
-	prevent_scheduling();
+    void body(State state, Priority priority, Log_Addr sp) {
+	_state = state;
+	_waiting = 0;
+	_joining = 0;
 
-	switch(_state) {
+	sp -= sizeof(int); 
+	*static_cast<unsigned int *>(sp) = 
+	    reinterpret_cast<unsigned int>(&implicit_exit);
+
+	switch(state) {
 	case RUNNING: break;
 	case SUSPENDED: _suspended.insert(&_link); break;
 	default: _ready.insert(&_link);
@@ -197,54 +198,19 @@ private:
     static void running(Thread * r) { _running = r; }
 
     static void prevent_scheduling() {
-	if(Traits::active_scheduler)
-	    CPU::int_disable();
+	if(Traits::active_scheduler) CPU::int_disable();
     }
     static void allow_scheduling() {
-	if(Traits::active_scheduler)
-	    CPU::int_enable();
+	if(Traits::active_scheduler) CPU::int_enable();
     }
 
-    static void reschedule() { // this is the master alarm handler (int dis.)
-	Queue::Element * e = _ready.head();
-	if(e && e->rank() <= _running->_link.rank()) {
-	    _running->_state = READY;
-	    _ready.insert(&_running->_link);
-	    switch_to(_ready.remove()->object());
-	}
-    }
+    static void reschedule(); // this is the master alarm handler
 
-    static void implicit_exit() { exit(CPU::fr()); }
+    static void implicit_exit();
 
-    static void switch_to(Thread * n) {
-	Thread * o = _running;
-    
-// 	o->_context->save(); // can be used to force an update
-	db<Thread>(INF) << "old={" << o << "," << *o->_context << "}\n";
-	db<Thread>(INF) << "new={" << n << "," << *n->_context << "}\n";
+    static void switch_to(Thread * n);
 
-	n->_state = RUNNING;
-	_running = n;
-
-	allow_scheduling(); // seems efectless, but shouldn't!!!
-	CPU::switch_context(&o->_context, n->_context);
-    }
-
-    static int idle() {
-	while(true) {
-	    CPU::halt();
-
-	    if(!_ready.empty())
-		yield();
-	    else
-		if(_suspended.empty()) { 
-		    db<Thread>(WRN) << "The last thread has exited!\n";
-		    db<Thread>(WRN) << "Halting the CPU ...\n";
-		    CPU::int_disable();
-		}
-	}
-	return 0;
-    }
+    static int idle();
 
 private:
     Log_Addr _stack;
