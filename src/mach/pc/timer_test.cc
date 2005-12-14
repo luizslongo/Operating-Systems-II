f1c7fe88e06a525f8002111e72f2b63562dbac1b
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2005-12-14 02:17:16 +0000

    Still a very simple test.

--- src/mach/pc/timer_test.cc
+++ src/mach/pc/timer_test.cc
@@ -11,7 +11,16 @@ int main()
 
     cout << "PC_Timer test\n";
 
-    PC_Timer timer;
+    PC_Timer timer(1000);
+    
+    cout << "count = " << timer.read() << "\n";
+    cout << "count = " << timer.read() << "\n";
+    cout << "count = " << timer.read() << "\n";
+    cout << "count = " << timer.read() << "\n";
+    cout << "count = " << timer.read() << "\n";
+    cout << "count = " << timer.read() << "\n";
+    
+    cout << "The End!\n";
 
     return 0;
 }
