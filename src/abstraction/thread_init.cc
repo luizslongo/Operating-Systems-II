aecfeb4164e043ca137ab5b88d007646e903931f
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2005-12-14 02:08:30 +0000

    _running MUST be created before _idle and with state=RUNNING, otherwise, _idle might start running immediatly (case preemptive=true), thus blocking init(). New CTTI. New INIT. New CONFIG.

--- src/abstraction/thread_init.cc
+++ src/abstraction/thread_init.cc
@@ -11,15 +11,19 @@ int Thread::init(System_Info * si)
     db<Init>(TRC) << "Thread::init(entry="
 		  << (void *)si->lmm.app_entry << ")\n";
 
-    if(Traits::active_scheduler)
-	Alarm::master(Traits::quantum, &reschedule);
+    prevent_scheduling();
 
-    _idle = new(kmalloc(sizeof(Thread))) Thread(&idle, READY, IDLE);
+    if(active_scheduler)
+	Alarm::master(QUANTUM, &reschedule);
 
     _running = 	new(malloc(sizeof(Thread))) 
 	Thread(reinterpret_cast<int (*)()>(si->lmm.app_entry), RUNNING);
 
+    _idle = new(kmalloc(sizeof(Thread))) Thread(&idle, READY, IDLE);
+
     _running->_context->load();
+
+    allow_scheduling();
 
     return 0;
 }
