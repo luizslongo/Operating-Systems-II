846630e02ca8dd9b170b972c5d1df5cd11be0193
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2006-01-06 17:33:01 +0000

    Generic names (NIC, instead of PC_NIC).

--- src/mach/pc/nic_test.cc
+++ src/mach/pc/nic_test.cc
@@ -1,6 +1,6 @@
 // EPOS-- PC NIC Mediator Test
 
-#include <mach/pc/nic.h>
+#include <nic.h>
 
 __USING_SYS
 
@@ -8,17 +8,17 @@ OStream cout;
 
 int main()
 {
-    PC_NIC nic;
-    PC_NIC::Address src, dst;
-    PC_NIC::Protocol prot;
+    NIC nic;
+    NIC::Address src, dst;
+    NIC::Protocol prot;
     char data[nic.mtu()];
 
     for(int i = 0; i < 10; i++)
-	nic.send(PC_NIC::BROADCAST, 0x8888, "alguem ai?\n", 12);
+	nic.send(NIC::BROADCAST, 0x8888, "alguem ai?\n", 12);
     for(int i = 0; i < 10; i++) 
 	nic.receive(&src, &prot, data, nic.mtu());
 
-    PC_NIC::Statistics stat = nic.statistics();
+    NIC::Statistics stat = nic.statistics();
     cout << "Statistics\n"
 	 << "Tx Packets: " << stat.tx_packets << "\n"
 	 << "Tx Bytes:   " << stat.tx_bytes << "\n"
