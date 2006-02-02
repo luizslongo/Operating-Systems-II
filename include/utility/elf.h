2544326536207f0e871f6b3020dd00478e1838f2
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2006-02-02 20:53:39 +0000

    Elf32_Addr (from Linux) used instead of void *. Xunxo to get rid of GNU_STACK removed.

--- include/utility/elf.h
+++ include/utility/elf.h
@@ -18,12 +18,12 @@ public:
 	    && (e_ident[EI_MAG2] == ELFMAG2) && (e_ident[EI_MAG3] == ELFMAG3);
     }
 
-    void * entry() { return (void *)((int)e_entry); }
-    int segments() { return e_phnum - 1; }
+    Elf32_Addr entry() { return e_entry; }
+    int segments() { return e_phnum; }
     
-    void * segment_address(int i) {
+    Elf32_Addr segment_address(int i) {
 	return (i > segments()) ? 0 :
-	    (void *)((int)(seg(i)->p_vaddr & ~(seg(i)->p_align - 1)));
+	    (seg(i)->p_vaddr & ~(seg(i)->p_align - 1));
     }
     int segment_size(int i) {
 	return (i > segments()) ? -1 : (int)(
@@ -33,7 +33,7 @@ public:
 	    & ~(seg(i)->p_align - 1));
     }
 
-    int load_segment(int i, void * addr = 0);
+    int load_segment(int i, Elf32_Addr addr = 0);
 
 private:
     Elf32_Phdr * pht() { return (Elf32_Phdr *)(((char *) this) + e_phoff); }
