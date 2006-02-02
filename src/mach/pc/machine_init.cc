e425a6d57b88eeaab0f88302262d7ee38b3c71dd
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2006-02-02 21:07:39 +0000

    Machine initialization is now architecture-dependend and is no longer directly carried by INIT. Instead, INIT invokes Machine::init(), which, in turn, must call all init()s. Double fault is now cached with page-fault, this giving out some pointers.

--- src/mach/pc/machine_init.cc
+++ src/mach/pc/machine_init.cc
@@ -1,16 +1,16 @@
 // EPOS-- PC Mediator Initialization
 
-#include <mach/pc/machine.h>
+#include <machine.h>
 
 __BEGIN_SYS
 
-int PC::init(System_Info * si)
+void PC::init()
 {
     db<Init, PC>(TRC) << "PC::init()\n";
 
     // Set all IDT entries to proper int_dispatch() offsets
     IA32::IDT_Entry * idt =
-	reinterpret_cast<IA32::IDT_Entry *>(Memory_Map<PC>::INT_VEC);
+	reinterpret_cast<IA32::IDT_Entry *>(Memory_Map<PC>::IDT);
     for(unsigned int i = 0; i < IA32::IDT_ENTRIES; i++)
 	if(i < INT_VECTOR_SIZE)
 	    idt[i] = IA32::IDT_Entry(IA32::GDT_SYS_CODE, 
@@ -27,10 +27,19 @@ int PC::init(System_Info * si)
 
     // Reset some important exception handlers
     _int_vector[IA32::EXC_PF] = reinterpret_cast<int_handler *>(exc_pf);
+    _int_vector[IA32::EXC_DOUBLE] = reinterpret_cast<int_handler *>(exc_pf);
     _int_vector[IA32::EXC_GPF] = reinterpret_cast<int_handler *>(exc_gpf);
     _int_vector[IA32::EXC_NODEV] = reinterpret_cast<int_handler *>(exc_fpu);
 
-    return 0;
+    // Initialize the hardware
+    IA32_MMU::init();
+
+    if(Traits<PC_PCI>::enabled)
+	PC_PCI::init();
+    if(Traits<PC_IC>::enabled)
+	PC_IC::init();
+    if(Traits<PC_NIC>::enabled)
+	PC_NIC::init();
 }
 
 __END_SYS
