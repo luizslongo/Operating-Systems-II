3218fcb99b9ff44ea75b27a2e1a8b79b355fb45a
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2006-02-02 20:53:57 +0000

    Refactoring to heap.cc.

--- include/utility/heap.h
+++ include/utility/heap.h
@@ -6,7 +6,6 @@
 #include <utility/debug.h>
 #include <utility/string.h>
 #include <utility/list.h>
-#include <machine.h>
 
 __BEGIN_SYS
 
@@ -19,25 +18,7 @@ public:
 	free(addr, bytes); 
     }
 
-    void * alloc(unsigned int bytes) {
-	if(!bytes)
-	    return 0;
-	
-	bytes += sizeof(int);
-	Element * e = search_decrementing(bytes);
-	if(!e) {
-	    db<Heap>(ERR) << "Heap::alloc: out of memory!\n";
-	    Machine::panic();
-	    return 0;
-	}
-	int * addr = reinterpret_cast<int *>(e->object() + e->size());
-	    
-	db<Heap>(TRC) << "Heap::alloc(this=" << this
-		      << ",bytes=" << bytes 
-		      << ") => " << (void *)addr << "\n";
-	addr[0] = bytes;
-	return &addr[1];
-    }
+    void * alloc(unsigned int bytes);
     void * calloc(unsigned int bytes) {
 	void * addr = alloc(bytes);
 	memset(addr, bytes, 0);
@@ -49,18 +30,7 @@ public:
 	int * addr = (int *)ptr;
 	free(&addr[-1], addr[-1]);
     }
-    void free(void * ptr, unsigned int bytes) {
-        db<Heap>(TRC) << "Heap::free(this=" << this
-		      << ",ptr=" << ptr
-		      << ",bytes=" << bytes << ")\n";
-
-	if(ptr && (bytes >= sizeof(Element))) {
-	    Element * e = new (ptr)
-		Element(reinterpret_cast<char *>(ptr), bytes);
-	    Element * m1, * m2;
-	    insert_merging(e, &m1, &m2);
-	}
-    }
+    void free(void * ptr, unsigned int bytes);
 };
 
 __END_SYS
