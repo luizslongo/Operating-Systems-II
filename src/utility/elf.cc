c7db45d0425a6192b08ab76b9c8ef0a07ff9f18b
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2006-02-03 19:35:47 +0000

    Elf32_Addr from Linux is now used.

--- src/utility/elf.cc
+++ src/utility/elf.cc
@@ -5,16 +5,16 @@
 
 __BEGIN_SYS
 
-int ELF::load_segment(int i, void * addr)
+int ELF::load_segment(int i, Elf32_Addr addr)
 {
     if((i > segments()) || (seg(i)->p_type != PT_LOAD))
 	return -1;
     
-    void * src = ((char *) this) + seg(i)->p_offset;
-    void * dst = (addr)? addr : segment_address(i);
+    char * src = (char *)(unsigned(this) + seg(i)->p_offset);
+    char * dst = (char *)((addr) ? addr : segment_address(i));
     
     memcpy(dst, src, seg(i)->p_filesz);
-    memset((char *)dst + seg(i)->p_filesz, 0,
+    memset(dst + seg(i)->p_filesz, 0,
 	   seg(i)->p_memsz - seg(i)->p_filesz);
     
     return seg(i)->p_memsz;
