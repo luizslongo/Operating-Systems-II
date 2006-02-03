d55919c4c1a714430dce6bf1dcc2e42c51d89ee0
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2006-02-03 19:36:29 +0000

    The application's heap is now a global object, which gets linked only when used (i.e. application call new).

--- src/utility/malloc.cc
+++ src/utility/malloc.cc
@@ -1,21 +1,12 @@
 // EPOS-- Application-level Dynamic Memory Utility Implementation
 
-#include <utility/heap.h>
+#include <utility/malloc.h>
+#include <mmu.h>
 
 __BEGIN_SYS
-extern Heap app_heap;
+
+Heap app_heap(
+    MMU::alloc(MMU::pages(Traits<Machine>::APPLICATION_HEAP_SIZE)),
+    Traits<Machine>::APPLICATION_HEAP_SIZE);
+
 __END_SYS
-
-// The preferred implementation of these methods is in <utility/malloc.h>, but 
-// GCC implementation of virtual destructors implicitly calls delete, so an 
-// extern linkage version is also necessary.
-
-void * operator new(unsigned int bytes) {
-    return __SYS(app_heap).alloc(bytes);
-}
-void * operator new[](unsigned int bytes) { 
-    return __SYS(app_heap).alloc(bytes); 
-}
-void operator delete(void * object) {
-    __SYS(app_heap).free(object);
-}
