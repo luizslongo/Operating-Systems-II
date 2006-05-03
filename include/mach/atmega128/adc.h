e19d1563c6ea42923924515856e5e56a0c12a442
lucas <lucas@lisha.ufsc.br>
2006-05-03 18:02:44 +0000

    New ADC interface: sample() for safe readings (enable, read, disable), and get() for raw ADC values.

--- include/mach/atmega128/adc.h
+++ include/mach/atmega128/adc.h
@@ -11,9 +11,9 @@ class ATMega128_ADC: public ADC_Common, private AVR_ADC
 {
 public:
     ATMega128_ADC() : AVR_ADC(SINGLE_ENDED_ADC0, SYSTEM_REF,
-			      FREE_RUNNING_MODE, CLOCK >> 7) {}
+			      SINGLE_CONVERSION_MODE, CLOCK >> 7) {}
     ATMega128_ADC(unsigned char channel, Hertz frequency) 
-	: AVR_ADC(channel, SYSTEM_REF, FREE_RUNNING_MODE, frequency) {}
+	: AVR_ADC(channel, SYSTEM_REF, SINGLE_CONVERSION_MODE, frequency) {}
     ATMega128_ADC(unsigned char channel, unsigned char reference,
 		  unsigned char trigger, Hertz frequency) 
 	: AVR_ADC(channel, reference, trigger, frequency) {}
@@ -26,11 +26,12 @@ public:
 		unsigned char * trigger, Hertz * frequency) {
 	AVR_ADC::config(channel, reference, trigger, frequency);
     }
-
+    
+    int sample() { return AVR_ADC::sample(); }
     int get() { return AVR_ADC::get(); }
     bool finished() { return AVR_ADC::finished(); }
 
-    void enable() { return AVR_ADC::enable(); }
+    bool enable() { return AVR_ADC::enable(); }
     void disable() { return AVR_ADC::disable(); }
 
     void reset() { return AVR_ADC::reset(); }
