3a6662ebe194e64613f9d80410a43f86f3912dac
lucas <lucas@lisha.ufsc.br>
2006-05-03 18:12:39 +0000

    Adding implementation for (internal) Timer_2 (used by CMAC).

--- include/mach/atmega128/timer.h
+++ include/mach/atmega128/timer.h
@@ -50,6 +50,48 @@ protected:
     static Count freq2count(const Hertz & f) { return CLOCK / f; }
 };
 
+class ATMega128_Timer_2: public Timer_Common, private AVR_Timer
+{
+private:
+    static const unsigned int CLOCK = Traits<Machine>::CLOCK >> 9;
+
+public:
+    // Register Settings
+    enum {
+    	TIMER_PRESCALE_1    = CSn0,
+	TIMER_PRESCALE_8    = CSn1,
+	TIMER_PRESCALE_64   = CSn1 | CSn0,
+	TIMER_PRESCALE_256  = CSn2,
+	TIMER_PRESCALE_1024 = CSn2 | CSn0
+    };
+
+public:
+    ATMega128_Timer_2() {}
+
+    ATMega128_Timer_2(const Hertz & f) {
+	frequency(f);
+    }
+
+    Hertz frequency() const { return count2freq(ocr2()); }
+    void frequency(const Hertz & f) {
+	ocr2(freq2count(f));
+	tccr2(WGM21 | TIMER_PRESCALE_1024);
+    };
+
+    void reset() { tcnt2(0); }
+
+    void enable(){ timsk(timsk() | OCIE2); }
+    void disable(){ timsk(timsk() & ~OCIE2); }
+
+    Tick read() { return tcnt2(); }
+
+protected:
+    static Hertz count2freq(const Count & c) { return CLOCK / c; }
+    static Count freq2count(const Hertz & f) { return CLOCK / f; }
+};
+
+
+
 __END_SYS
 
 #endif
