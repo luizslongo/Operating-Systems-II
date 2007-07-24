a22a753bd050166f315755f29edbc333059d7c85
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2007-07-24 20:41:30 +0000

    BIG WARNING HERE!
    The trick with an empty virtual destructor at Hander was an attempt to avoid the implicit delete call (jump actually) generated by GCC. It wasn't working smoothly, so modifications were made to have a in-library version of memory allocation functions (i.e. delete), so that now GCC can issue calls to them. This is accomplished by the "#pragma interface" at malloc.h and "/#pragma implementation" at gcc_helper.cc.
    
    This, however, might be just another silly workaround, since we still miss the explanation of why GCC is doing that, and, perhaps, will end up deleting something we didn't allocated!

--- include/utility/malloc.h
+++ include/utility/malloc.h
@@ -1,9 +1,12 @@
 // EPOS-- Application-level Dynamic Memory Utility Declarations
 
+#pragma interface
+
+#include <system/config.h>
+#include <utility/heap.h>
+
 #ifndef __malloc_h
 #define __malloc_h
-
-#include <utility/heap.h>
 
 __BEGIN_SYS
 extern Heap app_heap;
@@ -15,9 +18,9 @@ inline void * malloc(unsigned int bytes) {
 inline void * calloc(unsigned int n, unsigned int bytes) {
     return __SYS(app_heap).calloc(n * bytes); 
 }
-inline void * realloc(void * ptr, unsigned int bytes) {
-    return __SYS(app_heap).realloc(ptr, bytes);
-}
+// inline void * realloc(void * ptr, unsigned int bytes) {
+//     return __SYS(app_heap).realloc(ptr, bytes);
+// }
 inline void free(void * ptr) {
     __SYS(app_heap).free(ptr); 
 }
@@ -29,6 +32,9 @@ inline void * operator new[](unsigned int bytes) {
     return __SYS(app_heap).alloc(bytes); 
 }
 inline void operator delete(void * object) {
+    __SYS(app_heap).free(object);
+}
+inline void operator delete[](void * object) {
     __SYS(app_heap).free(object);
 }
 
---
+++ src/utility/gcc_helper.cc
@@ -0,0 +1,6 @@
+// EPOS-- GCC Helper
+
+#pragma implementation "malloc.h"
+
+// Backup copies of inline memory-realted functions
+#include <utility/malloc.h>
