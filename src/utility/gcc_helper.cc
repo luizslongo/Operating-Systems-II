38e92b82f6884d62b8027a41721ec3a014147552
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2007-07-26 23:57:49 +0000

    Since the name "malloc.cc" became available after app_heap was moved to Application::_heap, it is a better name for the backup versions of malloc functions. Other "GCC helper functions" will be placed accordingly.

--- src/utility/gcc_helper.cc
+++
@@ -1,6 +0,0 @@
-// EPOS-- GCC Helper
-
-#pragma implementation "malloc.h"
-
-// Backup copies of inline memory-realted functions
-#include <utility/malloc.h>
---
+++ src/utility/malloc.cc
@@ -0,0 +1,5 @@
+// EPOS-- Application-level Dynamic Memory Utility Implementation
+
+// Backup copies of inline memory-realted functions
+//#pragma implementation "malloc.h"
+#include <utility/malloc.h>
