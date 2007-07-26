88f309e508a47522a60cfd21849a883b8a9e6051
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2007-07-26 23:16:44 +0000

    Following the idea behind the "System" abstraction (i.e. an interface to system-wide operations and an aggregator for loose system partes), a "Application" abstraction was created, along with an initializer (init_application). Scaffold hold global objects respectively for system and application.
    Additionally, it made it easier to control global objects construction.

---
+++ src/system/system_scaffold.cc
@@ -0,0 +1,20 @@
+// EPOS-- System Scaffold and System Abstraction Implementation
+
+#include <utility/ostream.h>
+#include <utility/heap.h>
+#include <machine.h>
+#include <system.h>
+
+__BEGIN_SYS
+
+// Global objects
+OStream kout;
+OStream kerr;
+
+// System class attributes
+System_Info<Machine> * System::_si =
+    reinterpret_cast<System_Info<Machine> *>(Memory_Map<Machine>::SYS_INFO);
+
+Heap System::_heap;
+
+__END_SYS
