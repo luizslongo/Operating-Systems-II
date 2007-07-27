f535f83575117969a97184d336b8acdb40175307
Hugo Marcondes <hugom@lisha.ufsc.br>
2007-07-27 21:59:17 +0000

    In the case of ML_xxx machines, the bug in setup also affect the boot code. The system abstractions used by boot code (i.e Machine::panic() & OStream) was replaced by a custom code. With the elimination of this bug, this boot code was considerable reduced in size, so the max_boot_size was reduced to 2Kb (see makedefs).
    --
    The bug? Having a global constructor in a ".o" causes it to be linked independently of being used. SETUP and APPLICATION were getting almost whole copies of SYSTEM because of Sytem::_heap;

--- src/boot/ml310_boot.cc
+++ src/boot/ml310_boot.cc
@@ -11,25 +11,15 @@ typedef Memory_Map<ML310> MM;
 typedef void (function_type)();
 
 // Constants
-static const unsigned int BOOT_IMAGE = 9 * 1024;
+static const unsigned int BOOT_IMAGE = 2 * 1024;
 
 // Prototypes
 extern "C" { void _start(); }
+inline void _panic(unsigned int code);
 
 void _start()
 {
-    //Disable Interrupts
-    //Already disabled at startup ? - chack later
-
-    // GPIO Doc ...
-    //  0b0000 0000 0000 0000 0000 0000 0000 0000
-    //                                  |||| ||||-DBG7
-    //                                  |||| |||--DBG6
-    //                                  |||| ||---DBG5
-    //                                  ..............
-    //                                  |---------DBG0
     //Light on GPIO Leds
-
     *((volatile unsigned int *)(Traits<Machine>::LEDS_BASEADDR+4)) = 0x00000000;
     //Light Stage1 Boot.
     *((volatile unsigned int *)(Traits<Machine>::LEDS_BASEADDR)) = 0xFFFFFFFE;
@@ -37,8 +27,6 @@ void _start()
     // Setting a stack for EPOS initialization
     unsigned int stack_pointer = MM::SYS_STACK; 
     ASMV("mr 1,%0" : : "r" (stack_pointer));
-
-    db<Boot>(TRC) << "BOOT()\n";
 
     // The boot image was loaded by the monitor at BOOT_IMAGE
     char * tmp = reinterpret_cast<char *>(BOOT_IMAGE);
@@ -56,24 +44,29 @@ void _start()
     // Load SETUP
     ELF * stp_elf = reinterpret_cast<ELF *>(&bi[si->bm.setup_offset]);
     if(!stp_elf->valid()) {
-	db<Boot>(ERR) << "SETUP ELF image was corrupted!\n";
-	Machine::panic();
+	_panic(0x0F);
     }
     if(stp_elf->load_segment(0) < 0) {
-	db<Boot>(ERR) << "SETUP code segment was corrupted!\n";
-	Machine::panic();
+	_panic(0x06);
     }
     for(int i = 1; i < stp_elf->segments(); i++) {
 	if(stp_elf->load_segment(i) < 0) {
-	    db<Boot>(ERR) << "SETUP data segment was corrupted!\n";
-	    Machine::panic();
+	    _panic(0x09);
 	}
     }
 
     // Call setup
     void (*entry)() = reinterpret_cast<void (*)()>(stp_elf->entry());
-
     *((volatile unsigned int *)(Traits<Machine>::LEDS_BASEADDR)) = 0xFFFFFFFC;
-
     entry();
 }
+
+void _panic(unsigned int code){
+    while(1){
+       *((volatile unsigned int *)(Traits<Machine>::LEDS_BASEADDR)) = code;
+       for(int i = 0; i < 0x1000000; i++);
+       *((volatile unsigned int *)(Traits<Machine>::LEDS_BASEADDR)) = 0;
+       for(int i = 0; i < 0x1000000; i++);
+    }
+}
+
