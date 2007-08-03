dda1c95cb4cf4c2dfd02d9e0e245b1ed90b9fb86
Hugo Marcondes <hugom@lisha.ufsc.br>
2007-08-03 01:10:24 +0000

    Initialization of System::Heap() was moved to init_system.cc

--- src/arch/ppc32/mmu_init.cc
+++ src/arch/ppc32/mmu_init.cc
@@ -7,8 +7,9 @@ __BEGIN_SYS
 
 void PPC32_MMU::init()
 {
-    System_Info<Machine> * si = System::info();
+    db<PPC32_MMU>(TRC) << "PPC32_MMU::init()\n";
 
+    System_Info<Machine> * si = System::info();
     db<Init, PPC32_MMU>(INF) << "PPC32_MMU::memory={base=" 
 			     << (void *) si->pmm.mem_base << ",size="
 			     << (si->bm.mem_top - si->bm.mem_base) / 1024
@@ -26,12 +27,6 @@ void PPC32_MMU::init()
     free(si->pmm.free1_base, si->pmm.free1_top -si->pmm.free1_base);
     free(si->pmm.free2_base, si->pmm.free2_top -si->pmm.free2_base);
 
-    // Initialize the System's heap
-    db<Init, PPC32_MMU>(INF) << "PPC32_MMU::initializing system's heap="
-			     << Traits<Machine>::SYSTEM_HEAP_SIZE 
-			     << " bytes.\n";
-    System::heap()->free(alloc(pages(Traits<Machine>::SYSTEM_HEAP_SIZE)),
-			 Traits<Machine>::SYSTEM_HEAP_SIZE);
 }
 
 __END_SYS
