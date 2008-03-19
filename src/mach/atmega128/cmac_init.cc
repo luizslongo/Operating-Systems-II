e3d7c08fccb567c9af67bd13635894a4ef0ee193
lucas <lucas@lisha.ufsc.br>
2008-03-19 15:29:40 +0000

    Registering interrupt handlers and enabling timer.

--- src/mach/atmega128/cmac_init.cc
+++ src/mach/atmega128/cmac_init.cc
@@ -5,6 +5,17 @@
 
 __BEGIN_SYS
 
-void CMAC::init(unsigned int n) {}
+void CMAC::init(unsigned int n) {
+
+    Machine::int_vector(IC::IRQ_TIMER2_COMP, &timer_handler);
+
+    Machine::int_vector(IC::IRQ_SPI_STC, &spi_handler);
+    
+
+    _timer.frequency(30);
+
+    _timer.enable();
+
+}
 
  __END_SYS
