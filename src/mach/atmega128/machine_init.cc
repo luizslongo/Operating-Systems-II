3e918a1aff4c3474cbb3762e850fd4a28bc0aee7
roberto <roberto@lisha.ufsc.br>
2008-03-28 20:31:34 +0000

    Initialization of architecture mediators was refactoring into CPU::init().

--- src/mach/atmega128/machine_init.cc
+++ src/mach/atmega128/machine_init.cc
@@ -3,8 +3,6 @@
 #include <machine.h>
 #include <system.h>
 
-extern "C" unsigned __bss_end;
-
 extern "C" { void __epos_library_app_entry(void); }
 
 __BEGIN_SYS
@@ -13,23 +11,12 @@ void ATMega128::init()
 {
     db<Init, ATMega128>(TRC) << "ATMega128::init()\n";
 
-    System_Info<ATMega128> * si = System::info();
-    si->pmm.free_base	= (unsigned)&__bss_end;
-    si->pmm.free_top	= Memory_Map<Machine>::TOP;
-
-    si->lmm.app_entry =
+    System::info()->lmm.app_entry =
 	reinterpret_cast<unsigned int>(&__epos_library_app_entry);
 
     IC::disable();
 
     // Initialize the hardware
-    if(Traits<AVR8>::enabled)
-	AVR8::init();
-    if(Traits<AVR8_MMU>::enabled)
-	AVR8_MMU::init();
-    if(Traits<AVR8_TSC>::enabled)
-	AVR8_TSC::init();
-
     if(Traits<ATMega128_NIC>::enabled)
 	ATMega128_NIC::init();
 
