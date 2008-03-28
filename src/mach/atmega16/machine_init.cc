a7ae16b6998fd75779a2acb3f007a4e2aec7e58e
lucas <lucas@lisha.ufsc.br>
2008-03-28 21:58:42 +0000

    Arch mediators were refactored into CPU::init

--- src/mach/atmega16/machine_init.cc
+++ src/mach/atmega16/machine_init.cc
@@ -3,8 +3,6 @@
 #include <machine.h>
 #include <system.h>
 
-extern "C" unsigned __bss_end;
-
 extern "C" { void __epos_library_app_entry(void); }
 
 __BEGIN_SYS
@@ -13,20 +11,8 @@ void ATMega16::init()
 {
     db<Init, ATMega16>(TRC) << "ATMega16::init()\n";
 
-    System_Info<ATMega16> * si = System::info();
-    si->pmm.free_base	= (unsigned)&__bss_end;
-    si->pmm.free_top	= Memory_Map<Machine>::TOP;
-
-    si->lmm.app_entry =
+    System::info()->lmm.app_entry =
 	reinterpret_cast<unsigned int>(&__epos_library_app_entry);
-
-    // Initialize the hardware
-    if(Traits<AVR8>::enabled)
-	AVR8::init();
-    if(Traits<AVR8_MMU>::enabled)
-	AVR8_MMU::init();
-    if(Traits<AVR8_TSC>::enabled)
-	AVR8_TSC::init();
 
     if(Traits<ATMega16_NIC>::enabled)
 	ATMega16_NIC::init();
