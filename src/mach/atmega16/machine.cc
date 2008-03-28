6869f07e030b0ed78ff013017a2ce4770064650b
lucas <lucas@lisha.ufsc.br>
2008-03-28 21:58:20 +0000

    Adding __epos_free_init_memory

--- src/mach/atmega16/machine.cc
+++ src/mach/atmega16/machine.cc
@@ -19,3 +19,14 @@ extern "C" void __epos_call_handler(char offset) {
     if (handler != 0)
 	handler(0);
 }
+
+extern "C" void __epos_free_init_mem();
+
+extern "C" void __epos_free_init_mem() {
+    
+//    MMU::free(Memory_Map<ATMega128>::TOP,$
+//	      Memory_Map<ATMega128>::MEM_SIZE - 
+//	      Memory_Map<ATMega128>::TOP + 
+//	      Memory_Map<ATMega128>::PHY_MEM);
+    
+}
