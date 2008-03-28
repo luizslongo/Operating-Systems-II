a80031e65b83771500c4b532f61bdba2877c2892
roberto <roberto@lisha.ufsc.br>
2008-03-28 20:21:50 +0000

    Refactoring architecture mediators intializations into the CPU::init().

--- src/arch/avr8/cpu_init.cc
+++ src/arch/avr8/cpu_init.cc
@@ -1,6 +1,8 @@
 // EPOS-- AVR8 CPU Mediator Initialization
 
 #include <cpu.h>
+#include <mmu.h>
+#include <tsc.h>
 
 __BEGIN_SYS
 
@@ -18,6 +20,16 @@ void AVR8::init()
         out8(MCUCR, in8(MCUCR) & ~SE);
 	db<AVR8>(INF) << "AVR8:: Power Management Disabled\n";
     }
+
+    // Initialize the MMU
+    if(Traits<AVR8_MMU>::enabled)
+	AVR8_MMU::init();
+    else
+	db<Init, AVR8>(WRN) << "MMU is disabled!\n";
+
+    if(Traits<AVR8_TSC>::enabled)
+	AVR8_TSC::init();
+
 }
 
 __END_SYS
