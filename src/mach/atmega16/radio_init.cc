9d007b98c60782146693a08bb8ff8aa572153b3a
lucas <lucas@lisha.ufsc.br>
2008-03-28 21:59:10 +0000

    ATMega16 NIC is now a Dummy

--- src/mach/atmega16/radio_init.cc
+++ src/mach/atmega16/radio_init.cc
@@ -1,14 +1,3 @@
 // EPOS-- ATMega16 Radio (CC1000) NIC Mediator Initialization
 
-#include <system/kmalloc.h>
-#include <mach/atmega16/machine.h>
-#include <mach/atmega16/radio.h>
 
-__BEGIN_SYS
-
-void Radio::init(unsigned int unit)
-{
-    db<Init, Radio>(TRC) << "Radio::init(unit=" << unit << ")\n";
-}
-
-__END_SYS
