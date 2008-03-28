f618c289a5f8e8d6d0d79950bba9468f226f6839
roberto <roberto@lisha.ufsc.br>
2008-03-28 20:30:18 +0000

    __epos_free_init_mem() is called by __epos_library_app_entry to free system initialization memory before running main.

--- src/mach/atmega128/machine.cc
+++ src/mach/atmega128/machine.cc
@@ -20,3 +20,15 @@ extern "C" void __epos_call_handler(char offset) {
 	handler(0);
 }
 
+
+
+extern "C" void __epos_free_init_mem();
+
+extern "C" void __epos_free_init_mem() {
+    
+    MMU::free(Memory_Map<ATMega128>::TOP,
+	      Memory_Map<ATMega128>::MEM_SIZE - 
+	      Memory_Map<ATMega128>::TOP + 
+	      Memory_Map<ATMega128>::PHY_MEM);
+    
+}
