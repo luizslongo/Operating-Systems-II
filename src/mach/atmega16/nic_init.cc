5b592249b182da9821b916c617d77c368f4262a7
lucas <lucas@lisha.ufsc.br>
2008-03-28 21:59:03 +0000

    ATMega16 NIC is just a Dummy

--- src/mach/atmega16/nic_init.cc
+++ src/mach/atmega16/nic_init.cc
@@ -5,19 +5,10 @@
 
 __BEGIN_SYS
 
-template <int unit>
-inline static void call_init()
-{
-    Traits<ATMega16_NIC>::NICS::template Get<unit>::Result::init(unit);
-    call_init<unit + 1>();
-};
-
-template <> 
-inline static void call_init<Traits<ATMega16_NIC>::NICS::Length>() {};
 
 void ATMega16_NIC::init()
 {
-    call_init<0>();
+
 }
 
 __END_SYS
