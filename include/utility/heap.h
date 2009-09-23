66a4636a7c5bc5fb86f7cae2f705a6fa96a1f450
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2009-09-23 02:15:58 +0000

    Huge effort to spare 2 bytes (the spin-lock) for 8-bit microcontrollers. Now Heap has a SMP-wrapped (i.e. atomic) and a normal implementation.

--- include/utility/heap.h
+++ include/utility/heap.h
@@ -10,36 +10,132 @@
 
 __BEGIN_SYS
 
-class Heap: public Grouping_List<char>
+// Heap Common Package (actually the non-atomic heap)
+class Heap_Common: private Grouping_List<char>
 {
 public:
-    Heap() {
+    using Grouping_List<char>::empty;
+    using Grouping_List<char>::size;
+
+    Heap_Common() {
 	db<Init, Heap>(TRC) << "Heap() => " << this << "\n";
     }
 
-    Heap(void * addr, unsigned int bytes) {
+    Heap_Common(void * addr, unsigned int bytes) {
 	db<Init, Heap>(TRC) << "Heap(addr=" << addr << ",bytes=" << bytes 
 			    << ") => " << this << "\n";  
-	free(addr, bytes); 
     }
 
-    void * alloc(unsigned int bytes);
+    void * alloc(unsigned int bytes) {
+	if(!bytes)
+	    return 0;
+    
+	bytes += sizeof(int);
+	if(bytes < sizeof(Element))
+	    bytes = sizeof(Element);
+    
+	Element * e = search_decrementing(bytes);
+	if(!e) {
+	    out_of_memory();
+	    return 0;
+	}
+	int * addr = reinterpret_cast<int *>(e->object() + e->size());
+	    
+	db<Heap>(TRC) << "Heap::alloc(this=" << this
+		      << ",bytes=" << bytes 
+		      << ") => " << (void *)addr << "\n";
+
+	addr[0] = bytes;
+	return &addr[1];
+    }
+
     void * calloc(unsigned int bytes) {
 	void * addr = alloc(bytes);
 	memset(addr, bytes, 0);
 	return addr;	
     }
-    void * realloc(void * ptr, unsigned int bytes);
 
     void free(void * ptr) {
 	int * addr = reinterpret_cast<int *>(ptr);
 	free(&addr[-1], addr[-1]);
     }
-    void free(void * ptr, unsigned int bytes);
+
+    void free(void * ptr, unsigned int bytes) {
+	db<Heap>(TRC) << "Heap::free(this=" << this
+		      << ",ptr=" << ptr
+		      << ",bytes=" << bytes << ")\n";
+
+	if(ptr && (bytes >= sizeof(Element))) {
+	    Element * e = new (ptr)
+		Element(reinterpret_cast<char *>(ptr), bytes);
+	    Element * m1, * m2;
+	    insert_merging(e, &m1, &m2);
+	}
+    }
+
+private:
+    void out_of_memory();
+};
+
+
+// Wrapper for non-atomic heap  
+template <bool atomic>
+class Heap_Wrapper: public Heap_Common {
+public:
+    Heap_Wrapper() {}
+
+    Heap_Wrapper(void * addr, unsigned int bytes): Heap_Common(addr, bytes) {
+	free(addr, bytes); 
+    }
+};
+
+
+// Wrapper for atomic heap
+template<>
+class Heap_Wrapper<true>: public Heap_Common
+{
+public:
+    Heap_Wrapper() {}
+
+    Heap_Wrapper(void * addr, unsigned int bytes): Heap_Common(addr, bytes) {
+	_lock.acquire();
+	free(addr, bytes); 
+	_lock.release();
+    }
+
+    void * alloc(unsigned int bytes) {
+	_lock.acquire();
+	void * tmp = Heap_Common::alloc(bytes);
+	_lock.release();
+	return tmp;
+    }
+
+    void * calloc(unsigned int bytes) {
+	_lock.acquire();
+	void * tmp = Heap_Common::calloc(bytes);
+	_lock.release();
+	return tmp;
+    }
+
+    void free(void * ptr) {
+	_lock.acquire();
+	Heap_Common::free(ptr);
+	_lock.release();
+    }
+
+    void free(void * ptr, unsigned int bytes) {
+	_lock.acquire();
+	Heap_Common::free(ptr, bytes);
+	_lock.release();
+    }
 
 private:
     Spin _lock;
 };
+
+
+// Heap
+class Heap: public Heap_Wrapper<Traits<Thread>::smp> {};
 
 __END_SYS
 
--- src/utility/heap.cc
+++ src/utility/heap.cc
@@ -6,45 +6,10 @@
 __BEGIN_SYS
 
 // Methods
-void * Heap::alloc(unsigned int bytes)
+void Heap_Common::out_of_memory()
 {
-    if(!bytes)
-	return 0;
-    
-    bytes += sizeof(int);
-    
-    _lock.acquire();
-    Element * e = search_decrementing(bytes);
-    if(!e) {
-	db<Heap>(ERR) << "Heap::alloc: out of memory!\n";
-	Machine::panic();
-	_lock.release();
-	return 0;
-    }
-    int * addr = reinterpret_cast<int *>(e->object() + e->size());
-	    
-    db<Heap>(TRC) << "Heap::alloc(this=" << this
-		  << ",bytes=" << bytes 
-		  << ") => " << (void *)addr << "\n";
-    addr[0] = bytes;
-    _lock.release();
-    return &addr[1];
-}
-
-void Heap::free(void * ptr, unsigned int bytes)
-{
-    db<Heap>(TRC) << "Heap::free(this=" << this
-		  << ",ptr=" << ptr
-		  << ",bytes=" << bytes << ")\n";
-
-    if(ptr && (bytes >= sizeof(Element))) {
-	_lock.acquire();
-	Element * e = new (ptr)
-	    Element(reinterpret_cast<char *>(ptr), bytes);
-	Element * m1, * m2;
-	insert_merging(e, &m1, &m2);
-	_lock.release();
-    }
+    db<Heap>(ERR) << "Heap::alloc: out of memory!\n";
+    Machine::panic();
 }
 
 __END_SYS
