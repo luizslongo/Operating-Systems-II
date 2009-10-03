dc998b21202e62420ffc245c377fd040c8fdbccf
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2009-10-03 01:14:38 +0000

    Display is now a singleton.

--- src/mach/pc/rtc_test.cc
+++ src/mach/pc/rtc_test.cc
@@ -19,16 +19,15 @@ int main()
 
     cout << "It's now " << t0 << " seconds since epoch.\n";
 
-    Display disp;
     RTC::Date last_date = rtc.date();
     while(rtc.seconds_since_epoch() < t0 + TEST_DURATION) {
 	if(last_date.second() != rtc.date().second()) {
 	    last_date = rtc.date();
-	    disp.position(20, 30);
+	    Display::position(20, 30);
 	    cout << last_date.day() << '/'
 		 << last_date.month() << '/'
 		 << last_date.year() << ' ';
-	    disp.position(20, 40);
+	    Display::position(20, 40);
 	    cout << last_date.hour() << ':'
 		 << last_date.minute() << ':'
 		 << last_date.second() << "    ";
