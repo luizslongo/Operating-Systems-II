100e50af77846b5cc759c2bdfc7cd3153dfa9389
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2009-10-03 01:07:58 +0000

    Adding Timer::init()

--- src/mach/pc/machine_init.cc
+++ src/mach/pc/machine_init.cc
@@ -12,6 +12,8 @@ void PC::init()
 	PC_IC::init();
     if(Traits<PC_PCI>::enabled)
 	PC_PCI::init();
+    if(Traits<PC_Timer>::enabled)
+	PC_Timer::init();
     if(Traits<PC_NIC>::enabled)
 	PC_NIC::init();
 }
