e05461f917f723ce4959dfb477cb3e57cba4423e
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2009-10-09 01:14:08 +0000

    Performance increased by limiting PC_Timer to two channels: ALARM and SCHEDULER.

--- src/mach/pc/timer.cc
+++ src/mach/pc/timer.cc
@@ -11,19 +11,10 @@ PC_Timer * PC_Timer::_channels[CHANNELS];
 // Class methods
 void PC_Timer::int_handler(unsigned int i)
 {
-    if(Machine::cpu_id() == 0) {
-	if(_channels[ALARM])
-	    _channels[ALARM]->_handler();
+    if((Machine::cpu_id() == 0) && _channels[ALARM])
+	_channels[ALARM]->_handler();
 
-	if(_channels[USER] && _channels[USER]->_enabled
-	   && (--_channels[USER]->_current <= 0)) {
-	    _channels[USER]->_current = _channels[USER]->_initial;
-	    _channels[USER]->_handler();
-	}
-    }
-
-    if(_channels[SCHEDULER] && _channels[SCHEDULER]->_enabled
-       && (--_channels[SCHEDULER]->_current <= 0)) {
+    if(_channels[SCHEDULER] && (--_channels[SCHEDULER]->_current <= 0)) {
 	_channels[SCHEDULER]->_current = _channels[SCHEDULER]->_initial;
 	_channels[SCHEDULER]->_handler();
     }
