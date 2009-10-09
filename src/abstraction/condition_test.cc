ac0dd6f0dfb065593afc9fc7adb5c9e5728f62c0
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2009-10-09 01:25:07 +0000

    Bug fix: condition variables cannot be used as chopsticks in the Philosopher's Dinner, for they are "memory less". Barrier preserved, but chopsticks replaced by mutexes.

--- src/abstraction/condition_test.cc
+++ src/abstraction/condition_test.cc
@@ -1,27 +1,27 @@
 // EPOS-- Semaphore Test Program
 
 #include <utility/ostream.h>
-#include <thread.h>
-#include <condition.h>
-#include <alarm.h>
 #include <display.h>
+#include <thread.h>
+#include <alarm.h>
+#include <mutex.h>
+#include <condition.h>
 
 __USING_SYS
 
 const int iterations = 10;
 
 Thread * phil[5];
-Condition chopstick[5];
+Mutex chopstick[5];
 Condition barrier;
 
 OStream cout;
 
 int philosopher(int n, int l, int c)
 {
-    Display display;
     
-//     cout << "I'm the philosopher " << n << "\n";
-//     cout << "I'll print at " << l << " x " << c << "\n";
+    cout << "I'm the philosopher " << n << "\n";
+    cout << "I'll print at " << l << " x " << c << "\n";
  
     int first = (n < 4)? n : 0;
     int second = (n < 4)? n + 1 : 4;
@@ -29,17 +29,17 @@ int philosopher(int n, int l, int c)
     barrier.wait();
 
     for(int i = iterations; i > 0; i--) {
-	display.position(l, c);
+	Display::position(l, c);
  	cout << "thinking";
-	Alarm::delay(1000000);
+	Delay thinking(1000000);
 
-	chopstick[first].wait();   // get first chopstick
-	chopstick[second].wait();   // get second chopstick
-	display.position(l, c);
+	chopstick[first].lock();   // get first chopstick
+	chopstick[second].lock();   // get second chopstick
+	Display::position(l, c);
 	cout << " eating ";
-	Alarm::delay(500000);
-	chopstick[first].signal();   // release first chopstick
-	chopstick[second].signal();   // release second chopstick
+	Delay eating(500000);
+	chopstick[first].unlock();   // release first chopstick
+	chopstick[second].unlock();   // release second chopstick
     }
 
     return(iterations);
@@ -49,13 +49,9 @@ int main()
 {
     Display display;
 
-    display.clear();
+    Display::clear();
     cout << "The Philosopher's Dinner:\n";
 
-//     for(int i = 0; i < 5; i++)
-// 	chopstick[i] = new Condition;
-//     barrier = new Condition;
-    
     phil[0] = new Thread(&philosopher, 0, 5, 32);
     phil[1] = new Thread(&philosopher, 1, 10, 44);
     phil[2] = new Thread(&philosopher, 2, 16, 39);
@@ -64,32 +60,29 @@ int main()
 
     cout << "Philosophers are alife and hungry!\n";
 
-    cout << "The dinner is served!\n";
-    display.position(7, 44);
-    cout << '/';
-    display.position(13, 44);
-    cout << '\\';
-    display.position(16, 35);
-    cout << '|';
-    display.position(13, 27);
-    cout << '/';
-    display.position(7, 27);
-    cout << '\\';
-
     Alarm::delay(2000000);
     barrier.broadcast();
 
-    for(int i = 0; i < 5; i++)
-	chopstick[i].signal();
+    cout << "The dinner is served!\n";
+    Display::position(7, 44);
+    cout << '/';
+    Display::position(13, 44);
+    cout << '\\';
+    Display::position(16, 35);
+    cout << '|';
+    Display::position(13, 27);
+    cout << '/';
+    Display::position(7, 27);
+    cout << '\\';
+
+    Alarm::delay(6000000);
 
     for(int i = 0; i < 5; i++) {
 	int ret = phil[i]->join();
-	display.position(20 + i, 0);
+	Display::position(20 + i, 0);
 	cout << "Philosopher " << i << " ate " << ret << " times \n";
     }
 
-//     for(int i = 0; i < 5; i++)
-// 	delete chopstick[i];
     for(int i = 0; i < 5; i++)
 	delete phil[i];
     
