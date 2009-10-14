69d0f4bd21d774bbb61c19898a4b0cfae797b2e4
Giovani Gracioli <giovani@lisha.ufsc.br>
2009-10-14 16:54:04 +0000

    __epos_call_handler has moved to ic_init.cc.

--- src/mach/atmega128/machine.cc
+++ src/mach/atmega128/machine.cc
@@ -2,24 +2,7 @@
 
 #include <mach/atmega128/machine.h>
 
-__BEGIN_SYS
-
-ATMega128::int_handler * ATMega128::_int_vector[INT_VECTOR_SIZE];
-
-__END_SYS
-
-
 __USING_SYS
-
-extern "C" void __epos_call_handler(char offset);
-
-extern "C" void __epos_call_handler(char offset) {
-    Machine::int_handler * handler = Machine::int_vector(offset);
-
-    if (handler != 0)
-	handler(0);
-}
-
 
 
 extern "C" void __epos_free_init_mem();
