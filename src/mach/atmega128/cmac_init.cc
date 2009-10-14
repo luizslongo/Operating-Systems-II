5f7759dfb2a5836dc3a48cf7a3deae299432b6b8
Giovani Gracioli <giovani@lisha.ufsc.br>
2009-10-14 17:01:47 +0000

    IC is responsible for handling interrupts.

--- src/mach/atmega128/cmac_init.cc
+++ src/mach/atmega128/cmac_init.cc
@@ -7,10 +7,9 @@ __BEGIN_SYS
 
 void CMAC::init(unsigned int n) {
 
-    Machine::int_vector(IC::IRQ_TIMER2_COMP, &timer_handler);
+    IC::int_vector(IC::IRQ_TIMER2_COMP, &timer_handler);
 
-    Machine::int_vector(IC::IRQ_SPI_STC, &spi_handler);
-    
+    IC::int_vector(IC::IRQ_SPI_STC, &spi_handler);
 
     _timer.frequency(30);
 
