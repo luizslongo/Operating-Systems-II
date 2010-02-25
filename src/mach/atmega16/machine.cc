8f7368d8337fbb1c5f4855d363cd4394c50bac36
rodrigo <rodrigo@lisha.ufsc.br>
2010-02-25 19:18:17 +0000

    Changes in interrupt handlers.

--- src/mach/atmega16/machine.cc
+++ src/mach/atmega16/machine.cc
@@ -2,24 +2,6 @@
 
 #include <mach/atmega16/machine.h>
 
-__BEGIN_SYS
-
-ATMega16::int_handler * ATMega16::_int_vector[INT_VECTOR_SIZE];
-
-__END_SYS
-
-
-__USING_SYS
-
-extern "C" void __epos_call_handler(char offset);
-
-extern "C" void __epos_call_handler(char offset) {
-    Machine::int_handler * handler = Machine::int_vector(offset);
-
-    if (handler != 0)
-	handler(0);
-}
-
 extern "C" void __epos_free_init_mem();
 
 extern "C" void __epos_free_init_mem() {
