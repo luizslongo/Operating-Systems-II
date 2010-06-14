e8a35369ca32bbd9d20e1be74478669c17829c4a
Arliones Hoeller <arliones@lisha.ufsc.br>
2010-06-14 14:26:38 +0000

    More complete test. Using photo sensor.

--- app/atmega128_app.cc
+++ app/atmega128_app.cc
@@ -6,17 +6,30 @@
 #include <thread.h>
 #include <alarm.h>
 #include <timer.h>
+#include <sensor.h>
 
 __USING_SYS
 
+OStream cout;
+unsigned char count = 1;
+
 int main() {
-    unsigned char count = 0;
+	ATMega128_Photo_Sensor sensor;
+	UART uart;
 
     CPU::out8(Machine::IO::DDRA, 0xff);
     CPU::out8(Machine::IO::PORTA, ~0);
 
+    union {
+    	int s;
+    	char b[2];
+    } temp;
+
     while(1) {
-	CPU::out8(Machine::IO::PORTA, ~count++);
-	Alarm::delay(1000000);
+		CPU::out8(Machine::IO::PORTA, ~count++);
+		Alarm::delay(1000000);
+    	temp.s = sensor.sample();
+    	uart.put(temp.b[1]);
+    	uart.put(temp.b[0]);
     }
 }
