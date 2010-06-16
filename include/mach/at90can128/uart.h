8e6dc232799750157d525d83e16b21e649da71a6
Arliones Hoeller <arliones@lisha.ufsc.br>
2010-06-16 19:49:38 +0000

    power management for AVR_UART reviewed.

--- include/mach/at90can128/uart.h
+++ include/mach/at90can128/uart.h
@@ -10,13 +10,6 @@ __BEGIN_SYS
 class AT90CAN128_UART: public UART_Common, public AVR_UART
 {
 public:
-    enum {
-    FULL                = AVR_UART::FULL,
-    LIGHT               = AVR_UART::LIGHT,
-    STANDBY             = AVR_UART::STANDBY,
-    OFF                 = AVR_UART::OFF
-    };
-public:
     AT90CAN128_UART(unsigned int unit = 0) : AVR_UART(9600,8,0,1,unit) {}
     AT90CAN128_UART(unsigned int baud, unsigned int data_bits, unsigned int parity,
 	       unsigned int stop_bits, unsigned int unit = 0) 
@@ -35,9 +28,6 @@ public:
     void put(char c) { while(!txd_empty()); txd(c); }
 
     void loopback(bool flag) { AVR_UART::loopback(flag); }
-
-    char power() { return AVR_UART::power(); }
-    void power(char ps) { AVR_UART::power(ps); }
 };
 
 __END_SYS
