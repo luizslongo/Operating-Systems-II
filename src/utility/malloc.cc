8ce3df91a5303fbc8603d018e8d5a2d568e132ff
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2010-09-15 01:02:45 +0000

    The only thing EPOS still uses from the standard C lib are string/memory operations and malloc. From now on, those dependencies are supposedly gone. Libc symbols are declared in the standard namespace and have C linkage. This forced instantiation of functions in malloc.h is meant to be used by GCC internally (e.g. implicit virtual destructors).

--- src/utility/malloc.cc
+++ src/utility/malloc.cc
@@ -1,5 +1,24 @@
 // EPOS Application-level Dynamic Memory Utility Implementation
 
-// Backup copies of inline memory-realted functions
-//#pragma implementation "malloc.h"
+#define __malloc_h
+
 #include <utility/malloc.h>
+#include <application.h>
+
+__USING_SYS
+
+extern "C"
+{
+    void * malloc(size_t bytes) {
+	return Application::heap()->alloc(bytes);
+    }
+
+    void * calloc(size_t n, unsigned int bytes) {
+	return Application::heap()->calloc(n * bytes);
+    }
+
+    void free(void * ptr) {
+	Application::heap()->free(ptr);
+    }
+}
+
