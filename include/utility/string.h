252dcaec1c6afa975a556d8491455eeec873c163
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2010-09-15 00:55:44 +0000

    The only thing EPOS still uses from the standard C lib are string/memory operations and malloc. From now on, those dependencies are supposedly gone. Libc symbols are declared in the standard namespace and have C linkage.

--- include/utility/string.h
+++ include/utility/string.h
@@ -3,40 +3,17 @@
 #ifndef __string_h
 #define __string_h
 
-int memcmp(const void * s1, const void * s2, unsigned int n);
-void * memcpy(void * d, const void * s, unsigned int n);
-void * memset(void * d, int c, unsigned int n);
-int strcmp(const char * s1, const char * s2);
-int strncmp(const char * s1, const char * s2, unsigned int n);
-char * strcpy(char * d, const char * s);
-char * strncpy(char * d, const char * s, unsigned int n);
-unsigned int strlen(const char * s);
-
-/*
-inline int memcmp(void * d, const void * s, unsigned int n) {
-    return __builtin_memcmp(d, s, n);
+extern "C" {
+    int memcmp(const void * m1, const void * m2, size_t n);
+    void * memcpy(void * d, const void * s, size_t n);
+    void * memset(void * m, int c, size_t n);
+    void * memchr(const void * m, int c, size_t n);
+    int strcmp(const char * s1, const char * s2);
+    int strncmp(const char * s1, const char * s2, size_t n);
+    char * strcpy(char * d, const char * s);
+    char * strncpy(char * d, const char * s, size_t n);
+    char *strchr(const char * s, int c);
+    size_t strlen(const char * s);
 }
-inline void * memcpy(void * d, const void * s, unsigned int n) {
-    return __builtin_memcpy(d, s, n);
-}
-inline void * memset(void * d, int c, unsigned int n) {
-    return __builtin_memset(d, c, n);
-}
-inline int strcmp(char * d, const char * s) {
-    return __builtin_strcmp(d, s);
-}
-inline int strncmp(char * d, const char * s, unsigned int n) {
-    return __builtin_memcmp(d, s, n);
-}
-inline char * strcpy(char * d, const char * s) {
-    return __builtin_strcpy(d, s);
-}
-inline char * strncpy(char * d, const char * s, unsigned int n) {
-    return (char *)__builtin_memcpy(d, s, n);
-}
-inline unsigned int strlen(const char * s) {
-    return __builtin_strlen(s);
-}
-*/
 
 #endif
