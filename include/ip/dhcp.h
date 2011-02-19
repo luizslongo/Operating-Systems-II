ea75d31f7cd170906a28918ff308e701c3ea5769
rodrigovr <rodrigovr@lisha.ufsc.br>
2011-02-19 22:09:48 +0000

    just style. replaced all tabs with 4 spaces

--- include/ip/dhcp.h
+++ include/ip/dhcp.h
@@ -12,166 +12,166 @@ __BEGIN_SYS
  */
 class DHCP {
 public:
-	// Limited to opt_size = 308
+    // Limited to opt_size = 308
 
-	template<int opt_size> class Packet {
-	public:
-			u8 _op, _htype, _hlen, _hopts;
-			u32 _xid;
-			u16 _secs, _flags;
-			u32 _ciaddr, _yiaddr, _siaddr, _giaddr;
-			u8 _chaddr[16];
-			u8 _sname[64];
-			u8 _file[128];
-			u8 _magic[4];
-			u8 _options[opt_size];
-			u8 _end;
-			u8 _padding[312 - 5 - opt_size];
+    template<int opt_size> class Packet {
+    public:
+        u8 _op, _htype, _hlen, _hopts;
+        u32 _xid;
+        u16 _secs, _flags;
+        u32 _ciaddr, _yiaddr, _siaddr, _giaddr;
+        u8 _chaddr[16];
+        u8 _sname[64];
+        u8 _file[128];
+        u8 _magic[4];
+        u8 _options[opt_size];
+        u8 _end;
+        u8 _padding[312 - 5 - opt_size];
 
-			u8 op() const { return _op; }
-			u32 xid() const { return _xid; }
-			u16 secs() const { return CPU::ntohs(_secs); }
-			u32 your_address() const { return CPU::ntohl(_yiaddr); }
-			u32 server_address() const { return CPU::ntohl(_siaddr); }
-			u8 * options() const { return const_cast<u8 * const>(_options); }
+        u8 op() const { return _op; }
+        u32 xid() const { return _xid; }
+        u16 secs() const { return CPU::ntohs(_secs); }
+        u32 your_address() const { return CPU::ntohl(_yiaddr); }
+        u32 server_address() const { return CPU::ntohl(_siaddr); }
+        u8 * options() const { return const_cast<u8 * const>(_options); }
 
-			Packet()
-			{
-				memset(&_op, 0, sizeof(Packet));
-				_magic[0] =  99; // magic cookie
-				_magic[1] = 130;
-				_magic[2] =  83;
-				_magic[3] =  99;
-				_end = 255; // end of options
-			}
+        Packet()
+        {
+            memset(&_op, 0, sizeof(Packet));
+            _magic[0] =  99; // magic cookie
+            _magic[1] = 130;
+            _magic[2] =  83;
+            _magic[3] =  99;
+            _end = 255; // end of options
+        }
 
-	};
+    };
 
-	class Discover : public Packet<3> {
-	public:
-		Discover(IP * _net) : Packet<3>() {
-			_op = 1;
-			_htype = 1;
-			_hlen = _net->hw_address_len();
-			_xid = Pseudo_Random::random();
-			memcpy(_chaddr, &_net->hw_address(), _hlen);
-			_options[0] = 53; // DHCPMSG
-			_options[1] = 1;  // message size
-			_options[2] = 1;  // dhcp discover
-		}
+    class Discover : public Packet<3> {
+    public:
+        Discover(IP * _net) : Packet<3>() {
+            _op = 1;
+            _htype = 1;
+            _hlen = _net->hw_address_len();
+            _xid = Pseudo_Random::random();
+            memcpy(_chaddr, &_net->hw_address(), _hlen);
+            _options[0] = 53; // DHCPMSG
+            _options[1] = 1;  // message size
+            _options[2] = 1;  // dhcp discover
+        }
 
-	};
+    };
 
-	class Request : public Packet<8> {
-	public:
-		Request(IP * _net,const Packet<255> * discovered) : Packet<8>() {
-			_op = 1;
-			_htype = 1;
-			_hlen = _net->hw_address_len();
-			_xid = discovered->_xid;
-			_ciaddr = discovered->_ciaddr;
-			_siaddr = discovered->_siaddr;
-			memcpy(_chaddr, &_net->hw_address(), _hlen);
-			_options[0] = 53; // DHCP message
-			_options[1] = 1;  // size
-			_options[2] = 3;  // dhcp discover
-			_options[3] = 55; // parameter request
-			_options[4] = 3;  // size
-			_options[5] = 1;  // subnet
-			_options[6] = 3;  // router
-			_options[7] = 6;  // dns
-		}
-	};
+    class Request : public Packet<8> {
+    public:
+        Request(IP * _net,const Packet<255> * discovered) : Packet<8>() {
+            _op = 1;
+            _htype = 1;
+            _hlen = _net->hw_address_len();
+            _xid = discovered->_xid;
+            _ciaddr = discovered->_ciaddr;
+            _siaddr = discovered->_siaddr;
+            memcpy(_chaddr, &_net->hw_address(), _hlen);
+            _options[0] = 53; // DHCP message
+            _options[1] = 1;  // size
+            _options[2] = 3;  // dhcp discover
+            _options[3] = 55; // parameter request
+            _options[4] = 3;  // size
+            _options[5] = 1;  // subnet
+            _options[6] = 3;  // router
+            _options[7] = 6;  // dns
+        }
+    };
 
-	class Client;
+    class Client;
 };
 
 class DHCP::Client : public UDP::Socket {
 public:
-	enum {
-		IDLE,
-		DISCOVER,
-		REQUEST,
-		RENEW,
-		RELEASE
-	};
+    enum {
+        IDLE,
+        DISCOVER,
+        REQUEST,
+        RENEW,
+        RELEASE
+    };
 protected:
-	short _state;
+    short _state;
 
-	u32 _xid;
+    u32 _xid;
 
-	IP::Address _ip, _mask, _gw, _bcast, _ns;
+    IP::Address _ip, _mask, _gw, _bcast, _ns;
 public:
-	Client(UDP * udp) :
-		UDP::Socket(udp,UDP::Address(0,68),UDP::Address(~0,67)),
-		_state(IDLE)
-	{
+    Client(UDP * udp) :
+        UDP::Socket(udp,UDP::Address(0,68),UDP::Address(~0,67)),
+        _state(IDLE)
+    {
 
-	}
+    }
 
-	~Client() {}
+    ~Client() {}
 
-	// UDP::Socket virtual methods
-	void received(const UDP::Address & src,const char *data, unsigned int size)
-	{
-		db<IP>(INF) << "DHCP::Client state: " << _state << "\n";
-		const DHCP::Packet<255> * packet = reinterpret_cast<const DHCP::Packet<255> *>(data);
+    // UDP::Socket virtual methods
+    void received(const UDP::Address & src,const char *data, unsigned int size)
+    {
+        db<IP>(INF) << "DHCP::Client state: " << _state << "\n";
+        const DHCP::Packet<255> * packet = reinterpret_cast<const DHCP::Packet<255> *>(data);
 
-		if (_xid != packet->xid()) {
-			db<IP>(TRC) << "This DHCP message does not belong to me\n";
-		}
+        if (_xid != packet->xid()) {
+            db<IP>(TRC) << "This DHCP message does not belong to me\n";
+        }
 
-		switch(_state) {
-		case DISCOVER:
+        switch(_state) {
+        case DISCOVER:
 
-			if (packet->your_address()) {
-				set_remote(src);
+            if (packet->your_address()) {
+                set_remote(src);
 
-				UDP::Address me(packet->your_address(),68);
-			//	set_local(me);
-			//	_udp->ip()->set_address(me.ip());
+                UDP::Address me(packet->your_address(),68);
+            //  set_local(me);
+            //  _udp->ip()->set_address(me.ip());
 
-				_state = REQUEST;
-				db<IP>(INF) << "Server " << src.ip() << " offered IP " << me.ip() << "\n";
+                _state = REQUEST;
+                db<IP>(INF) << "Server " << src.ip() << " offered IP " << me.ip() << "\n";
                 parse_options(packet);
 
-				DHCP::Request req(_udp->ip(),packet);
-				SegmentedBuffer sb(&req,sizeof(DHCP::Request));
-				send(&sb);
-			}
+                DHCP::Request req(_udp->ip(),packet);
+                SegmentedBuffer sb(&req,sizeof(DHCP::Request));
+                send(&sb);
+            }
 
-			break;
+            break;
 
-		case REQUEST:
+        case REQUEST:
 
-			if (packet->your_address()) {
-				_ip = IP::Address((u32)packet->your_address());
+            if (packet->your_address()) {
+                _ip = IP::Address((u32)packet->your_address());
 
                 parse_options(packet);
-				_state = RENEW;
+                _state = RENEW;
 
-			}
-		}
-	}
+            }
+        }
+    }
 
-	void configure() {
-		db<IP>(INF) << "DHCP::Client sending discover msg\n";
-		_state = DISCOVER;
+    void configure() {
+        db<IP>(INF) << "DHCP::Client sending discover msg\n";
+        _state = DISCOVER;
 
-		DHCP::Discover pkt(_udp->ip());
-		SegmentedBuffer sb(&pkt,sizeof(DHCP::Discover));
+        DHCP::Discover pkt(_udp->ip());
+        SegmentedBuffer sb(&pkt,sizeof(DHCP::Discover));
 
-		_xid = pkt.xid();
-		send(&sb);
-	}
+        _xid = pkt.xid();
+        send(&sb);
+    }
 
-	void renew() {
+    void renew() {
 
-	}
+    }
 
-	void release() {
+    void release() {
 
-	}
+    }
 
     void parse_options(const Packet<255> * packet) {
         db<IP>(TRC) << "DHCP::Parsing OPTIONS\n";
@@ -219,14 +219,14 @@ public:
                 }
                 i += opt[i];
                 break;
-            case 58: // renew time in secs
+            case 51: // lease time in secs
                 ++i;
                 if (opt[i] == 4) { // Good size!
                     _renew_time = (((u32)(opt[i+1]) << 24) & 0xFF000000) |
                                   (((u32)(opt[i+2]) << 16) & 0x00FF0000) |
                                   (((u32)(opt[i+3]) << 8 ) & 0x0000FF00) |
                                   (((u32)(opt[i+4])      ) & 0x000000FF);
-                    db<IP>(TRC) << "Lease time " << _renew_time << endl;
+                    db<IP>(TRC) << "Lease time " << _lease_time << endl;
                 }
                 i += opt[i];
                 break;
@@ -246,13 +246,13 @@ public:
         }
     }
 
-    unsigned long _renew_time;
+    unsigned long _lease_time;
 
-	IP::Address address() { return _ip; }
-	IP::Address netmask() { return _mask; }
-	IP::Address gateway() { return _gw; }
-	IP::Address brodcast() { return _bcast; }
-	IP::Address nameserver() { return _ns; }
+    IP::Address address() { return _ip; }
+    IP::Address netmask() { return _mask; }
+    IP::Address gateway() { return _gw; }
+    IP::Address brodcast() { return _bcast; }
+    IP::Address nameserver() { return _ns; }
 };
 
 __END_SYS
