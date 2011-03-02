410392427d1ba6b83beaf82e9d2dff3b983eb290
rodrigovr <rodrigovr@lisha.ufsc.br>
2011-03-02 19:59:03 +0000

    just style

--- include/ip/ip.h
+++ include/ip/ip.h
@@ -18,7 +18,7 @@ typedef signed   long  s32;
 __BEGIN_SYS
 class IP_Address : public NIC_Common::Address<4> {
 public:
-	IP_Address() {}
+    IP_Address() {}
     IP_Address(unsigned char addr[4]) 
         : NIC_Common::Address<4>(addr[0],addr[1],addr[2],addr[3]) {}
             
@@ -26,26 +26,26 @@ public:
         addr = CPU::htonl(addr);
         memcpy(this, &addr, sizeof(addr)); 
     }
-	IP_Address(u8 a0, u8 a1 = 0, u8 a2 = 0, u8 a3 = 0)
-		: NIC_Common::Address<4>(a0, a1, a2, a3) {}
+    IP_Address(u8 a0, u8 a1 = 0, u8 a2 = 0, u8 a3 = 0)
+        : NIC_Common::Address<4>(a0, a1, a2, a3) {}
 
-	operator u32() { return *reinterpret_cast<u32 *>(this); }
-	operator u32() const { return *reinterpret_cast<const u32 *>(this); }
-		
-	IP_Address(const char * _addr) {
-		unsigned char addr[4];
-		addr[0] = 0; addr[1] = 0; addr[2] = 0; addr[3] = 0;
-		int i;
-		for(i=0;i<4;++i) {
-			char * sep = strchr(_addr,'.');
-			addr[i] = atol(_addr);
-			if (!sep) break;
-			_addr = ++sep;
-		}
-		memcpy(this,addr,sizeof(this));
-	}
-	
-	friend Debug& operator<<(Debug& db,const IP_Address& addr) {
+    operator u32() { return *reinterpret_cast<u32 *>(this); }
+    operator u32() const { return *reinterpret_cast<const u32 *>(this); }
+        
+    IP_Address(const char * _addr) {
+        unsigned char addr[4];
+        addr[0] = 0; addr[1] = 0; addr[2] = 0; addr[3] = 0;
+        int i;
+        for(i=0;i<4;++i) {
+            char * sep = strchr(_addr,'.');
+            addr[i] = atol(_addr);
+            if (!sep) break;
+            _addr = ++sep;
+        }
+        memcpy(this,addr,sizeof(this));
+    }
+    
+    friend Debug& operator<<(Debug& db,const IP_Address& addr) {
         const u8 * _addr = reinterpret_cast<const u8*>(&addr);
         db << dec << (int)(_addr[0]) << '.' << (int)(_addr[1]) 
            << '.' << (int)(_addr[2]) << '.' << (int)(_addr[3]);
@@ -58,187 +58,187 @@ class IP: public NIC_Common, /*public NIC::Observer,*/ public Data_Observed<IP_A
 {
 public:
 
-	typedef Data_Observer<IP_Address> Observer;
-	typedef IP_Address                Address;
-	typedef NIC::Address              MAC_Address;
-	typedef u8                        Protocol;
-	typedef ARP<NIC, IP>              _ARP;
+    typedef Data_Observer<IP_Address> Observer;
+    typedef IP_Address                Address;
+    typedef NIC::Address              MAC_Address;
+    typedef u8                        Protocol;
+    typedef ARP<NIC, IP>              _ARP;
 
-	static const u16 MTU = ~0;
-	static const Address BROADCAST;
-	static const Address NULL;
+    static const u16 MTU = ~0;
+    static const Address BROADCAST;
+    static const Address NULL;
 
-	enum {
-		PROT_IP   = NIC::IP,
-		PROT_ARP  = NIC::ARP,
-		PROT_RARP = NIC::RARP
-	};
+    enum {
+        PROT_IP   = NIC::IP,
+        PROT_ARP  = NIC::ARP,
+        PROT_RARP = NIC::RARP
+    };
 
-	class Header {
-	public:
-		enum {
-			MF_FLAG = 1, // More Fragments
-			DF_FLAG = 2  // Don't Fragment
-		};
+    class Header {
+    public:
+        enum {
+            MF_FLAG = 1, // More Fragments
+            DF_FLAG = 2  // Don't Fragment
+        };
 
-	public:
-		Header() {}
+    public:
+        Header() {}
 
-		Header(const Address & src, const Address & dst, const Protocol & prot, u16 size) :
-			_ihl(DEF_IHL), 	_version(DEF_VER), _tos(DEF_TOS),
-			_length(CPU::htons(sizeof(Header) + size)), _id(CPU::htons(pktid++)),
-			_offset(0), _flags(0), _ttl(DEF_TTL), _protocol(prot), _checksum(0),
-			_src_ip(src), _dst_ip(dst)
-		{
-			calculate_checksum();
-		}
+        Header(const Address & src, const Address & dst, const Protocol & prot, u16 size) :
+            _ihl(DEF_IHL),  _version(DEF_VER), _tos(DEF_TOS),
+            _length(CPU::htons(sizeof(Header) + size)), _id(CPU::htons(pktid++)),
+            _offset(0), _flags(0), _ttl(DEF_TTL), _protocol(prot), _checksum(0),
+            _src_ip(src), _dst_ip(dst)
+        {
+            calculate_checksum();
+        }
 
-		const Address & src_ip() const { return _src_ip; }
-		const Address & dst_ip() const { return _dst_ip; }
-		u32 hlength() { return _ihl * 4; }
-		u32 length() const { return CPU::ntohs(_length); }
-		u16 flags() const {
-			return CPU::ntohs(_flags << 13 | _offset) >> 13;
-		}
-		u16 offset() const {
-			return CPU::ntohs(_flags << 13 | _offset) & 0x1fff;
-		}
-		const Protocol & protocol() const { return _protocol; }
-		u16 id() const { return CPU::ntohs(_id); }
+        const Address & src_ip() const { return _src_ip; }
+        const Address & dst_ip() const { return _dst_ip; }
+        u32 hlength() { return _ihl * 4; }
+        u32 length() const { return CPU::ntohs(_length); }
+        u16 flags() const {
+            return CPU::ntohs(_flags << 13 | _offset) >> 13;
+        }
+        u16 offset() const {
+            return CPU::ntohs(_flags << 13 | _offset) & 0x1fff;
+        }
+        const Protocol & protocol() const { return _protocol; }
+        u16 id() const { return CPU::ntohs(_id); }
 
-		// setters for fragment operations
-		void set_src(const Address & src_ip){ _src_ip = src_ip; }
-		void set_length(u16 length) { _length = CPU::htons(length); }
-		void set_offset(u16 off) {
-			u16 x = CPU::htons(flags()<<13|off);
-			_offset = x&0x1fff;
-			_flags  = x>>13;
-		}
-		void set_flags(u16 flg) {
-			u16 x = CPU::htons(flg<<13|offset());
-			_offset = x&0x1fff;
-			_flags  = x>>13;
-		}
-		void set_protocol(u8 protocol){
-			_protocol = protocol;
-		}
+        // setters for fragment operations
+        void set_src(const Address & src_ip){ _src_ip = src_ip; }
+        void set_length(u16 length) { _length = CPU::htons(length); }
+        void set_offset(u16 off) {
+            u16 x = CPU::htons(flags()<<13|off);
+            _offset = x&0x1fff;
+            _flags  = x>>13;
+        }
+        void set_flags(u16 flg) {
+            u16 x = CPU::htons(flg<<13|offset());
+            _offset = x&0x1fff;
+            _flags  = x>>13;
+        }
+        void set_protocol(u8 protocol){
+            _protocol = protocol;
+        }
 
-		void calculate_checksum();
-		
-		friend Debug& operator<< (Debug &db, const IP::Header &h) {
-			IP::Address ip_src(h._src_ip), ip_dst(h._dst_ip);
-			u16 flags = h.flags();
+        void calculate_checksum();
+        
+        friend Debug& operator<< (Debug &db, const IP::Header &h) {
+            IP::Address ip_src(h._src_ip), ip_dst(h._dst_ip);
+            u16 flags = h.flags();
 
-			db 	<< "{ver=" << h._version
-			<< ",ihl=" << h._ihl
-			<< ",tos=" << h._tos
-			<< ",len=" << CPU::ntohs(h._length)
-			<< ",id="  << CPU::ntohs(h._id)
-			<< ",off=" << h.offset()
-			<< ",flg=" << (flags == IP::Header::DF_FLAG ? "[DF]" : 
-			(flags == IP::Header::MF_FLAG ? "[MF]" : "[ ]"))
-			<< ",ttl=" << h._ttl
-			<< ",pro=" << h._protocol
-			<< ",chk=" << (void *)h._checksum
-			<< ",src=" << ip_src
-			<< ",dst=" << ip_dst
-			<< "}";
-			return db;
-		}
+            db  << "{ver=" << h._version
+            << ",ihl=" << h._ihl
+            << ",tos=" << h._tos
+            << ",len=" << CPU::ntohs(h._length)
+            << ",id="  << CPU::ntohs(h._id)
+            << ",off=" << h.offset()
+            << ",flg=" << (flags == IP::Header::DF_FLAG ? "[DF]" : 
+            (flags == IP::Header::MF_FLAG ? "[MF]" : "[ ]"))
+            << ",ttl=" << h._ttl
+            << ",pro=" << h._protocol
+            << ",chk=" << (void *)h._checksum
+            << ",src=" << ip_src
+            << ",dst=" << ip_dst
+            << "}";
+            return db;
+        }
 
-	private:
-		u8  _ihl:4;     // IP Header Length (in 32-bit words)
-		u8  _version:4; // IP Version
-		u8  _tos;       // Type Of Service (no used -> 0)
-		u16 _length;    // Size of datagram (header + data)
-		u16 _id;        // Datagram id
-		u16 _offset:13; // Fragment offset (x 8 bytes)
-		u16 _flags:3;   // Flags (UN, DF, MF)
-		u8  _ttl;       // Time To Live
-		Protocol  _protocol;  // RFC 1700 (1->ICMP, 6->TCP, 17->UDP)
-		volatile u16 _checksum;  // Header checksum
-		Address _src_ip;    // Source IP address
-		Address _dst_ip;    // Destination IP addres
-	
-		static unsigned short pktid;
-	};
+    private:
+        u8  _ihl:4;     // IP Header Length (in 32-bit words)
+        u8  _version:4; // IP Version
+        u8  _tos;       // Type Of Service (no used -> 0)
+        u16 _length;    // Size of datagram (header + data)
+        u16 _id;        // Datagram id
+        u16 _offset:13; // Fragment offset (x 8 bytes)
+        u16 _flags:3;   // Flags (UN, DF, MF)
+        u8  _ttl;       // Time To Live
+        Protocol  _protocol;  // RFC 1700 (1->ICMP, 6->TCP, 17->UDP)
+        volatile u16 _checksum;  // Header checksum
+        Address _src_ip;    // Source IP address
+        Address _dst_ip;    // Destination IP addres
+    
+        static unsigned short pktid;
+    };
 
-	MAC_Address arp(const Address & la);
-	Address    rarp(const MAC_Address & pa);
+    MAC_Address arp(const Address & la);
+    Address    rarp(const MAC_Address & pa);
 
-	const Address & address() { return _self; }
-	const Address & gateway() { return _gateway; }
-	const Address & netmask() { return _netmask; }
-	
-	void set_address(const Address & addr) { _self = addr; }
-	void set_gateway(const Address & addr) { _gateway = addr; }
-	void set_netmask(const Address & addr) { _netmask = addr; }
+    const Address & address() { return _self; }
+    const Address & gateway() { return _gateway; }
+    const Address & netmask() { return _netmask; }
+    
+    void set_address(const Address & addr) { _self = addr; }
+    void set_gateway(const Address & addr) { _gateway = addr; }
+    void set_netmask(const Address & addr) { _netmask = addr; }
 
-	s32 send(const Address & to,const char * data,u16 size,Protocol proto) {
-		SegmentedBuffer sb(data,size);
-		return send(_self,to,&sb,proto);
-	}
+    s32 send(const Address & to,const char * data,u16 size,Protocol proto) {
+        SegmentedBuffer sb(data,size);
+        return send(_self,to,&sb,proto);
+    }
 
-	s32 send(const Address & to,SegmentedBuffer * data,Protocol proto) {
-		return send(_self,to,data,proto);
-	}
+    s32 send(const Address & to,SegmentedBuffer * data,Protocol proto) {
+        return send(_self,to,data,proto);
+    }
 
-	s32 send(const Address & from,const Address & to,SegmentedBuffer * data,Protocol proto);
+    s32 send(const Address & from,const Address & to,SegmentedBuffer * data,Protocol proto);
 
-	//TODO: unit not working
-	IP(unsigned int unit=0);
-	~IP();
+    //TODO: unit not working
+    IP(unsigned int unit=0);
+    ~IP();
 
-//	void update(NIC::Observed * o, int p);
+//  void update(NIC::Observed * o, int p);
 
-	void worker_loop();
-	void process_incoming();
-	void process_ip(char * data,u16 size);
+    void worker_loop();
+    void process_incoming();
+    void process_ip(char * data,u16 size);
 
-	void kill();
+    void kill();
 
-	NIC * nic() { return &_nic; }
-	const MAC_Address & hw_address() { return _nic.address(); }
-	const int hw_address_len() { return sizeof(MAC_Address); }
-	const u16 mtu() { return _nic.mtu(); }
+    NIC * nic() { return &_nic; }
+    const MAC_Address & hw_address() { return _nic.address(); }
+    const int hw_address_len() { return sizeof(MAC_Address); }
+    const u16 mtu() { return _nic.mtu(); }
 
-	static u16 calculate_checksum(const void* ptr, u16 count);
-	
-	static IP* instance(unsigned int unit = 0) { 
-		if (!_instance[unit])
-			_instance[unit] = new IP(unit);
-		return _instance[unit]; 
-	}
+    static u16 calculate_checksum(const void* ptr, u16 count);
+    
+    static IP* instance(unsigned int unit = 0) { 
+        if (!_instance[unit])
+            _instance[unit] = new IP(unit);
+        return _instance[unit]; 
+    }
 
-	static int thread_main(IP * thiz);
+    static int thread_main(IP * thiz);
 private:
-	NIC _nic;
+    NIC _nic;
 
-	_ARP::Table _arpt;
-	
-	Address _self;
-	Address _netmask;
-	Address _gateway;
-	Address _broadcast;
+    _ARP::Table _arpt;
+    
+    Address _self;
+    Address _netmask;
+    Address _gateway;
+    Address _broadcast;
 
-	volatile bool _alive;
-	Thread * _thread;
+    volatile bool _alive;
+    Thread * _thread;
 
-	static const unsigned int DEF_VER = 4;
-	static const unsigned int DEF_IHL = 5;    // 20 bytes
-	static const unsigned int DEF_TOS = 0;
-	static const unsigned int DEF_TTL = 0x40; // 64 hops
+    static const unsigned int DEF_VER = 4;
+    static const unsigned int DEF_IHL = 5;    // 20 bytes
+    static const unsigned int DEF_TOS = 0;
+    static const unsigned int DEF_TTL = 0x40; // 64 hops
 
-	// Pseudo header for checksum calculations
-	struct Pseudo_Header {
-		u32 src_ip;
-		u32 dst_ip;
-		u8 zero;
-		u8 protocol;
-		u16 length;
-	};
-	
-	static IP* _instance[Traits<NIC>::NICS::Length];
+    // Pseudo header for checksum calculations
+    struct Pseudo_Header {
+        u32 src_ip;
+        u32 dst_ip;
+        u8 zero;
+        u8 protocol;
+        u16 length;
+    };
+    
+    static IP* _instance[Traits<NIC>::NICS::Length];
 };
 
 __END_SYS
--- include/ip/udp.h
+++ include/ip/udp.h
@@ -7,10 +7,10 @@ __BEGIN_SYS
 
 class UDP_Address:public IP_Address {
  public:
-	UDP_Address() {	}
-	UDP_Address(u16 port):IP_Address(IP::NULL), _port(port) { }
-	UDP_Address(u32 ip, u16 port):IP_Address(ip), _port(port) { }
-	UDP_Address(IP_Address ip, u16 port):IP_Address(ip), _port(port) { }
+    UDP_Address() { }
+    UDP_Address(u16 port):IP_Address(IP::NULL), _port(port) { }
+    UDP_Address(u32 ip, u16 port):IP_Address(ip), _port(port) { }
+    UDP_Address(IP_Address ip, u16 port):IP_Address(ip), _port(port) { }
     UDP_Address(const char *addr) : IP_Address(addr) {
         char *sep = strchr(addr,':');
         if (sep) {
@@ -20,156 +20,156 @@ class UDP_Address:public IP_Address {
         }
     }
 
-	friend Debug & operator <<(Debug & db, const UDP_Address & h) {
-		return print_common(db, h);
-	}
+    friend Debug & operator <<(Debug & db, const UDP_Address & h) {
+        return print_common(db, h);
+    }
 
-	friend OStream & operator <<(OStream & out, const UDP_Address & h) {
-		return print_common(out, h);
-	}
+    friend OStream & operator <<(OStream & out, const UDP_Address & h) {
+        return print_common(out, h);
+    }
 
 
-	u16 port() const {
-		return _port;
-	}
-	IP_Address ip() const {
-		return static_cast < IP_Address > (*this);
-	}
-	void change_port(u16 new_port) {
-		_port = new_port;
-	}
+    u16 port() const {
+        return _port;
+    }
+    IP_Address ip() const {
+        return static_cast < IP_Address > (*this);
+    }
+    void change_port(u16 new_port) {
+        _port = new_port;
+    }
 
  private:
-	u16 _port;
+    u16 _port;
 
-	template < typename T >
-	    static T & print_common(T & out, const UDP_Address & h) {
-		out << h.ip() << ":" << h.port();
-		return out;
-	}
+    template < typename T >
+        static T & print_common(T & out, const UDP_Address & h) {
+        out << h.ip() << ":" << h.port();
+        return out;
+    }
 };
 
 class UDP: public IP::Observer, public Data_Observed < UDP_Address > {
  protected:
-	void calculate_checksum();
+    void calculate_checksum();
 
  public:
-	// UDP ID (IP Frame)
-	static const IP::Protocol ID_UDP = 0x11;
+    // UDP ID (IP Frame)
+    static const IP::Protocol ID_UDP = 0x11;
 
-	typedef UDP_Address Address;
+    typedef UDP_Address Address;
 
-	class Header {
-	 public:
-		Header() {} 
+    class Header {
+     public:
+        Header() {} 
 
-		Header(u16 src_port = 0, u16 dst_port = 0, u16 data_size = 0):
-			_src_port(CPU::htons(src_port)),
-			    _dst_port(CPU::htons(dst_port)),
-			    _length(CPU::htons(sizeof(UDP::Header) + data_size)),
-			    _checksum(0)
-		{ }
-		void checksum(u16 cs) {
-			_checksum = cs;
-		}
-		u16 dst_port() const {
-			return CPU::ntohs(_dst_port);
-		}
-		u16 src_port() const {
-			return CPU::ntohs(_src_port);
-		}
-		friend Debug & operator <<(Debug & db, const Header & h) {
-			db << "{sprt=" << CPU::ntohs(h._src_port)
-			    << ",dprt=" << CPU::ntohs(h._dst_port)
-			    << ",len=" << CPU::ntohs(h._length)
-			    << ",chk=" << (void *)h._checksum << "}";
-			return db;
-		}
+        Header(u16 src_port = 0, u16 dst_port = 0, u16 data_size = 0):
+            _src_port(CPU::htons(src_port)),
+                _dst_port(CPU::htons(dst_port)),
+                _length(CPU::htons(sizeof(UDP::Header) + data_size)),
+                _checksum(0)
+        { }
+        void checksum(u16 cs) {
+            _checksum = cs;
+        }
+        u16 dst_port() const {
+            return CPU::ntohs(_dst_port);
+        }
+        u16 src_port() const {
+            return CPU::ntohs(_src_port);
+        }
+        friend Debug & operator <<(Debug & db, const Header & h) {
+            db << "{sprt=" << CPU::ntohs(h._src_port)
+                << ",dprt=" << CPU::ntohs(h._dst_port)
+                << ",len=" << CPU::ntohs(h._length)
+                << ",chk=" << (void *)h._checksum << "}";
+            return db;
+        }
 
-	 private:
-		u16 _src_port;	// Source UDP port
-		u16 _dst_port;	// Destination UDP port
-		u16 _length;	// Length of datagram (header + data) in bytes
-		u16 _checksum;	// Pseudo header checksum (see RFC)
-	};
+     private:
+        u16 _src_port;  // Source UDP port
+        u16 _dst_port;  // Destination UDP port
+        u16 _length;    // Length of datagram (header + data) in bytes
+        u16 _checksum;  // Pseudo header checksum (see RFC)
+    };
 
-	UDP(IP * ip) : _ip(ip) {
-		_ip->attach(this, ID_UDP);
-	}
+    UDP(IP * ip) : _ip(ip) {
+        _ip->attach(this, ID_UDP);
+    }
 
-	~UDP() {
-		_ip->detach(this, ID_UDP);
-	}
+    ~UDP() {
+        _ip->detach(this, ID_UDP);
+    }
 
-	s32 send(Address local, Address remote, SegmentedBuffer * data);
+    s32 send(Address local, Address remote, SegmentedBuffer * data);
 
-	class Socket: public Data_Observer <UDP_Address> {
-		friend class UDP;
-	 protected:
-		UDP::Address _local;
-		UDP::Address _remote;
+    class Socket: public Data_Observer <UDP_Address> {
+        friend class UDP;
+     protected:
+        UDP::Address _local;
+        UDP::Address _remote;
 
-		UDP *_udp;
-	 public:
-		Socket(UDP * udp, UDP::Address local, UDP::Address remote)
-		:_local(local), _remote(remote), _udp(udp) {
-			_udp->attach(this, _local.port());
-		}
+        UDP *_udp;
+     public:
+        Socket(UDP * udp, UDP::Address local, UDP::Address remote)
+        :_local(local), _remote(remote), _udp(udp) {
+            _udp->attach(this, _local.port());
+        }
 
-		~Socket() {
-			_udp->detach(this, _local.port());
-		}
+        ~Socket() {
+            _udp->detach(this, _local.port());
+        }
 
-		s32 send(const char *data, u16 size) {
-			SegmentedBuffer sb(data, size);
-			return send(&sb);
-		}
-		s32 send(SegmentedBuffer * data) {
-			db<IP>(TRC) << "UDP::Socket::send()\n";
-			return _udp->send(_local, _remote, data);
-		}
+        s32 send(const char *data, u16 size) {
+            SegmentedBuffer sb(data, size);
+            return send(&sb);
+        }
+        s32 send(SegmentedBuffer * data) {
+            db<IP>(TRC) << "UDP::Socket::send()\n";
+            return _udp->send(_local, _remote, data);
+        }
 
-		void set_remote(const UDP::Address & party) {
-			_remote = party;
-		}
-		const Address & remote() {
-			return _remote;
-		}
+        void set_remote(const UDP::Address & party) {
+            _remote = party;
+        }
+        const Address & remote() {
+            return _remote;
+        }
 
-		void set_local(const UDP::Address & local) {
-			//TODO: deatach/attach here
-			_local = local;
-		}
+        void set_local(const UDP::Address & local) {
+            //TODO: deatach/attach here
+            _local = local;
+        }
 
-		// every Socket should implement one
-		virtual void received(const UDP::Address & src,
-				      const char *data, unsigned int size) = 0;
+        // every Socket should implement one
+        virtual void received(const UDP::Address & src,
+                      const char *data, unsigned int size) = 0;
 
-		void update(Observed * o, long c,UDP_Address src,
-			     UDP_Address dst,void * data,unsigned int size);
-	};
+        void update(Observed * o, long c,UDP_Address src,
+                 UDP_Address dst,void * data,unsigned int size);
+    };
 
-	// Data_Observer callback
-	void update(Data_Observed<IP::Address> *ob, long c, IP::Address src,
-		    IP::Address dst, void *data, unsigned int size);
+    // Data_Observer callback
+    void update(Data_Observed<IP::Address> *ob, long c, IP::Address src,
+            IP::Address dst, void *data, unsigned int size);
 
-	IP *ip() {
-		return _ip;
-	}
+    IP *ip() {
+        return _ip;
+    }
 
  private:
 
-	void constructor_common();
+    void constructor_common();
 
-	struct Pseudo_Header {
-		u32 src_ip;
-		u32 dst_ip;
-		u8 zero;
-		u8 protocol;
-		u16 length;
-	};
+    struct Pseudo_Header {
+        u32 src_ip;
+        u32 dst_ip;
+        u8 zero;
+        u8 protocol;
+        u16 length;
+    };
 
-	IP *_ip;
+    IP *_ip;
 };
 
 __END_SYS
