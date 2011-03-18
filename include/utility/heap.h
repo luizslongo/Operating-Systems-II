f05e74d6bc27244738023c1065b61b1f3450fcb2
rodrigovr <rodrigovr@lisha.ufsc.br>
2011-03-18 21:34:31 +0000

    reduced code size of everything by moving alloc and free to .cc istead of .h

--- include/utility/heap.h
+++ include/utility/heap.h
@@ -26,52 +26,13 @@ public:
 			    << ") => " << this << "\n";  
     }
 
-    void * alloc(unsigned int bytes) {
-	if(!bytes)
-	    return 0;
-    
-	bytes += sizeof(int);
-	if(bytes < sizeof(Element))
-	    bytes = sizeof(Element);
-    
-	Element * e = search_decrementing(bytes);
-	if(!e) {
-	    out_of_memory();
-	    return 0;
-	}
-	int * addr = reinterpret_cast<int *>(e->object() + e->size());
-	    
-	db<Heap>(TRC) << "Heap::alloc(this=" << this
-		      << ",bytes=" << bytes 
-		      << ") => " << (void *)addr << "\n";
+    void * alloc(unsigned int bytes); 
 
-	addr[0] = bytes;
-	return &addr[1];
-    }
+    void * calloc(unsigned int bytes); 
 
-    void * calloc(unsigned int bytes) {
-	void * addr = alloc(bytes);
-	memset(addr, bytes, 0);
-	return addr;	
-    }
+    void free(void * ptr); 
 
-    void free(void * ptr) {
-	int * addr = reinterpret_cast<int *>(ptr);
-	free(&addr[-1], addr[-1]);
-    }
-
-    void free(void * ptr, unsigned int bytes) {
-	db<Heap>(TRC) << "Heap::free(this=" << this
-		      << ",ptr=" << ptr
-		      << ",bytes=" << bytes << ")\n";
-
-	if(ptr && (bytes >= sizeof(Element))) {
-	    Element * e = new (ptr)
-		Element(reinterpret_cast<char *>(ptr), bytes);
-	    Element * m1, * m2;
-	    insert_merging(e, &m1, &m2);
-	}
-    }
+    void free(void * ptr, unsigned int bytes);
 
 private:
     void out_of_memory();
--- src/utility/heap.cc
+++ src/utility/heap.cc
@@ -13,4 +13,51 @@ void Heap_Common::out_of_memory()
     Machine::panic();
 }
 
+void * Heap_Common::alloc(unsigned int bytes) {
+    if(!bytes)
+        return 0;
+    
+    bytes += sizeof(int);
+    if(bytes < sizeof(Element))
+        bytes = sizeof(Element);
+    
+    Element * e = search_decrementing(bytes);
+    if(!e) {
+        out_of_memory();
+        return 0;
+    }
+    int * addr = reinterpret_cast<int *>(e->object() + e->size());
+        
+    db<Heap>(TRC) << "Heap::alloc(this=" << this
+              << ",bytes=" << bytes 
+              << ") => " << (void *)addr << "\n";
+
+    addr[0] = bytes;
+    return &addr[1];
+}
+
+void * Heap_Common::calloc(unsigned int bytes) {
+    void * addr = alloc(bytes);
+    memset(addr, bytes, 0);
+    return addr;    
+}
+
+void Heap_Common::free(void * ptr) {
+    int * addr = reinterpret_cast<int *>(ptr);
+    free(&addr[-1], addr[-1]);
+}
+
+void Heap_Common::free(void * ptr, unsigned int bytes) {
+    db<Heap>(TRC) << "Heap::free(this=" << this
+              << ",ptr=" << ptr
+              << ",bytes=" << bytes << ")\n";
+
+    if(ptr && (bytes >= sizeof(Element))) {
+        Element * e = new (ptr)
+        Element(reinterpret_cast<char *>(ptr), bytes);
+        Element * m1, * m2;
+        insert_merging(e, &m1, &m2);
+    }
+}
+
 __END_SYS
