7b4e5e3310452696f627d84898d09812472c6a2d
meira <meira@lisha.ufsc.br>
2011-04-19 17:57:04 +0000

    The last code was assuming arguments in the stack, and it's not real for a naked function like context_switch.

--- src/arch/arm7/cpu.cc
+++ src/arch/arm7/cpu.cc
@@ -28,21 +28,16 @@ void ARM7::Context::load() const volatile
  */
 void ARM7::switch_context(Context * volatile * o, Context * volatile n)
 {
-	/* 'o' and 'n' were saved by default respectively in
-	 * sp+4 and sp+0, but they still also in r0 and r1
-	 */
 
 	ASMV("ldr r0, [r0, #0]\n"
 		 "stmia r0, {r0-r12,lr,sp}\n"
 		 "str lr, [r0, #60]\n" // overwriting pc with lr
 		 "mrs r3, cpsr\n"
 		 "str r3, [r0, #64]\n"
-		 "ldr r3, [sp, #4]\n"
-		 "str r3, [r0, #0]\n"
 		 "ldr r3, [r1, #64]\n"
 		 "msr spsr_cfsx, r3\n"
 		 "ldmfd r1, {r0-r12,lr,pc,sp}^\n"
-		);
+        );
 }
 
 
