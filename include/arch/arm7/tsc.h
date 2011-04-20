36dbb4991d341378e28d4b9a36584cf8c4f4c748
Arliones Hoeller <arliones@lisha.ufsc.br>
2011-04-20 11:57:09 +0000

    implementation of ARM7_TSC using Timer_3.

--- include/arch/arm7/tsc.h
+++ include/arch/arm7/tsc.h
@@ -4,6 +4,11 @@
 #define __arm7_tsc_h
 
 #include <tsc.h>
+#include <cpu.h>
+
+//TODO: how to avoid breaking the Arch <=> Mach isolation?
+// the solution to this should bring time_stamp() implementation back to header
+class MC13224V_Timer_3;
 
 __BEGIN_SYS
 
@@ -11,18 +16,22 @@ class ARM7_TSC: public TSC_Common
 {
 private:
     static const unsigned int CLOCK = Traits<Machine>::CLOCK;
+    static const unsigned int RESOLUTION = (2^sizeof(CPU::Reg16))/CLOCK;
 
 public:
     ARM7_TSC() {}
 
-    static Hertz frequency() { return CLOCK; }
+    static Hertz frequency() {return CLOCK;}
 
-    static Time_Stamp time_stamp() {
-    Time_Stamp ts;
-    //ASMV("rdtsc" : "=A" (ts) : ); // must be volatile!
-    return ts;
-    }
+    static void init();
 
+    static Time_Stamp time_stamp();
+
+private:
+    static void tsc_timer_handler();
+
+    static volatile unsigned long _ts;
+    static MC13224V_Timer_3 _timer;
 };
 
 __END_SYS
--- src/arch/arm7/tsc.cc
+++ src/arch/arm7/tsc.cc
@@ -1,9 +1,24 @@
 // EPOS-- ARM7_TSC Implementation
 
 #include <arch/arm7/tsc.h>
+//TODO: how to avoid breaking the Arch <=> Mach isolation?
+#include <timer.h>
+
 
 __BEGIN_SYS
 
-//volatile unsigned long ARM7_TSC::_ts;
+volatile unsigned long ARM7_TSC::_ts = 0;
+MC13224V_Timer_3 ARM7_TSC::_timer(ARM7_TSC::RESOLUTION,
+        ARM7_TSC::tsc_timer_handler);
 
 __END_SYS
+
+__USING_SYS
+
+ARM7_TSC::Time_Stamp ARM7_TSC::time_stamp() {
+    return _timer.read() | _ts << (sizeof(CPU::Reg16) * 8);
+}
+
+void ARM7_TSC::tsc_timer_handler() {
+    _ts++;
+}
