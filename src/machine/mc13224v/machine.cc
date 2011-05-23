7d6b4b0e5abab0ad677375686b5ddbd864df2574
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2011-05-23 13:42:29 +0000

    Starting it over again

--- src/machine/mc13224v/machine.cc
+++
@@ -1,7 +0,0 @@
-// EPOS-- MC13224V Mediator Implementation
-
-#include <mach/mc13224v/machine.h>
-
-__BEGIN_SYS
-
-__END_SYS
--- src/machine/ml310/uart.cc
+++
@@ -1,7 +0,0 @@
-// EPOS ML310 UART Mediator Implementation
-
-#include <mach/ml310/uart.h>
-
-__BEGIN_SYS
-
-__END_SYS
