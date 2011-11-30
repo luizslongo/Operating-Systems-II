329cbb80e8e260f574bf79966b38076ce97bb106
Giovani Gracioli <giovani@lisha.ufsc.br>
2011-11-30 14:19:47 +0000

    _channels[ALARM]->_current and  _channels[SCHEDULER]->_current must be incremented at beggining of the method.

--- src/mach/pc/timer.cc
+++ src/mach/pc/timer.cc
@@ -11,9 +11,13 @@ PC_Timer * PC_Timer::_channels[CHANNELS];
 // Class methods
 void PC_Timer::int_handler(unsigned int i)
 {
+  
+    _channels[ALARM]->_current++;
+    _channels[SCHEDULER]->_current++;
+    
     if((Machine::cpu_id() == 0) && _channels[ALARM])
 	_channels[ALARM]->_handler();
-
+   
     if(_channels[SCHEDULER] && (--_channels[SCHEDULER]->_current <= 0)) {
 	_channels[SCHEDULER]->_current = _channels[SCHEDULER]->_initial;
 	_channels[SCHEDULER]->_handler();
