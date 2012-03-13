7acfc4572caf7d6b7a8ba517e1fed3f2583b2860
Giovani Gracioli <giovani@lisha.ufsc.br>
2012-03-13 14:21:42 +0000

    Adding more 5 class ids that map registers to the address 0x00: 1536, 1537, 1920, 3075, and 3077.

--- src/mach/pc/pci.cc
+++ src/mach/pc/pci.cc
@@ -16,6 +16,7 @@ PC_PCI::Reg32 PC_PCI::_base_address[Region::N] = {
 void PC_PCI::header(const PC_PCI::Locator & l, PC_PCI::Header * h)
 {
     IA32 cpu;
+	int last;
 
     h->vendor_id = cfg16(l.bus, l.dev_fn, VENDOR_ID);
     if((h->vendor_id != 0) && (h->vendor_id != 0xffff)) {
@@ -38,7 +39,7 @@ void PC_PCI::header(const PC_PCI::Locator & l, PC_PCI::Header * h)
 	    cfg32(l.bus, l.dev_fn, _base_address[i], h->region[i].phy_addr); 
 	    cpu.int_enable();
 	    if( (h->region[i].phy_addr & BASE_ADDRESS_SPACE_MASK) || 
-		( (h->type&0x7f) == HEADER_TYPE_BRIDGE || (h->type&0x7f) == HEADER_TYPE_CARDBUS) || h->class_id == 257 || h->class_id == 1027) { // I/O
+		( (h->type&0x7f) == HEADER_TYPE_BRIDGE || (h->type&0x7f) == HEADER_TYPE_CARDBUS) || h->class_id == 257 || h->class_id == 1027 || h->class_id == 1536 || h->class_id == 1537 || h->class_id == 1920 || h->class_id == 3075 || h->class_id == 3077) { // I/O
 		h->region[i].memory = false;
 		h->region[i].phy_addr &= BASE_ADDRESS_IO_MASK;
 		h->region[i].log_addr = ~0;
