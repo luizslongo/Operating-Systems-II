379a748bd163cac2e48bdf90576e2dc84f1e7bc8
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2012-10-14 18:40:32 +0000

    Flag "active_scheduler" has been merged into the semantics of flag "preemptive". Flag "idle_waiting" has been eliminated (see remark on semaphore) and flag "smp" by "multicore", since the new generation of CC students has never heard of SMP!

--- include/thread.h
+++ include/thread.h
@@ -13,17 +13,15 @@ __BEGIN_SYS
 class Thread
 {
     friend class Synchronizer_Common;
+    friend class Alarm;
 
 protected:
-    static const bool active_scheduler = Traits<Thread>::active_scheduler;
     static const bool preemptive = Traits<Thread>::preemptive;
-    static const bool energy_aware = Traits<Thread>::energy_aware;
-    static const bool smp = Traits<Thread>::smp;
-	static const unsigned int MAX_CPUS = Traits<Machine>::MAX_CPUS;
+    static const bool multicore = Traits<Thread>::multicore;
+    static const bool reboot = Traits<System>::reboot;
 
     static const unsigned int QUANTUM = Traits<Thread>::QUANTUM;
-    static const unsigned int STACK_SIZE =
-        Traits<Machine>::APPLICATION_STACK_SIZE;
+    static const unsigned int STACK_SIZE = Traits<Machine>::APPLICATION_STACK_SIZE;
 
     typedef CPU::Log_Addr Log_Addr;
     typedef CPU::Context Context;
@@ -48,21 +46,22 @@ public:
     };
 
     // Thread Queue
-    typedef Queue<Thread> ThreadQueue;
+    typedef Ordered_Queue<Thread, Priority> Queue;
 
 public:
     Thread(int (* entry)(), 
            const State & state = READY,
            const Priority & priority = NORMAL,
            unsigned int stack_size = STACK_SIZE)
-    : _state(state), _link(this)
+    : _state(state), _link(this, priority)
     {
         lock();
 
         _stack = kmalloc(stack_size);
-        _context = CPU::init_stack(_stack, stack_size, &implicit_exit, entry);
+        _context = CPU::init_stack(_stack, stack_size,
+                                   &implicit_exit, entry);
 
-        common_constructor(entry, stack_size);
+        common_constructor(entry, stack_size); // implicit unlock
     }
 
     template<typename T1>
@@ -70,15 +69,15 @@ public:
            const State & state = READY,
            const Priority & priority = NORMAL,
            unsigned int stack_size = STACK_SIZE)
-    : _state(state), _link(this)
+    : _state(state), _link(this, priority)
     {
         lock();
 
         _stack = kmalloc(stack_size);
-        _context = CPU::init_stack(_stack, stack_size, &implicit_exit, entry, 
-                                   a1);
+        _context = CPU::init_stack(_stack, stack_size,
+                                   &implicit_exit, entry, a1);
 
-        common_constructor(entry, stack_size);
+        common_constructor(entry, stack_size); // implicit unlock()
     }
 
     template<typename T1, typename T2>
@@ -86,15 +85,15 @@ public:
            const State & state = READY,
            const Priority & priority = NORMAL,
            unsigned int stack_size = STACK_SIZE)
-    : _state(state), _link(this)
+    : _state(state), _link(this, priority)
     {
         lock();
 
         _stack = kmalloc(stack_size);
-        _context = CPU::init_stack(_stack, stack_size, &implicit_exit, entry,
-                                   a1, a2);
+        _context = CPU::init_stack(_stack, stack_size,
+                                   &implicit_exit, entry, a1, a2);
 
-        common_constructor(entry, stack_size);
+        common_constructor(entry, stack_size); // implicit unlock()
     }
 
     template<typename T1, typename T2, typename T3>
@@ -102,22 +101,23 @@ public:
            const State & state = READY,
            const Priority & priority = NORMAL,
            unsigned int stack_size = STACK_SIZE)
-    : _state(state), _link(this)
+    : _state(state), _link(this, priority)
     {
         lock();
 
         _stack = kmalloc(stack_size);
-        _context = CPU::init_stack(_stack, stack_size, &implicit_exit, entry, 
-                                   a1, a2, a3);
+        _context = CPU::init_stack(_stack, stack_size,
+                                   &implicit_exit, entry, a1, a2, a3);
 
-        common_constructor(entry, stack_size);
+        common_constructor(entry, stack_size); // implicit unlock()
     }
 
     ~Thread();
 
     const volatile State & state() const { return _state; }
 
-    volatile const Priority  & priority() const { return _priority; }
+    const volatile Priority  & priority() const { return _link.rank(); }
+    void priority(const Priority & p);
 
     int join();
     void pass();
@@ -133,22 +133,19 @@ public:
 protected:
     void common_constructor(Log_Addr entry, unsigned int stack_size);
 
-    static Thread* volatile running() { return _running; }
-
-    ThreadQueue::Element* link() { return &_link; }
+    static Thread * volatile running() { return _running; }
 
     static void lock() {
         CPU::int_disable();
-        if(smp)
+        if(multicore)
             _lock.acquire();
     }
 
     static void unlock() {
-        if(smp)
+        if(multicore)
             _lock.release();
         CPU::int_enable();
     }
-
 
     static void reschedule();
 
@@ -156,26 +153,44 @@ protected:
 
     static void implicit_exit();
 
+    static void dispatch(Thread * prev, Thread * next) {
+        if(prev != next) {
+            if(!cooperative) {
+                // prev->_context->save(); // can be used to force an update
+                db<Thread>(TRC) << "Thread::dispatch(prev=" << prev
+                    << ",next=" << next << ")\n";
+                db<Thread>(INF) << "prev={" << prev << ","
+                    << *prev->_context << "}\n";
+                db<Thread>(INF) << "next={" << next << ","
+                    << *next->_context << "}\n";
+            }
+
+            if(multicore)
+                _lock.release();
+            CPU::switch_context(&prev->_context, next->_context);
+        } else
+            if(multicore)
+                _lock.release();
+
+        CPU::int_enable();
+    }
+
     static void idle();
 
 protected:
     Log_Addr _stack;
     Context * volatile _context;
     volatile State _state;
-    ThreadQueue::Element _link;
+    Queue::Element _link;
 
     static Spin _lock;
     static Scheduler_Timer * _timer;
 
 private:
-    volatile Priority _priority;
-
-private:
     static Thread * volatile _running;
-    static ThreadQueue _ready;
-    static ThreadQueue _suspended;
+    static Queue _ready;
+    static Queue _suspended;
 };
-
 
 __END_SYS
 
