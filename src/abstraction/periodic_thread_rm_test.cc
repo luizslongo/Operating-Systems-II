19483130b1f4e3de0b11d67fc1b9d0c588844783
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2013-05-28 01:55:28 +0000

    Trying to fix the UNIX perm flags that were mistakenly associated to these files

--- src/abstraction/periodic_thread_rm_test.cc
+++ src/abstraction/periodic_thread_rm_test.cc
@@ -6,56 +6,72 @@
 
 __USING_SYS
 
-const int iterations = 100;
-const long period_a = 100; // ms
-const long period_b = 100; // ms
-const long period_c =  62; // ms
-const long wcet_a = 40; // ms
-const long wcet_b = 20; // ms
-const long wcet_c = 12; // ms
+const unsigned int iterations = 100;
+const unsigned int period_a = 100; // ms
+const unsigned int period_b = 80; // ms
+const unsigned int period_c = 60; // ms
+const unsigned int wcet_a = 50; // ms
+const unsigned int wcet_b = 20; // ms
+const unsigned int wcet_c = 10; // ms
 
-int func_a(void);
-int func_b(void);
-int func_c(void);
-void print(char c);
-long max(long a, long b, long c) { return ((a >= b) && (a >= c)) ? a : ((b >= a) && (b >= c) ? b : c); }
+int func_a();
+int func_b();
+int func_c();
+long max(unsigned int a, unsigned int b, unsigned int c) { return ((a >= b) && (a >= c)) ? a : ((b >= a) && (b >= c) ? b : c); }
 
 OStream cout;
-
 Chronometer chrono;
+Periodic_Thread * thread_a;
+Periodic_Thread * thread_b;
+Periodic_Thread * thread_c;
+
+inline void exec(char c, Alarm::Microsecond time = 0)
+{
+    Alarm::Microsecond elapsed = Alarm::elapsed() / 1000;
+    cout << "\n" << elapsed << "\t" << c
+         << "\t[p(A)=" << thread_a->priority() / 1000
+         << ", p(B)=" << thread_b->priority() / 1000
+         << ", p(C)=" << thread_c->priority() / 1000 << "]";
+    if(time) {
+        // Delay was not used here to prevent scheduling interference due to blocking
+        for(Alarm::Microsecond end = elapsed + time, last = end; end > elapsed; elapsed = Alarm::elapsed() / 1000)
+            if(last != elapsed) {
+                cout << "\n" << elapsed << "\t" << c
+                    << "\t[p(A)=" << thread_a->priority() / 1000
+                    << ", p(B)=" << thread_b->priority() / 1000
+                    << ", p(C)=" << thread_c->priority() / 1000 << "]";
+                last = elapsed;
+            }
+    }
+}
+
 
 int main()
 {
     cout << "Periodic Thread Abstraction Test\n";
 
     cout << "\nThis test consists in creating three periodic threads as follows:\n";
-    cout << "- Every " << period_a << "ms, thread A prints \"a\", waits for " << wcet_a << "ms and then prints another \"a\";\n";
-    cout << "- Every " << period_b << "ms, thread B prints \"b\", waits for " << wcet_b << "ms and then prints another \"b\";\n";
-    cout << "- Every " << period_c << "ms, thread C prints \"c\", waits for " << wcet_c << "ms and then prints another \"c\";\n";
+    cout << "- Every " << period_a << "ms, thread A execs \"a\", waits for " << wcet_a << "ms and then execs another \"a\";\n";
+    cout << "- Every " << period_b << "ms, thread B execs \"b\", waits for " << wcet_b << "ms and then execs another \"b\";\n";
+    cout << "- Every " << period_c << "ms, thread C execs \"c\", waits for " << wcet_c << "ms and then execs another \"c\";\n";
 
     cout << "Threads will now be created and I'll wait for them to finish...\n";
 
-    Periodic_Thread thread_a(&func_a, period_a * 1000, iterations);
-    print('A');
-    Periodic_Thread thread_b(&func_b, period_b * 1000, iterations);
-    print('B');
-    Periodic_Thread thread_c(&func_c, period_c * 1000, iterations);
-    print('C');
+    thread_a = new Periodic_Thread(&func_a, period_a * 1000, iterations);
+    thread_b = new Periodic_Thread(&func_b, period_b * 1000, iterations);
+    thread_c = new Periodic_Thread(&func_c, period_c * 1000, iterations);
 
-    print('M');
+    exec('M');
 
     chrono.start();
 
-    int status_a = thread_a.join();
-    print('A');
-    int status_b = thread_b.join();
-    print('B');
-    int status_c = thread_c.join();
-    print('C');
+    int status_a = thread_a->join();
+    int status_b = thread_b->join();
+    int status_c = thread_c->join();
 
     chrono.stop();
 
-    print('M');
+    exec('M');
 
     cout << "\n... done!\n";
     cout << "\n\nThread A exited with status \"" << char(status_a)
@@ -73,42 +89,33 @@ int main()
 
 int func_a()
 {
+    exec('A');
     for(int i = 0; i < iterations; i++) {
-        print('a');
-        // Delay was not used here to prevent scheduling interference due to blocking
-        for(Alarm::Microsecond t = Alarm::elapsed() + wcet_a * 1000; t > Alarm::elapsed(););
-        print('a');
+        exec('a', wcet_a);
         Periodic_Thread::wait_next();
     }
+    exec('A');
     return 'A';   
 }
 
 int func_b()
 {
+    exec('B');
     for(int i = 0; i < iterations; i++) {
-        print('b');
-        // Delay was not used here to prevent scheduling interference due to blocking
-        for(Alarm::Microsecond t = Alarm::elapsed() + wcet_b * 1000; t > Alarm::elapsed(););
-        print('b');
+        exec('b', wcet_b);
         Periodic_Thread::wait_next();
     }
+    exec('B');
     return 'B';   
 }
 
 int func_c()
 {
+    exec('C');
     for(int i = 0; i < iterations; i++) {
-        print('c');
-        // Delay was not used here to prevent scheduling interference due to blocking
-        for(Alarm::Microsecond t = Alarm::elapsed() + wcet_c * 1000; t > Alarm::elapsed(););
-        print('c');
+        exec('c', wcet_c);
         Periodic_Thread::wait_next();
     }
+    exec('C');
     return 'C';
-}
-
-void print(char c)
-{
-    Alarm::Microsecond elapsed = Alarm::elapsed() / 1000;
-    cout << "\n" << elapsed << ":\t" << c << " [pri=" << Thread::self()->priority() / 1000 << "]";
 }
