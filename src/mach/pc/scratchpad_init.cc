04d105fcd56814f82fdcaca93318d172b0f6784e
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2013-08-31 13:00:46 +0000

    To smooth the integration with the kernel branch, constructors receiving "SELF" have been converted into a static method called sefl().

--- src/mach/pc/scratchpad_init.cc
+++ src/mach/pc/scratchpad_init.cc
@@ -3,6 +3,7 @@
 #include <system/config.h>
 #include <system.h>
 #include <address_space.h>
+#include <segment.h>
 #include <scratchpad.h>
 
 __BEGIN_SYS
@@ -12,7 +13,7 @@ void PC_Scratchpad::init()
     db<Init, Scratchpad>(TRC) << "Scratchpad::init(a=" << ADDRESS << ",s=" << SIZE << ")" << endl;
 
     _segment = new (SYSTEM) Segment(CPU::Phy_Addr(ADDRESS), SIZE);
-    _heap = new (SYSTEM) Heap(Address_Space(SELF).attach(*_segment), _segment->size());
+    _heap = new (SYSTEM) Heap(Address_Space::self()->attach(*_segment), _segment->size());
 }
 
 __END_SYS
