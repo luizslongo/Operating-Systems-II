ec0d98ec2023cb11ef0f6068ada5b52bf1cd0412
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2013-08-31 12:56:39 +0000

    To smooth the integration with the kernel branch, constructors receiving "SELF" have been converted into a static method called sefl().

--- src/abstraction/address_space.cc
+++ src/abstraction/address_space.cc
@@ -1,60 +1,52 @@
 // EPOS Address_Space Abstraction Implementation
 
 #include <address_space.h>
+#include <thread.h>
+#include <task.h>
 
 __BEGIN_SYS
 
 // Methods
+
+Address_Space::Address_Space(MMU::Page_Directory * pd) : MMU::Directory(pd)
+{
+    db <Address_Space> (TRC) << "Address_Space(pd=" << pd << ") [Directory::pd=" << Directory::pd() << "] => " << this << endl;
+}
+
 Address_Space::Address_Space()
 {
-    db <Address_Space> (TRC) << "Address_Space() {Directory::pd=" << Directory::pd()
-                             << "}\n";
+    db <Address_Space> (TRC) << "Address_Space() [Directory::pd=" << Directory::pd() << "] => " << this << endl;
 }
-
-
-Address_Space::Address_Space(const Self & s)
-: Directory(reinterpret_cast<MMU::Page_Table *>(CPU::pdp()))
-{
-    db <Address_Space> (TRC) << "Address_Space(SELF) => {Directory::pd="
-                             << reinterpret_cast<void *>(CPU::pdp()) << "}\n";
-}
-
 
 Address_Space::~Address_Space()
 {
-    db<Address_Space>(TRC) << "~Address_Space() {Directory::pd="
-                           << Directory::pd() << "}\n";
+    db<Address_Space>(TRC) << "~Address_Space(this=" << this << ") [Directory::pd=" << Directory::pd() << "]" << endl;
 }
-
 
 Address_Space::Log_Addr Address_Space::attach(const Segment & seg)
 {
     Log_Addr tmp = Directory::attach(seg);
 
-    db <Address_Space> (TRC) << "Address_Space::attach(seg=" << &seg << ") => " << tmp << "\n";
+    db <Address_Space> (TRC) << "Address_Space::attach(seg=" << &seg << ") => " << tmp << endl;
 
     return tmp;
 }
-
 
 Address_Space::Log_Addr Address_Space::attach(const Segment & seg, Address_Space::Log_Addr addr)
 {
     Log_Addr tmp = Directory::attach(seg, addr);
 
-    db <Address_Space> (TRC) << "Address_Space::attach(seg=" << &seg
-                             << ",addr=" << addr << ") => " << tmp << "\n";
+    db <Address_Space> (TRC) << "Address_Space::attach(seg=" << &seg << ",addr=" << addr << ") => " << tmp << endl;
 
     return tmp;
 }
 
-
 void Address_Space::detach(const Segment & seg)
 {
-    db <Address_Space> (TRC) << "Address_Space::detach(seg=" << &seg << ")\n";
+    db <Address_Space> (TRC) << "Address_Space::detach(seg=" << &seg << ")" << endl;
 
     Directory::detach(seg);
 }
-
 
 Address_Space::Phy_Addr Address_Space::physical(Address_Space::Log_Addr address)
 {
