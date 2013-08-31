ede1fd1be3d96b76984e3292e0e8a85fd2ade008
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2013-08-31 12:57:57 +0000

    To smooth the integration with the kernel branch, constructors receiving "SELF" have been converted into a static method called sefl().

--- src/abstraction/task_init.cc
+++ src/abstraction/task_init.cc
@@ -1,6 +1,7 @@
 // EPOS Task Abstraction Initialization
 
 #include <utility/elf.h>
+#include <mmu.h>
 #include <system.h>
 #include <task.h>
 
@@ -8,20 +9,19 @@ __BEGIN_SYS
 
 void Task::init()
 {
-    System_Info<Machine> * si = System::info();
-
     db<Init, Task>(TRC) << "Task::init()" << endl;
 
-    _master = new (SYSTEM) Task (new (SYSTEM) Address_Space(SELF),
+    System_Info<Machine> * si = System::info();
+
+    _master = new (SYSTEM) Task (new (SYSTEM) Address_Space(MMU::current()),
                                  new (SYSTEM) Segment(Log_Addr(si->lm.app_code), si->lm.app_code_size),
                                  new (SYSTEM) Segment(Log_Addr(si->lm.app_data), si->lm.app_data_size),
                                  CPU::Log_Addr(Memory_Map<Machine>::APP_CODE),
                                  CPU::Log_Addr(Memory_Map<Machine>::APP_DATA));
 
     if(si->bm.extras_offset != -1)
-        db<Init>(INF) << "Task:: additional tasks at " 
-        	      << (void *)si->pmm.ext_base 
-        	      << ":" << si->pmm.ext_top << endl;
+        db<Init>(INF) << "Task:: additional tasks at "  << reinterpret_cast<void *>(si->pmm.ext_base) << ":" << si->pmm.ext_top << endl;
+
 }
 
 __END_SYS
