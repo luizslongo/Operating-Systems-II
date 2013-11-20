d0780532ea02a211fc695cbd3060705c2b322e9f
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2013-11-20 13:20:07 +0000

    ARP working

--- include/arp.h
+++ include/arp.h
@@ -1,58 +1,108 @@
+// EPOS ARP Protocol Declarations
+
 #ifndef __arp_h
 #define __arp_h
 
-#include <network_service.h>
-
-#ifdef __network_service_h
-
-#include <utility/malloc.h>
 #include <utility/hash.h>
 #include <alarm.h>
 #include <condition.h>
 
 __BEGIN_SYS
 
-template<typename Link_Layer, typename Network_Layer>
-class ARP: public Network_Service_Common<Link_Layer, Network_Layer>
+template<typename NIC, typename Network, unsigned int HTYPE = 1>  // Ethernet Hardware Type (HTYPE) = 1
+class ARP: private NIC::Observer
 {
-public:
-    typedef Network_Service_Common<Link_Layer, Network_Layer> Base;
-
-    using Base::_nic;
-    using Base::_network;
-    using Base::_my_nic_address;
-
-    typedef typename Base::Link_Address Link_Address;
-    typedef typename Base::Network_Address Network_Address;
-
-    typedef typename Base::Protocol Protocol;
-    enum {
-        M_ARP    = Link_Layer::ARP,
-        M_RARP   = Link_Layer::RARP,
-        P_ELP    = Link_Layer::ELP,
-        P_IP     = Link_Layer::IP,
-        P_ROUTER = Link_Layer::ROUTER
-    };
-
-    ARP(Link_Layer* nic, Network_Layer* network):
-        Base(nic, network)
-    { }
-
 private:
-    class Entry
-    {
-    public:
-        Entry(const Network_Address & la,
-                Link_Address pa = Link_Layer::BROADCAST):
-            _phy_addr(pa), _link(this, la)
-        { }
+    static const unsigned int ENTRIES = Traits<Network>::NODES;
 
-        Link_Address _phy_addr;
-        Condition _condition;
-        typename Simple_Hash<Entry, 8>::Element _link;
-    };
+    class Entry;
+    typedef Simple_Hash<Entry, ENTRIES> Hash;
+    typedef typename Hash::Element Element;
 
-    // ARP Operations
+
+    // Addresses
+    typedef typename Network::Address PA;
+    typedef typename NIC::Address HA;
+
+
+    // ARP Table Entry
+     class Entry
+     {
+     public:
+         Entry(const PA & pa, const HA & ha = HA::BROADCAST): _ha(ha), _link(this, pa) {}
+
+         HA _ha;
+         Condition _condition;
+         Element _link;
+     };
+
+
+//     class ARP
+//     {
+//     private:
+//         typedef Simple_Hash<ARP, 10, Address> Hash;
+//         typedef Hash::Element Element;
+//
+//     public:
+//         ARP(NIC * n, const Address & log, const MAC_Address & phy)
+//         : _log(log), _phy(phy), _nic(n), _link(this, log) {
+//             _table.insert(&_link);
+//         }
+//         ~ARP() { _table.remove(&_link); }
+//
+//         static const MAC_Address & get(const Address & log) { return _table.search_key(log)->object()->_phy; }
+//
+//     private:
+//         Address _log;
+//         MAC_Address _phy;
+//         NIC * _nic;
+//         Element _link;
+//
+//         static Hash _table;
+//     };
+
+
+     // ARP Table
+     class Table: private Hash
+     {
+     public:
+         Condition * insert(const PA & pa) {
+             Element * el = Hash::search_key(pa);
+             Entry * e;
+
+             if(el)
+                 e = el->object();
+             else {
+                 e = new (SYSTEM) Entry(pa);
+                 Hash::insert(&e->_link);
+             }
+
+             return &e->_condition;
+         }
+
+         void update(const PA & pa, const HA & ha) {
+             Element * el = Hash::search_key(pa);
+             Entry * e;
+
+             if(el) {
+                 e = el->object();
+                 e->_ha = ha;
+                 e->_condition.broadcast();
+             } else {
+                 e = new (SYSTEM) Entry(pa, ha);
+                 Hash::insert(&e->_link);
+             }
+         }
+
+         HA search(const PA & pa) {
+             Element * el = Hash::search_key(pa);
+             return el ? el->object()->_ha : HA::NULL;
+         }
+     };
+
+
+public:
+    // ARP/RARP Operations
     typedef unsigned short Oper;
     enum {
         REQUEST      = 1,
@@ -61,229 +111,136 @@ private:
         RARP_REPLY   = 4
     };
 
-    class Table: public Simple_Hash<Entry, 8>
-    {
-    private:
-        typedef Simple_Hash<Entry, 8> Base;
 
-    public:
-        typedef typename Base::Element Element;
-
-    public:
-        Condition * insert(const Network_Address & la)
-        {
-            Element * el = Base::search_key(la);
-            Entry * e;
-
-            if(el)
-                e = el->object();
-            else {
-                e = new(kmalloc(sizeof(Entry))) Entry(la);
-                Base::insert(&e->_link);
-            }
-
-            return &e->_condition;
-        }
-
-        void update(const Network_Address & la, const Link_Address & pa)
-        {
-            Element * el = Base::search_key(la);
-            Entry * e;
-
-            if(el) {
-                e = el->object();
-                e->_phy_addr = pa;
-                e->_condition.broadcast();
-            } else {
-                e = new(kmalloc(sizeof(Entry))) Entry(la, pa);
-                Base::insert(&e->_link);
-            }
-        }
-
-        Link_Address search(const Network_Address & la)
-        {
-            Element * el = Base::search_key(la);
-
-            if(el)
-                return el->object()->_phy_addr;
-            else
-                return Link_Layer::BROADCAST;
-        }
-    };
-
-public:
-    class Control_Info // ARP Packet
+    // ARP Packet
+    class Packet
     {
     public:
-        // Hardware Types (HTYPE)
-        enum { HTYPE_ETHERNET = 1 };
-
-    public:
-        Control_Info() {}
-        ~Control_Info() {}
-
-        void fill(Oper op, Link_Address sha, Network_Address spa,
-                Link_Address tha, Network_Address tpa)
-        {
-            _htype = htons(HTYPE_ETHERNET);
-            _ptype = htons(P_IP);
-            _hlen  = sizeof(Link_Address);
-            _plen  = sizeof(Network_Address);
-            _oper  = htons(op);
-            _sha   = sha;
-            _spa   = spa;
-            _tha   = tha;
-            _tpa   = tpa;
-        }
+        Packet(Oper op, const HA & sha, const PA & spa, const HA & tha, const PA & tpa)
+        : _htype(htons(HTYPE)), _ptype(htons(Network::PROTOCOL)), _hlen(sizeof(HA)), _plen(sizeof(PA)),
+          _oper(htons(op)), _sha(sha), _spa(spa), _tha(tha), _tpa(tpa) {}
+        ~Packet() {}
 
         void op(const Oper & o) { _oper = htons(o); }
-        void sha(const Link_Address & a) { _sha = a; }
-        void spa(const Network_Address & a) { _spa = a; }
-        void tha(const Link_Address & a) { _tha = a; }
-        void tpa(const Network_Address & a) { _tpa = a; }
+        void sha(const HA & a) { _sha = a; }
+        void spa(const PA & a) { _spa = a; }
+        void tha(const HA & a) { _tha = a; }
+        void tpa(const PA & a) { _tpa = a; }
 
         Oper op() const { return ntohs(_oper); }
-        const Link_Address & sha() const { return _sha; }
-        const Network_Address & spa() const { return _spa; }
-        const Link_Address & tha() const { return _tha; }
-        const Network_Address & tpa() const { return _tpa; }
+        const HA & sha() const { return _sha; }
+        const PA & spa() const { return _spa; }
+        const HA & tha() const { return _tha; }
+        const PA & tpa() const { return _tpa; }
 
-        friend Debug & operator<<(Debug & db, const Control_Info & p)
+        friend Debug & operator<<(Debug & db, const Packet & p)
         {
-            db  << "{htype="  << ntohs(p._htype)
-                << ",ptype="  << hex << ntohs(p._ptype) << dec
-                << ",hlen="   << p._hlen
-                << ",plen="   << p._plen
-                << ",opcode=" << ntohs(p._oper)
-                << ",sha="    << p._sha
-                << ",spa="    << p._spa
-                << ",tha="    << p._tha
-                << ",tpa="    << p._tpa << "}";
-
+            db  << "{htp=" << ntohs(p._htype)
+                << ",ptp=" << hex << ntohs(p._ptype) << dec
+                << ",hln=" << p._hlen
+                << ",pln=" << p._plen
+                << ",opc=" << ntohs(p._oper)
+                << ",sha=" << p._sha
+                << ",spa=" << p._spa
+                << ",tha=" << p._tha
+                << ",tpa=" << p._tpa << "}";
             return db;
         }
 
     private:
-        unsigned short      _htype; // Hardware Type
-        unsigned short      _ptype; // Protocol Type
-        unsigned char       _hlen;  // Hardware Address Length
-        unsigned char       _plen;  // Protocol Address Length
-        unsigned short      _oper;  // Operation
-        Link_Address        _sha; // Sender Hardware Address (48 bits)
-        Network_Address     _spa; // Sender Protocol Address (32 bits)
-        Link_Address        _tha; // Target Hardware Address (48 bits)
-        Network_Address     _tpa; // Target Protocol Address (32 bits)
-    } __attribute__((packed,may_alias));
+        unsigned short  _htype; // Hardware Type
+        unsigned short  _ptype; // Protocol Type
+        unsigned char   _hlen;  // Hardware Address Length
+        unsigned char   _plen;  // Protocol Address Length
+        unsigned short  _oper;  // Operation
+        HA              _sha;   // Sender Hardware Address (48 bits)
+        PA              _spa;   // Sender Protocol Address (32 bits)
+        HA              _tha;   // Target Hardware Address (48 bits)
+        PA              _tpa;   // Target Protocol Address (32 bits)
+    } __attribute__((packed, may_alias));
+
 
 public:
-    void update(const Network_Address& la, const Link_Address& pa)
-    {
-        _arpt.update(la, pa);
-    }
+    ARP(NIC * nic, Network * net): _nic(nic), _net(net) { _nic->attach(this, NIC::ARP); }
 
-    Link_Address resolve(const Network_Address& la, const char * pdu)
-    {
-        for(unsigned int i = 0; i < Traits<Network>::TRIES; i++) {
-            Link_Address pa = _arpt.search(la);
-            if(pa) {
-                db<Network>(TRC) << "ARP::resolve(la=" << la
-                    << ") => " << pa << "\n";
+    void update(const PA & pa, const HA & ha) { _table.update(pa, ha); }
 
-                return pa;
-            }
+    const HA & resolve(const PA & pa) {
+        db<ARP>(TRC) << "ARP::resolve(pa=" << pa << ")" << endl;
 
-            Condition * cond = _arpt.insert(la);
-            Control_Info request;
-            request.fill(REQUEST, _nic->address(),
-                    _network->address(), Link_Layer::BROADCAST, la);
+        const HA & ha = _table.search(pa);
+        for(unsigned int i = 0; (i < Traits<Network>::RETRIES) && !ha; i++) {
+            Condition * cond = _table.insert(pa);
 
-            _nic->send(Link_Layer::BROADCAST,
-                    M_ARP, &request, sizeof(Control_Info));
+            Packet request(REQUEST, _nic->address(), _net->address(), HA::BROADCAST, pa);
 
-            db<Network>(INF) << "ARP::resolve:request sent!\n";
+            db<ARP>(INF) << "ARP::resolve:packet=" << &request << " => " << request << endl;
+
+            _nic->send(HA::BROADCAST, NIC::ARP, &request, sizeof(Packet));
+
+            db<ARP>(INF) << "ARP::resolve:request sent!" << endl;
 
             Condition_Handler handler(cond);
-            Alarm alarm(Traits<Network>::TIMEOUT, &handler, 1);
+            Alarm alarm(Traits<Network>::TIMEOUT * 1000000, &handler, 1);
             cond->wait();
         }
 
-        db<Network>(TRC) << "ARP::resolve(la=" << la
-            << ") => not found!\n";
+        db<ARP>(INF) << "ARP::resolve(pa=" << pa << ") => " << ha << endl;
 
-        return Link_Layer::BROADCAST;
+        return ha;
     }
 
-    virtual Network_Address resolve(const Link_Address& pa)
-    {
-        for(unsigned int i = 0; i < Traits<Network>::TRIES; i++) {
-            Network_Address la = Network_Address(0,0,0,0);
-            if(la) {
-                db<Network>(TRC) << "ARP::resolve(pa=" << pa
-                    << ") => " << la << "\n";
+    const PA & resolve(const HA & ha) {
+        db<ARP>(TRC) << "ARP::resolve(ha=" << ha << ")" << endl;
 
-                return la;
-            }
+        PA pa = PA(0);
+        for(unsigned int i = 0; (i < Traits<Network>::RETRIES) && !pa; i++) {
+            Condition * cond = _table.insert(pa);
 
-            Condition * cond = _arpt.insert(la);
-            Control_Info request;
-            request.fill(RARP_REQUEST, pa, la, pa, la);
-            _nic->send(Link_Layer::BROADCAST,
-                    M_RARP, &request, sizeof(Control_Info));
+            Packet request(RARP_REQUEST, ha, pa, ha, pa);
 
-            db<Network>(INF) << "ARP::resolve:request sent!\n";
+            _nic->send(HA::BROADCAST, NIC::RARP, &request, sizeof(Packet));
+
+            db<ARP>(INF) << "RARP::resolve:request sent => " << request << endl;
 
             Condition_Handler handler(cond);
-            Alarm alarm(Traits<Network>::TIMEOUT, &handler, 1);
+            Alarm alarm(Traits<Network>::TIMEOUT * 1000000, &handler, 1);
             cond->wait();
         }
 
-        db<Network>(TRC) << "ARP::resolve(pa=" << pa
-            << ") => not found!\n";
+        db<Network>(TRC) << "RARP::resolve(ha=" << ha << ") => " << pa << endl;
 
-        return Network_Address(0,0,0,0);
+        return pa;
     }
 
-    void received(const Link_Address& src, Protocol proto,
-            const char* data, int size)
+    void update(typename NIC::Observed * nic, int prot, typename NIC::Buffer * buf)
     {
-        if (proto == M_ARP) {
-            const Control_Info& packet =
-                *reinterpret_cast<const Control_Info *>(data);
-            db<Network>(INF)
-                << "ARP::update:ARP_Control_Info=" << packet << "\n";
+        db<ARP>(TRC) << "ARP::update(nic=" << nic << ",prot=" << prot << ",buf=" << buf << ")" << endl;
 
-            if((packet.op() == REQUEST)
-                    && (packet.tpa() == _network->address())) {
-                Control_Info reply;
-                reply.fill(REPLY, _nic->address(), _network->address(),
-                        packet.sha(), packet.spa());
+        Packet * packet = buf->frame()->template data<Packet>();
 
-                db<Network>(INF)
-                    << "ARP::update: ARP_Control_Info=" << reply << "\n";
-                _nic->send(packet.sha(),
-                        M_ARP, &reply, sizeof(Control_Info));
+        db<ARP>(INF) << "ARP::update:pkt=" << packet << " => " << *packet << endl;
 
-                db<Network>(INF)
-                    << "ARP::update: ARP request answered!\n";
-            } else if((packet.op() == REPLY)
-                    && (packet.tha() == _nic->address())) {
-                db<Network>(INF)
-                    << "ARP::update: ARP reply received!\n";
+        if((packet->op() == REQUEST) && (packet->tpa() == _net->address())) {
+            Packet reply(REPLY, _nic->address(), _net->address(), packet->sha(), packet->spa());
 
-                _arpt.update(packet.spa(), packet.sha());
-            }
+            db<ARP>(INF) << "ARP::update:reply sent => " << reply << endl;
+
+            _nic->send(packet->sha(), NIC::ARP, &reply, sizeof(Packet));
+        } else if((packet->op() == REPLY) && (packet->tha() == _nic->address())) {
+            db<ARP>(INF) << "ARP::update:reply received => " << *packet << endl;
+
+            _table.update(packet->spa(), packet->sha());
         } else
-            db<Network>(WRN)
-                << "Network::update:unknown packet type (" << proto << ")\n";
+            db<ARP>(WRN) << "ARP::update:unknown packet type (" << prot << ")" << endl;
     }
 
 private:
-    Table _arpt;
+    Table _table;
+    NIC * _nic;
+    Network * _net;
 };
 
 __END_SYS
 
-#endif /* __network_service_h */
-
-#endif /* __arp_h */
-
+#endif
