5357c97859cba6ec11b9d93f3e6c9911ff6a5fa5
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2014-08-31 01:32:17 +0000

    QEMU's built-in DHCP server is also performing NAT, so our TCP test gets killed. This is a temporary solution, because it makes it difficult to validate DHCP.

--- app/network_traits.h
+++ app/network_traits.h
@@ -9,7 +9,7 @@ template<> struct Traits<Network>: public Traits<void>
 {
     static const bool enabled = (Traits<Build>::NODES > 1);
 
-    static const bool debugged = true;
+//    static const bool debugged = true;
 
     static const unsigned int NODES = Traits<Build>::NODES;
     static const unsigned int RETRIES = 3;
@@ -40,12 +40,12 @@ template<> struct Traits<IP>: public Traits<Network>
     static const unsigned int TTL  = 0x40; // Time-to-live
 };
 
-template<> struct Traits<IP>::Config<0>: public Traits<IP>::Default_Config
+template<> struct Traits<IP>::Config<0> //: public Traits<IP>::Default_Config
 {
-//    static const unsigned int  TYPE      = MAC;
-//    static const unsigned long ADDRESS   = 0x0a000100;   // 10.0.1.x x=MAC[5]
-//    static const unsigned long NETMASK   = 0xffffff00;   // 255.255.255.0
-//    static const unsigned long GATEWAY   = 0x0a000101;   // 10.0.1.1
+    static const unsigned int  TYPE      = MAC;
+    static const unsigned long ADDRESS   = 0x0a000100;   // 10.0.1.x x=MAC[5]
+    static const unsigned long NETMASK   = 0xffffff00;   // 255.255.255.0
+    static const unsigned long GATEWAY   = 0;   // 10.0.1.1
 };
 
 template<> struct Traits<IP>::Config<1>: public Traits<IP>::Default_Config
@@ -55,6 +55,12 @@ template<> struct Traits<IP>::Config<1>: public Traits<IP>::Default_Config
 template<> struct Traits<UDP>: public Traits<Network>
 {
     static const bool checksum = true;
+};
+
+template<> struct Traits<TCP>: public Traits<Network>
+{
+    static const bool debugged = true;
+    static const unsigned int WINDOW = 4096;
 };
 
 template<> struct Traits<DHCP>: public Traits<Network>
