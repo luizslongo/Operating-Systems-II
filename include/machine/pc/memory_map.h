bd3903f381add7417e6d6ae1b5f2c0eaae468b4b
Mateus Ludwich <mateus@lisha.ufsc.br>
2015-10-15 21:38:39 +0000

    PCI must start after VGA. Otherwise they will overlap. In another words: Memory_Map<PC>::VGA + VGA_SIZE <= Memory_Map<PC>::PCI

--- include/machine/pc/memory_map.h
+++ include/machine/pc/memory_map.h
@@ -27,7 +27,7 @@ struct Memory_Map<PC>
         IO =            Traits<PC>::IO_BASE,
         APIC =          IO,
         VGA =           IO +  4 * 1024,
-        PCI =           IO + Traits<PC_Display>::FRAME_BUFFER_SIZE,
+        PCI =           VGA + Traits<PC_Display>::FRAME_BUFFER_SIZE,
 
         SYS =           Traits<PC>::SYS,
         IDT =           SYS + 0x00000000,
