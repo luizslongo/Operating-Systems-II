9a089afed5c6e5b306968747b6477c8c49ea4d8b
Antonio Augusto Frohlich <guto@lisha.ufsc.br>
2016-05-19 13:50:58 +0000

    DMA_Buffer was being used wrong. Fixed!

--- include/machine/pc/fpga.h
+++ include/machine/pc/fpga.h
@@ -9,9 +9,10 @@
 
 __BEGIN_SYS
 
-class XilinxXXX
+class XAP1052
 {
 protected:
+    // Type Imports
     typedef CPU::Reg8 Reg8;
     typedef CPU::Reg16 Reg16;
     typedef CPU::Reg32 Reg32;
@@ -20,6 +21,11 @@ protected:
     typedef CPU::IO_Port IO_Port;
     typedef CPU::IO_Irq IO_Irq;
     typedef MMU::DMA_Buffer DMA_Buffer;
+
+    // PCI ID
+    static const unsigned int PCI_VENDOR_ID = 0x10ee;
+    static const unsigned int PCI_DEVICE_ID = 0x6024;
+    static const unsigned int PCI_REG_CTRL = 0;
 
 public:
     // Offsets from base I/O address (PCI region 0)
@@ -49,13 +55,13 @@ public:
         reg(DCSR) |= 1;                     // Write: DCSR (offset 0) with value of 1 (Reset Device)
         reg(DCSR) |= 0;                     // Write: DCSR (offset 0) with value of 0 (Make Active)
 
-        reg(WDMATLPA) = _dma_buf;           // Write: Write DMA TLP Address register with starting address
+        reg(WDMATLPA) = _dma_buf->log_address(); // Write: Write DMA TLP Address register with starting address
         reg(WDMATLPS) = 0x20;               // Write: Write DMA TLP Size register with default value (32dwords)
         reg(WDMATLPC) = 0x2000;             // Write: Write DMA TLP Count register with default value (2000)
         reg(WDMATLPP) = 0x00000000;         // Write: Write DMA TLP Pattern register with default value (0x0)
 
         reg(RDMATLPP) = 0xfeedbeef;         // Write: Read DMA Expected Data Pattern with default value (feedbeef)
-        reg(RDMATLPA) = _dma_buf + 4 * 1024; // Write: Read DMA TLP Address register with starting address.
+        reg(RDMATLPA) = _dma_buf->log_address() + 4 * 1024; // Write: Read DMA TLP Address register with starting address.
         reg(RDMATLPS) = 0x20;               // Write: Read DMA TLP Size register with default value (32dwords)
         reg(RDMATLPC) = 0x2000;             // Write: Read DMA TLP Count register with default value (2000)
 
@@ -77,21 +83,16 @@ protected:
 
 
 // PC FPGA
-class PC_FPGA: private FPGA_Common, private XilinxXXX
+class PC_FPGA: private FPGA_Common, private XAP1052
 {
     friend class PC;
 
 private:
-    // PCI ID
-    static const unsigned int PCI_VENDOR_ID = 0x10ee;
-    static const unsigned int PCI_DEVICE_ID = 0x6024;
-    static const unsigned int PCI_REG_CTRL = 0;
-
     // Traits
     static const unsigned int DMA_BUFFER_SIZE = Traits<FPGA>::DMA_BUFFER_SIZE;
 
 public:
-    typedef XilinxXXX Engine;
+    typedef XAP1052 Engine;
 
 public:
     PC_FPGA() {}
