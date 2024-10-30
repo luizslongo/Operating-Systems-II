89d8add7aec1c0542d0711073628c7fa00e5f7a1
Enzo Brum <darosabrumenzo@gmail.com>
2024-10-30 01:01:21 -0300

    fix traits

--- app/hello/hello_traits.h
+++ app/hello/hello_traits.h
@@ -21,7 +21,7 @@ template<> struct Traits<Build>: public Traits_Tokens
     static const bool enabled = true;
     static const bool debugged = true;
     static const bool trace = false;
-    static const bool monitored = false;
+    static const bool monitored = true;
     static const bool hysterically_debugged = true;
 };
 
